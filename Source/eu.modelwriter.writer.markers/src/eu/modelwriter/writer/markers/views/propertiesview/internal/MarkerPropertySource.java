bed7b755f635eb281ef697ae1d8b9b4ff7e30bf3
Ferhat Erata <ferhat.erata@unitbilisim.com>
2015-07-17 00:54:11 +0300

    Minor changes on property view

--- Source/eu.modelwriter.writer.markers/src/eu/modelwriter/writer/markers/views/propertiesview/internal/MarkerPropertySource.java
+++ Source/eu.modelwriter.writer.markers/src/eu/modelwriter/writer/markers/views/propertiesview/internal/MarkerPropertySource.java
@@ -5,10 +5,12 @@ import java.util.ArrayList;
 
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.views.properties.IPropertyDescriptor;
 import org.eclipse.ui.views.properties.IPropertySource;
 import org.eclipse.ui.views.properties.PropertyDescriptor;
 
+import eu.modelwriter.writer.markers.MarkerActivator;
 import eu.modelwriter.writer.markers.internal.MarkElement;
 import eu.modelwriter.writer.markers.internal.Serialization;
 
@@ -20,18 +22,27 @@ public class MarkerPropertySource implements IPropertySource {
 	private static final Object SOURCES = new Object();
 	private static final Object TARGETS = new Object();
 
-	public MarkerPropertySource(MarkElement marker) throws ClassNotFoundException, IOException, CoreException {
+	public MarkerPropertySource(MarkElement marker)
+			throws ClassNotFoundException, IOException, CoreException {
 		this.marker = marker;
 		IMarker mark = MarkElement.getMarker(marker);
-		if ((mark).getAttribute(MarkElement.getTargetAttributeName()) != null)
-			targets = Serialization.getInstance()
-					.fromString((String) (mark).getAttribute(MarkElement.getTargetAttributeName()));
-		if ((mark).getAttribute(MarkElement.getSourceAttributeName()) != null)
-			sources = Serialization.getInstance()
-					.fromString((String) (mark).getAttribute(MarkElement.getSourceAttributeName()));
+		if (mark != null) {
+			if (mark.getAttribute(MarkElement.getTargetAttributeName()) != null)
+				targets = Serialization.getInstance().fromString((String) (mark)
+						.getAttribute(MarkElement.getTargetAttributeName()));
+			if (mark.getAttribute(MarkElement.getSourceAttributeName()) != null)
+				sources = Serialization.getInstance().fromString((String) (mark)
+						.getAttribute(MarkElement.getSourceAttributeName()));
+		} else {
+			MessageDialog dialog = new MessageDialog(MarkerActivator.getShell(),
+					"Property Source Information", null, "Marker is null",
+					MessageDialog.ERROR, new String[] { "OK" }, 0);
+			dialog.open();
+		}
 	}
 
-	public MarkerPropertySource(MarkElement marker, ArrayList<MarkElement> sources, ArrayList<MarkElement> targets) {
+	public MarkerPropertySource(MarkElement marker,
+			ArrayList<MarkElement> sources, ArrayList<MarkElement> targets) {
 		this.marker = marker;
 
 	}
@@ -44,8 +55,10 @@ public class MarkerPropertySource implements IPropertySource {
 
 	@Override
 	public IPropertyDescriptor[] getPropertyDescriptors() {
-		return new IPropertyDescriptor[] { new PropertyDescriptor(DETAILS, "Details"),
-				new PropertyDescriptor(SOURCES, "Sources"), new PropertyDescriptor(TARGETS, "Targets") };
+		return new IPropertyDescriptor[] {
+				new PropertyDescriptor(DETAILS, "Details"),
+				new PropertyDescriptor(SOURCES, "Sources"),
+				new PropertyDescriptor(TARGETS, "Targets") };
 	}
 
 	@Override
