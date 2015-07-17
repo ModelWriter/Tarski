cfc15b63b2cf5e7afcdb8bdf10deaa4db0dba69d
Ferhat Erata <ferhat.erata@unitbilisim.com>
2015-07-17 05:37:22 +0300

    eu.modelwriter.model is created and com.example.decorators deleted.

--- Source/eu.modelwriter.model/src/eu/modelwriter/model/Activator.java
+++ Source/eu.modelwriter.model/src/eu/modelwriter/model/Activator.java
@@ -1,32 +1,44 @@
-package com.example.decorators;
+package eu.modelwriter.model;
 
+import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.ecore.presentation.EcoreEditor;
 import org.eclipse.jface.viewers.DecoratingLabelProvider;
 import org.eclipse.jface.viewers.ILabelDecorator;
 import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorReference;
 import org.eclipse.ui.IPartListener2;
+import org.eclipse.ui.IViewReference;
 import org.eclipse.ui.IWorkbenchPartReference;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
+/**
+ * The activator class controls the plug-in life cycle
+ */
 public class Activator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "eu.modelwriter.model"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
 
 	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
+	 * The constructor
 	 */
-	public static Activator getDefault() {
-		return plugin;
+	public Activator() {
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.
+	 * BundleContext)
+	 */
 	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
@@ -35,7 +47,30 @@ public class Activator extends AbstractUIPlugin {
 				.addPartListener(new IPartListener2() {
 					@Override
 					public void partActivated(IWorkbenchPartReference partRef) {
-						// TODO Auto-generated method stub
+						if (partRef instanceof IViewReference)
+							return;
+						if (partRef.getPart(false) instanceof IEditorPart
+								&& partRef.getPart(
+										false) instanceof EcoreEditor) {
+
+							EcoreEditor eEditor = (EcoreEditor) partRef
+									.getPart(false);
+							TreeViewer viewer = (TreeViewer) eEditor
+									.getViewer();
+							ILabelProvider labelProvider = (ILabelProvider) viewer
+									.getLabelProvider();
+
+							if (labelProvider instanceof DecoratingLabelProvider) {
+								return;
+							} else {
+								ILabelDecorator decorator = getActiveWorkbenchWindow()
+										.getWorkbench().getDecoratorManager()
+										.getLabelDecorator();
+								viewer.setLabelProvider(
+										new DecoratingLabelProvider(
+												labelProvider, decorator));
+							}
+						}
 
 					}
 
@@ -61,29 +96,7 @@ public class Activator extends AbstractUIPlugin {
 
 					@Override
 					public void partOpened(IWorkbenchPartReference partRef) {
-						if (partRef.getPart(false) instanceof IEditorPart
-								&& partRef.getPart(
-										false) instanceof EcoreEditor) {
-							EcoreEditor eEditor = (EcoreEditor) partRef
-									.getPart(false);
-							TreeViewer viewer = (TreeViewer) eEditor
-									.getViewer();
-
-							ILabelDecorator decorator = getActiveWorkbenchWindow()
-									.getWorkbench().getDecoratorManager()
-									.getLabelDecorator();
-							if (viewer
-									.getLabelProvider() instanceof DecoratingLabelProvider) {
-								return;
-							} else {
-								viewer.setLabelProvider(
-										new DecoratingLabelProvider(
-												(ILabelProvider) viewer
-														.getLabelProvider(),
-												decorator));
-							}
-						}
-
+						// TODO Auto-generated method stub
 					}
 
 					@Override
@@ -106,18 +119,37 @@ public class Activator extends AbstractUIPlugin {
 				});
 	}
 
-	@Override
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.
+	 * BundleContext)
+	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
-
 	}
 
-	public Activator() {
-
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
 	}
 
 	public static IWorkbenchWindow getActiveWorkbenchWindow() {
 		return PlatformUI.getWorkbench().getActiveWorkbenchWindow();
 	}
+
+	public void log(String msg) {
+		log(msg, null);
+	}
+
+	public void log(String msg, Exception e) {
+		getLog().log(new Status(Status.INFO, getBundle().getSymbolicName(),
+				Status.OK, msg, e));
+	}
+
 }
--- Source/eu.modelwriter.writer.markers/src/eu/modelwriter/writer/markers/hyperlinkdetectors/TargetViewHyperlinkDetector.java
+++ Source/eu.modelwriter.writer.markers/src/eu/modelwriter/writer/markers/hyperlinkdetectors/TargetViewHyperlinkDetector.java
@@ -23,10 +23,18 @@ public class TargetViewHyperlinkDetector extends AbstractHyperlinkDetector
 			boolean canShowMultipleHyperlinks) {
 
 		int offset = region.getOffset();
+		int length = region.getLength();
+		if (offset == 0 && length == 0)
+			return null;
+
+		if (MarkerActivator.getEditor() == null
+				|| MarkerActivator.getEditor().getEditorInput() == null)
+			return null;
+
 		IFile file = (IFile) MarkerActivator.getEditor().getEditorInput()
 				.getAdapter(IFile.class);
-		List<IMarker> markedList = MarkerFactory.findAllMarkers(file);
 
+		List<IMarker> markedList = MarkerFactory.findAllMarkers(file);
 		for (IMarker iMarker : markedList) {
 			try {
 				// look for keyword
