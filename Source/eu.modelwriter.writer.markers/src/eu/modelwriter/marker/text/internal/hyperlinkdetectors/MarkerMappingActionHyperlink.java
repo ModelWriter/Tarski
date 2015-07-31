274addd70a12b0a242080f6010b13ded86f64f7b
Ferhat Erata <ferhat.erata@unitbilisim.com>
2015-07-31 14:47:03 +0300

    minor updates

--- Source/eu.modelwriter.writer.markers/src/eu/modelwriter/marker/text/internal/hyperlinkdetectors/MarkerMappingActionHyperlink.java
+++ Source/eu.modelwriter.writer.markers/src/eu/modelwriter/marker/text/internal/hyperlinkdetectors/MarkerMappingActionHyperlink.java
@@ -1,22 +1,13 @@
 package eu.modelwriter.marker.text.internal.hyperlinkdetectors;
 
-import java.io.IOException;
-import java.util.ArrayList;
-
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.hyperlink.IHyperlink;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
 
 import eu.modelwriter.marker.Activator;
-import eu.modelwriter.marker.Serialization;
-import eu.modelwriter.marker.internal.MarkElement;
 import eu.modelwriter.marker.internal.MarkerFactory;
-import eu.modelwriter.marker.ui.views.TargetView;
 import eu.modelwriter.marker.ui.wizards.internal.MarkerMappingAction;
 
 public class MarkerMappingActionHyperlink implements IHyperlink {
