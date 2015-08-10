52a8811986d938307a7ed18cafd3d6a1df3caa92
Ferhat Erata <ferhat.erata@unitbilisim.com>
2015-08-10 09:21:26 +0300

    Typing Wizard

---
+++ Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/wizards/markerwizard/MarkerWizard.java
@@ -0,0 +1,15 @@
+package eu.modelwriter.marker.ui.internal.wizards.markerwizard;
+
+import org.eclipse.jface.wizard.Wizard;
+import eu.modelwriter.marker.*;
+import eu.modelwriter.marker.typing.*;
+
+public class MarkerWizard extends Wizard {
+
+  @Override
+  public boolean performFinish() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+}
