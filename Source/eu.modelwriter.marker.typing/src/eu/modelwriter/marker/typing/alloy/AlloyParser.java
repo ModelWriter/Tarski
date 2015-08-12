6db310e2c9d253d5d4515e94ac5a79f487faf601
Emre Kirmizi <emre.kirmizi@unitbilisim.com>
2015-08-12 11:52:56 +0300

    Parse errors and warnings are displayed as popup message.

--- Source/eu.modelwriter.marker.typing/src/eu/modelwriter/marker/typing/alloy/AlloyParser.java
+++ Source/eu.modelwriter.marker.typing/src/eu/modelwriter/marker/typing/alloy/AlloyParser.java
@@ -2,6 +2,9 @@ package eu.modelwriter.marker.typing.alloy;
 
 import java.util.ArrayList;
 
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.PlatformUI;
+
 import edu.mit.csail.sdg.alloy4.A4Reporter;
 import edu.mit.csail.sdg.alloy4.ConstList;
 import edu.mit.csail.sdg.alloy4.Err;
@@ -13,6 +16,7 @@ import edu.mit.csail.sdg.alloy4compiler.ast.Sig.Field;
 import edu.mit.csail.sdg.alloy4compiler.ast.Sig.PrimSig;
 import edu.mit.csail.sdg.alloy4compiler.ast.Sig.SubsetSig;
 import edu.mit.csail.sdg.alloy4compiler.parser.CompUtil;
+import eu.modelwriter.marker.MarkerActivator;
 import eu.modelwriter.marker.internal.MarkerTypeElement;
 
 public class AlloyParser {
@@ -35,35 +39,42 @@ public class AlloyParser {
   }
 
   private void parse(String filename) {
-    A4Reporter rep = new A4Reporter() {
-      // For example, here we choose to display each "warning" by printing it to System.out
-      @Override
-      public void warning(ErrorWarning msg) {
-        System.out.print("Relevance Warning:\n" + (msg.toString().trim()) + "\n\n");
-        System.out.flush();
-      }
-    };
-
-    // Parse+typecheck the model
-    System.out.println("=========== Parsing+Typechecking " + filename + " =============");
-    Module world;
     try {
+      A4Reporter rep = new A4Reporter() {
+        // For example, here we choose to display each "warning" by printing it to System.out
+
+        @Override
+        public void warning(ErrorWarning msg) {
+          MessageDialog dialog =
+              new MessageDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+                  "Alloy Error Information", null,
+                  "Relevance Warning:\n" + (msg.toString().trim()) + "\n\n",
+                  MessageDialog.INFORMATION, new String[] {"OK"}, 0);
+          dialog.open();
+        }
+      };
+
+      // Parse+typecheck the model
+      System.out.println("=========== Parsing+Typechecking " + filename + " =============");
+      Module world;
+
       world = CompUtil.parseEverything_fromFile(rep, null, filename);
+
 
       SafeList<Sig> list = world.getAllSigs();
       for (Sig sig : list) {
-//        String str = sig.toString().substring(sig.toString().indexOf("/")+1);
-        
-        if (sig instanceof PrimSig){
+        // String str = sig.toString().substring(sig.toString().indexOf("/")+1);
+
+        if (sig instanceof PrimSig) {
           PrimSig primSig = (PrimSig) sig;
-//          if (!primSig.children().isEmpty()){
-//            convertToMarkerType(primSig);
-////            types.add(str);
-//          }
-          if (primSig.isTopLevel()){
+          // if (!primSig.children().isEmpty()){
+          // convertToMarkerType(primSig);
+          //// types.add(str);
+          // }
+          if (primSig.isTopLevel()) {
             types.add(convertToMarkerType(primSig));
           }
-        } else if (sig instanceof SubsetSig){
+        } else if (sig instanceof SubsetSig) {
           SubsetSig subsetSig = (SubsetSig) sig;
           ConstList<Sig> listOfParents = subsetSig.parents;
         }
@@ -73,50 +84,57 @@ public class AlloyParser {
               field.decl().expr.toString().substring(0, field.decl().expr.toString().indexOf(" "));
           rels.add(str2);
         }
-
       }
     } catch (Err e) {
-      // TODO hatayi yakala
-      e.printStackTrace();
+      // TODO Auto-generated catch block
+      MessageDialog dialog =
+          new MessageDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+              "Alloy Error Information", null, e.getMessage(), MessageDialog.INFORMATION,
+              new String[] {"OK"}, 0);
+      dialog.open();
     }
   }
-  
-  private MarkerTypeElement convertToMarkerType(Sig rootSig){
-    if (rootSig instanceof PrimSig){
+
+  private MarkerTypeElement convertToMarkerType(Sig rootSig) {
+    if (rootSig instanceof PrimSig) {
       PrimSig primSig = (PrimSig) rootSig;
-      MarkerTypeElement rootType = new MarkerTypeElement(primSig.toString().substring(primSig.toString().indexOf("/")+1));
+      MarkerTypeElement rootType =
+          new MarkerTypeElement(primSig.toString().substring(primSig.toString().indexOf("/") + 1));
       try {
-        if (primSig.children().isEmpty()){
+        if (primSig.children().isEmpty()) {
           return rootType;
-        }
-        else {
-          for (int i=0; i<primSig.children().size(); i++) {
+        } else {
+          for (int i = 0; i < primSig.children().size(); i++) {
             rootType.getChildren().add(convertToMarkerType(primSig.children().get(i)));
           }
           return rootType;
         }
       } catch (Err e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
+        MessageDialog dialog =
+            new MessageDialog(MarkerActivator.getShell(), "Alloy Error Information", null,
+                e.getMessage(), MessageDialog.INFORMATION, new String[] {"OK"}, 0);
+        dialog.open();
       }
-//      try {
-//        PrimSig primSig = (PrimSig) rootSig;
-//        MarkerTypeElement root = new MarkerTypeElement(primSig.toString());
-//        SafeList<Sig.PrimSig> listOfChildren = primSig.children();
-//        for (PrimSig childPrimSig : listOfChildren) {
-//          MarkerTypeElement child = new MarkerTypeElement(childPrimSig.toString());
-//          root.getChildren().add(child);
-//        }
-//        if (primSig.isTopLevel()){
-//          types.add(root);
-//        }
-//      } catch (Err e) {
-//        // TODO Auto-generated catch block
-//        e.printStackTrace();
-//      }
-    } else if (rootSig instanceof SubsetSig){
-//      SubsetSig subsetSig = (SubsetSig) rootSig;
-//      subsetSig.parents
+      // try {
+      // PrimSig primSig = (PrimSig) rootSig;
+      // MarkerTypeElement root = new MarkerTypeElement(primSig.toString());
+      // SafeList<Sig.PrimSig> listOfChildren = primSig.children();
+      // for (PrimSig childPrimSig : listOfChildren) {
+      // MarkerTypeElement child = new MarkerTypeElement(childPrimSig.toString());
+      // root.getChildren().add(child);
+      // }
+      // if (primSig.isTopLevel()){
+      // types.add(root);
+      // }
+      // } catch (Err e) {
+      // MessageDialog dialog =
+      // new MessageDialog(MarkerActivator.getShell(), "Alloy Error Information", null,
+      // e.getMessage(), MessageDialog.INFORMATION, new String[] {"OK"}, 0);
+      // dialog.open();
+      // }
+    } else if (rootSig instanceof SubsetSig) {
+      // SubsetSig subsetSig = (SubsetSig) rootSig;
+      // subsetSig.parents
     }
     return null;
   }
