f4f4677ac185effef0582a688cd71b973dfd24c2
Serhat Çelik <serhat.celik@unitbilisim.com>
2015-08-12 09:55:51 +0300

    Refs table has been added to preferences

---
+++ Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/preferences/RefColumnLabelProvider.java
@@ -0,0 +1,23 @@
+package eu.modelwriter.marker.ui.internal.preferences;
+
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.TableColumn;
+
+public abstract class RefColumnLabelProvider extends ColumnLabelProvider {
+
+	public abstract String getText(Object element);
+
+	public abstract String getTitle();
+
+	public TableViewerColumn addColumnTo(TableViewer viewer) {
+		TableViewerColumn tableViewerColumn = new TableViewerColumn(viewer, SWT.NONE);
+		TableColumn column = tableViewerColumn.getColumn();
+		column.setText(getTitle());
+		tableViewerColumn.setLabelProvider(this);
+		return tableViewerColumn;
+	}
+
+}
