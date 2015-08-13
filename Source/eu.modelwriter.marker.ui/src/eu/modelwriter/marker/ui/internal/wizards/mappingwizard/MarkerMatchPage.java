cc0d43afb35dd8faf760c34c15da7b191adfb6d1
Serhat Çelik <serhat.celik@unitbilisim.com>
2015-08-13 10:20:51 +0300

    Relation dialog has been added to MarkerMappingWizard

--- Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/wizards/mappingwizard/MarkerMatchPage.java
+++ Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/wizards/mappingwizard/MarkerMatchPage.java
@@ -9,7 +9,10 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.viewers.CheckStateChangedEvent;
 import org.eclipse.jface.viewers.CheckboxTreeViewer;
 import org.eclipse.jface.viewers.ICheckStateListener;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.ITreeSelection;
 import org.eclipse.jface.viewers.ITreeViewerListener;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TreeExpansionEvent;
 import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.jface.wizard.WizardPage;
@@ -22,6 +25,7 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 
 import eu.modelwriter.marker.internal.MarkElement;
+import org.eclipse.swt.widgets.Label;
 
 public class MarkerMatchPage extends WizardPage {
   public static CheckboxTreeViewer markTreeViewer = null;
@@ -40,9 +44,17 @@ public class MarkerMatchPage extends WizardPage {
 
     Composite composite = new Composite(parent, SWT.NONE);
     composite.setLayout(new GridLayout(1, false));
-
-    Button preserveCase = new Button(composite, SWT.CHECK);
-    preserveCase.setText("&Show only files that contain Marker(s)");
+    
+    Button btnNewButton = new Button(composite, SWT.NONE);
+    btnNewButton.addSelectionListener(new SelectionAdapter() {
+    	@Override
+    	public void widgetSelected(SelectionEvent e) {
+    		RelationDialog relationDialog= new RelationDialog(getShell());
+    		relationDialog.open();
+    	}
+    });
+    btnNewButton.setText("Add Relation");
+    btnNewButton.setEnabled(false);
 
     markTreeViewer = new CheckboxTreeViewer(composite);
     markTreeViewer.getTree().setLayoutData(new GridData(GridData.FILL_BOTH));
@@ -59,29 +71,6 @@ public class MarkerMatchPage extends WizardPage {
         markTreeViewer.setChecked(MarkElement.getiMarker(checkedMarkElement), true);
       }
     }
-
-    preserveCase.addSelectionListener(new SelectionAdapter() {
-      public void widgetSelected(SelectionEvent event) {
-        boolean preserveCase = ((Button) event.widget).getSelection();
-
-        if (preserveCase) {
-          markTreeViewer.setFilters(filter);
-//          if (beforeMappingTargetMarkElements.size() != 0) {
-//            for (MarkElement checkedMarkElement : beforeMappingTargetMarkElements) {
-//              markTreeViewer.setChecked(MarkElement.getiMarker(checkedMarkElement), true);
-//            }
-//          }
-        } else {
-          markTreeViewer.resetFilters();
-//          if (beforeMappingTargetMarkElements.size() != 0) {
-//            for (MarkElement checkedMarkElement : beforeMappingTargetMarkElements) {
-//              markTreeViewer.setChecked(MarkElement.getiMarker(checkedMarkElement), true);
-//            }
-//          }
-        }
-
-      }
-    });
 
     markTreeViewer.addTreeListener(new ITreeViewerListener() {
 
@@ -153,6 +142,43 @@ public class MarkerMatchPage extends WizardPage {
         setPageComplete(true);
       }
     });
+    markTreeViewer.addSelectionChangedListener(new ISelectionChangedListener() {
+		
+		@Override
+		public void selectionChanged(SelectionChangedEvent event) {
+        	if(((ITreeSelection)event.getSelection()).getFirstElement() instanceof IMarker)
+        		btnNewButton.setEnabled(true);
+        	else 
+        		btnNewButton.setEnabled(false);
+			
+		}
+	});
     setControl(composite);
+    
+        Button preserveCase_1 = new Button(composite, SWT.CHECK);
+        preserveCase_1.setText("&Show only files that contain Marker(s)");
+        
+            preserveCase_1.addSelectionListener(new SelectionAdapter() {
+              public void widgetSelected(SelectionEvent event) {
+                boolean preserveCase = ((Button) event.widget).getSelection();
+        
+                if (preserveCase) {
+                  markTreeViewer.setFilters(filter);
+        //          if (beforeMappingTargetMarkElements.size() != 0) {
+        //            for (MarkElement checkedMarkElement : beforeMappingTargetMarkElements) {
+        //              markTreeViewer.setChecked(MarkElement.getiMarker(checkedMarkElement), true);
+        //            }
+        //          }
+                } else {
+                  markTreeViewer.resetFilters();
+        //          if (beforeMappingTargetMarkElements.size() != 0) {
+        //            for (MarkElement checkedMarkElement : beforeMappingTargetMarkElements) {
+        //              markTreeViewer.setChecked(MarkElement.getiMarker(checkedMarkElement), true);
+        //            }
+        //          }
+                }
+        
+              }
+            });
   }
 }
---
+++ Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/wizards/mappingwizard/RelationDialog.java
@@ -0,0 +1,83 @@
+package eu.modelwriter.marker.ui.internal.wizards.mappingwizard;
+
+import java.io.IOException;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Table;
+
+import eu.modelwriter.marker.Serialization;
+import eu.modelwriter.marker.ui.internal.preferences.RefColumn;
+import eu.modelwriter.marker.ui.internal.wizards.markerwizard.MarkerPage;
+
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.TableViewer;
+
+public class RelationDialog extends Dialog {
+	private Table table;
+
+	/**
+	 * Create the dialog.
+	 * @param parentShell
+	 */
+	public RelationDialog(Shell parentShell) {
+		super(parentShell);
+	}
+
+	/**
+	 * Create contents of the dialog.
+	 * @param parent
+	 */
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		Composite container = (Composite) super.createDialogArea(parent);
+		container.setLayout(new FillLayout(SWT.HORIZONTAL));
+		
+		TableViewer tableViewer = new TableViewer(container, SWT.BORDER | SWT.FULL_SELECTION);
+		table = tableViewer.getTable();
+		
+		tableViewer.setContentProvider(ArrayContentProvider.getInstance());
+	    new RefColumn().addColumnTo(tableViewer);
+	    
+	    String rels = MarkerPage.settings.get("rels");
+	      if (rels != null) {
+	        try {
+				tableViewer.setInput(Serialization.getInstance().fromString(rels));
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	      }
+		
+
+		return container;
+	}
+
+	/**
+	 * Create contents of the button bar.
+	 * @param parent
+	 */
+	@Override
+	protected void createButtonsForButtonBar(Composite parent) {
+		createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
+		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
+	}
+
+	/**
+	 * Return the initial size of the dialog.
+	 */
+	@Override
+	protected Point getInitialSize() {
+		return new Point(345, 431);
+	}
+
+}
