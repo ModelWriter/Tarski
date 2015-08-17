f551bf978c6228c462627add3b8aafdd845d7aa8
Ümit Anıl Öztürk <anil.ozturk@unitbilisim.com>
2015-08-17 08:13:47 +0300

    marker visibility.

---
+++ Source/eu.modelwriter.marker.command/src/eu/modelwriter/marker/command/MarkerVisibilityHandler.java
@@ -0,0 +1,55 @@
+package eu.modelwriter.marker.command;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class MarkerVisibilityHandler extends AbstractHandler {
+
+  @Override
+  public Object execute(ExecutionEvent event) throws ExecutionException {
+//    DefaultMarkerAnnotationAccess dmaa = new DefaultMarkerAnnotationAccess();
+//    AnnotationModel anno = new AnnotationModel();
+//    anno.
+//    MarkerAnnotationPreferences map = EditorsPlugin.getDefault().getMarkerAnnotationPreferences();
+//    Iterator iter = map.getAnnotationPreferences().iterator();
+//    while (iter.hasNext()) {AnnotationPreference
+//      Object object = (Object) iter.next();
+//      if (((AnnotationPreference)object).getAnnotationType().equals(MarkerFactory.ANNOTATION_MARKING)){
+//        Object markerAnot = ((AnnotationPreference)object).getAnnotationType();
+//        AnnotationType markerAnotType = (AnnotationType) markerAnot;
+//        markerAnotType.getPreference().getPreferenceLabel();
+//        markerAnotType.getPreference().getOverviewRulerPreferenceKey();
+//        markerAnotType.getPreference().getTextPreferenceKey();
+//        markerAnotType.getPreference().getVerticalRulerPreferenceKey();
+//      }
+//    }
+//    AnnotationPreference ap = EditorsUI.getAnnotationPreferenceLookup().getAnnotationPreference(MarkerFactory.ANNOTATION_MARKING);
+//    ap.getPreferenceLabel();
+//    ap.getOverviewRulerPreferenceKey();
+//    ap.getTextPreferenceKey();
+//    ap.getVerticalRulerPreferenceKey();
+//    ap.setIncludeOnPreferencePage(true);
+//    MarkerAnnotationPreferences ap = EditorsPlugin.getDefault().getMarkerAnnotationPreferences();O
+//    Iterator iter = ap.getAnnotationPreferences().iterator();
+//    while (iter.hasNext()) {
+//      Object object = (Object) iter.next();
+//    }
+//    EditorsUI.getAnnotationPreferenceLookup()
+//        .getAnnotationPreference(MarkerFactory.ANNOTATION_MARKING);
+    // ap.setTextStylePreferenceKey(AnnotationPreference.STYLE_DASHED_BOX);
+    // ap.setVerticalRulerPreferenceValue(false);
+    // ap.setTextPreferenceValue(false);
+    // ap.setOverviewRulerPreferenceValue(false);
+    // ap.setColorPreferenceValue(new RGB(255, 0, 0));
+//     AnnotationPreference ap = new AnnotationPreference();
+//     AnnotationType type = new AnnotationType("new", null);
+//     AnnotationPreference ap = new AnnotationPreference();
+//     ap.setColorPreferenceKey(AnnotationPreference.STYLE_NONE);
+//     type.setAnnotationPreference(ap);
+//     "Annotation".setType(type);
+
+    return null;
+  }
+
+}
