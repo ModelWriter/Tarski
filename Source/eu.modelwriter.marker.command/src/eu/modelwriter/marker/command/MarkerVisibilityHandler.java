964fd84e38bdfdcf2b6e32df3ae923ebb5995fe7
Ümit Anıl Öztürk <anil.ozturk@unitbilisim.com>
2015-08-17 10:51:05 +0300

    when you dont want to see markers you can disable them.

--- Source/eu.modelwriter.marker.command/src/eu/modelwriter/marker/command/MarkerVisibilityHandler.java
+++ Source/eu.modelwriter.marker.command/src/eu/modelwriter/marker/command/MarkerVisibilityHandler.java
@@ -3,53 +3,68 @@ package eu.modelwriter.marker.command;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.ui.IDecoratorManager;
+import org.eclipse.ui.editors.text.EditorsUI;
+import org.eclipse.ui.texteditor.AnnotationPreference;
+
+import eu.modelwriter.marker.internal.MarkerFactory;
 
 public class MarkerVisibilityHandler extends AbstractHandler {
+  private static boolean isHidden = false;
+
+  public static boolean isHidden() {
+    return isHidden;
+  }
 
   @Override
   public Object execute(ExecutionEvent event) throws ExecutionException {
-//    DefaultMarkerAnnotationAccess dmaa = new DefaultMarkerAnnotationAccess();
-//    AnnotationModel anno = new AnnotationModel();
-//    anno.
-//    MarkerAnnotationPreferences map = EditorsPlugin.getDefault().getMarkerAnnotationPreferences();
-//    Iterator iter = map.getAnnotationPreferences().iterator();
-//    while (iter.hasNext()) {AnnotationPreference
-//      Object object = (Object) iter.next();
-//      if (((AnnotationPreference)object).getAnnotationType().equals(MarkerFactory.ANNOTATION_MARKING)){
-//        Object markerAnot = ((AnnotationPreference)object).getAnnotationType();
-//        AnnotationType markerAnotType = (AnnotationType) markerAnot;
-//        markerAnotType.getPreference().getPreferenceLabel();
-//        markerAnotType.getPreference().getOverviewRulerPreferenceKey();
-//        markerAnotType.getPreference().getTextPreferenceKey();
-//        markerAnotType.getPreference().getVerticalRulerPreferenceKey();
-//      }
-//    }
-//    AnnotationPreference ap = EditorsUI.getAnnotationPreferenceLookup().getAnnotationPreference(MarkerFactory.ANNOTATION_MARKING);
-//    ap.getPreferenceLabel();
-//    ap.getOverviewRulerPreferenceKey();
-//    ap.getTextPreferenceKey();
-//    ap.getVerticalRulerPreferenceKey();
-//    ap.setIncludeOnPreferencePage(true);
-//    MarkerAnnotationPreferences ap = EditorsPlugin.getDefault().getMarkerAnnotationPreferences();O
-//    Iterator iter = ap.getAnnotationPreferences().iterator();
-//    while (iter.hasNext()) {
-//      Object object = (Object) iter.next();
-//    }
-//    EditorsUI.getAnnotationPreferenceLookup()
-//        .getAnnotationPreference(MarkerFactory.ANNOTATION_MARKING);
-    // ap.setTextStylePreferenceKey(AnnotationPreference.STYLE_DASHED_BOX);
-    // ap.setVerticalRulerPreferenceValue(false);
-    // ap.setTextPreferenceValue(false);
-    // ap.setOverviewRulerPreferenceValue(false);
-    // ap.setColorPreferenceValue(new RGB(255, 0, 0));
-//     AnnotationPreference ap = new AnnotationPreference();
-//     AnnotationType type = new AnnotationType("new", null);
-//     AnnotationPreference ap = new AnnotationPreference();
-//     ap.setColorPreferenceKey(AnnotationPreference.STYLE_NONE);
-//     type.setAnnotationPreference(ap);
-//     "Annotation".setType(type);
+    AnnotationPreference prefMarking = EditorsUI.getAnnotationPreferenceLookup()
+        .getAnnotationPreference(MarkerFactory.ANNOTATION_MARKING);
+    AnnotationPreference prefMapping = EditorsUI.getAnnotationPreferenceLookup()
+        .getAnnotationPreference(MarkerFactory.ANNOTATION_MAPPING);
+    IPreferenceStore store = EditorsUI.getPreferenceStore();
+    IDecoratorManager decoratorManager =
+        Activator.getDefault().getWorkbench().getDecoratorManager();
+
+    if (!isHidden) {
+      isHidden = true;
+      store.setValue(prefMarking.getOverviewRulerPreferenceKey(), false);
+      store.setValue(prefMarking.getVerticalRulerPreferenceKey(), false);
+      store.setValue(prefMarking.getTextPreferenceKey(), false);
+      store.setValue(prefMapping.getOverviewRulerPreferenceKey(), false);
+      store.setValue(prefMapping.getVerticalRulerPreferenceKey(), false);
+      store.setValue(prefMapping.getTextPreferenceKey(), false);
+      try {
+        decoratorManager.setEnabled(MarkerFactory.ECORE_DECORATOR, false);
+        decoratorManager.setEnabled(MarkerFactory.FILE_DECORATOR, false);
+      } catch (CoreException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+
+    } else {
+      isHidden = false;
+      store.setValue(prefMarking.getOverviewRulerPreferenceKey(), true);
+      store.setValue(prefMarking.getVerticalRulerPreferenceKey(), true);
+      store.setValue(prefMarking.getTextPreferenceKey(), true);
+      store.setValue(prefMapping.getOverviewRulerPreferenceKey(), true);
+      store.setValue(prefMapping.getVerticalRulerPreferenceKey(), true);
+      store.setValue(prefMapping.getTextPreferenceKey(), true);
+      store.setValue(prefMarking.getColorPreferenceKey(), "0,0,255");
+      store.setValue(prefMapping.getColorPreferenceKey(), "255,0,0");
+
+      try {
+        decoratorManager.setEnabled(MarkerFactory.ECORE_DECORATOR, true);
+        decoratorManager.setEnabled(MarkerFactory.FILE_DECORATOR, true);
+      } catch (CoreException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
+    store.needsSaving();
 
     return null;
   }
-
 }
