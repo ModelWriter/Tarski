c66e369ed69db35ad25081face3574ed69bb73fc
Ümit Anıl Öztürk <anil.ozturk@unitbilisim.com>
2015-08-19 15:47:53 +0300

    Adding type to existing marker and mark with selected type commands have been provided for tree selection.

--- Source/eu.modelwriter.marker.command/src/eu/modelwriter/marker/command/AddTypeToMarkerHandler.java
+++ Source/eu.modelwriter.marker.command/src/eu/modelwriter/marker/command/AddTypeToMarkerHandler.java
@@ -7,9 +7,19 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EModelElement;
+import org.eclipse.emf.ecore.ENamedElement;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.presentation.EcoreEditor;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ITreeSelection;
+import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PlatformUI;
 
 import eu.modelwriter.marker.MarkerActivator;
@@ -27,8 +37,8 @@ public class AddTypeToMarkerHandler extends AbstractHandler {
 
     IFile file = (IFile) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
         .getActiveEditor().getEditorInput().getAdapter(IFile.class);
-    
-    if (selection instanceof ITextSelection){
+
+    if (selection instanceof ITextSelection) {
       ITextSelection textSelection = (ITextSelection) selection;
       ArrayList<IMarker> markerList = MarkerFactory.findMarkersInSelection(file, textSelection);
       if (markerList != null) {
@@ -43,7 +53,7 @@ public class AddTypeToMarkerHandler extends AbstractHandler {
             return null;
           selectedMarker = selectionWizard.getSelectedMarker();
         }
-        
+
         MarkerWizard markerWizard = new MarkerWizard(selectedMarker);
 
         WizardDialog dialog = new WizardDialog(MarkerActivator.getShell(), markerWizard);
@@ -53,11 +63,72 @@ public class AddTypeToMarkerHandler extends AbstractHandler {
         } else {
           System.out.println("Cancel pressed");
         }
-        
+
         MarkerUpdater.updateTargets(selectedMarker);
-        MarkerUpdater.updateSources(selectedMarker);      
+        MarkerUpdater.updateSources(selectedMarker);
+      }
+    } else if (selection instanceof TreeSelection) {
+      ITreeSelection treeSelection = (ITreeSelection) selection;
+      IEditorPart editor =
+          PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+      if (selection != null && editor instanceof EcoreEditor) {
+        if (treeSelection.getFirstElement() instanceof ENamedElement
+            && ((ENamedElement) treeSelection.getFirstElement()).getName() != null
+            && !((ENamedElement) treeSelection.getFirstElement()).getName().isEmpty()) {
+          URI uri = EcoreUtil.getURI((ENamedElement) treeSelection.getFirstElement());
+
+          IMarker selectedMarker;
+
+          selectedMarker = MarkerFactory.findMarkersByUri(file, uri.toString());
+
+          if (selectedMarker != null && selectedMarker.exists()) {
+            MarkerWizard markerWizard = new MarkerWizard(selectedMarker);
+
+            WizardDialog dialog = new WizardDialog(MarkerActivator.getShell(), markerWizard);
+
+            if (dialog.open() == org.eclipse.jface.window.Window.OK) {
+              System.out.println("Ok pressed");
+            } else {
+              System.out.println("Cancel pressed");
+            }
+
+            MarkerUpdater.updateTargets(selectedMarker);
+            MarkerUpdater.updateSources(selectedMarker);
+          } else {
+            MessageDialog dialog = new MessageDialog(MarkerActivator.getShell(),
+                "There is no marker in this position", null, "Please select valid marker",
+                MessageDialog.INFORMATION, new String[] {"OK"}, 0);
+            dialog.open();
+          }
+        } else if (!((EObject) treeSelection.getFirstElement() instanceof EModelElement)) {
+          URI uri = EcoreUtil.getURI((EObject) treeSelection.getFirstElement());
+
+          IMarker selectedMarker;
+          selectedMarker = MarkerFactory.findMarkersByUri(file, uri.toString());
+
+          if (selectedMarker != null && selectedMarker.exists()) {
+            MarkerWizard markerWizard = new MarkerWizard(selectedMarker);
+
+            WizardDialog dialog = new WizardDialog(MarkerActivator.getShell(), markerWizard);
+
+            if (dialog.open() == org.eclipse.jface.window.Window.OK) {
+              System.out.println("Ok pressed");
+            } else {
+              System.out.println("Cancel pressed");
+            }
+
+            MarkerUpdater.updateTargets(selectedMarker);
+            MarkerUpdater.updateSources(selectedMarker);
+          } else {
+            MessageDialog dialog = new MessageDialog(MarkerActivator.getShell(),
+                "There is no marker in this position", null, "Please select valid marker",
+                MessageDialog.INFORMATION, new String[] {"OK"}, 0);
+            dialog.open();
+          }
+        }
       }
     }
+
     return null;
   }
 }
--- Source/eu.modelwriter.marker.command/src/eu/modelwriter/marker/command/MarkWithTypeHandler.java
+++ Source/eu.modelwriter.marker.command/src/eu/modelwriter/marker/command/MarkWithTypeHandler.java
@@ -4,13 +4,25 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EModelElement;
+import org.eclipse.emf.ecore.ENamedElement;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.presentation.EcoreEditor;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ITreeSelection;
+import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PlatformUI;
 
 import eu.modelwriter.marker.MarkerActivator;
 import eu.modelwriter.marker.internal.MarkerFactory;
+import eu.modelwriter.marker.internal.MarkerUpdater;
 import eu.modelwriter.marker.ui.internal.wizards.markerwizard.MarkerPage;
 import eu.modelwriter.marker.ui.internal.wizards.markerwizard.MarkerWizard;
 
@@ -30,24 +42,85 @@ public class MarkWithTypeHandler extends AbstractHandler {
 
     ISelection selection = MarkerFactory.getSelection();
     IFile file = (IFile) MarkerActivator.getEditor().getEditorInput().getAdapter(IFile.class);
-    ITextSelection textSelection = (ITextSelection) selection;
-    if (selection instanceof ITextSelection && MarkerFactory.findMarkerWithAbsolutePosition(file,
-        textSelection.getOffset(), textSelection.getOffset() + textSelection.getLength()) != null) {
-      MessageDialog dialog = new MessageDialog(MarkerActivator.getShell(), "Mark Information", null,
-          "In these area, there is already a marker", MessageDialog.WARNING, new String[] {"OK"},
-          0);
-      dialog.open();
-      return null;
-    }
+    if (selection instanceof ITextSelection) {
+      ITextSelection textSelection = (ITextSelection) selection;
+      if (selection instanceof ITextSelection
+          && MarkerFactory.findMarkerWithAbsolutePosition(file, textSelection.getOffset(),
+              textSelection.getOffset() + textSelection.getLength()) != null) {
+        MessageDialog dialog = new MessageDialog(MarkerActivator.getShell(), "Mark Information",
+            null, "In these area, there is already a marker", MessageDialog.WARNING,
+            new String[] {"OK"}, 0);
+        dialog.open();
+        return null;
+      }
 
-    MarkerWizard markerWizard = new MarkerWizard(selection, file);
+      MarkerWizard markerWizard = new MarkerWizard(selection, file);
 
-    WizardDialog dialog = new WizardDialog(MarkerActivator.getShell(), markerWizard);
+      WizardDialog dialog = new WizardDialog(MarkerActivator.getShell(), markerWizard);
 
-    if (dialog.open() == org.eclipse.jface.window.Window.OK) {
-      System.out.println("Ok pressed");
-    } else {
-      System.out.println("Cancel pressed");
+      if (dialog.open() == org.eclipse.jface.window.Window.OK) {
+        System.out.println("Ok pressed");
+      } else {
+        System.out.println("Cancel pressed");
+      }
+    } else if (selection instanceof TreeSelection) {
+      ITreeSelection treeSelection = (ITreeSelection) selection;
+      IEditorPart editor =
+          PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+      if (selection != null && editor instanceof EcoreEditor) {
+        if (treeSelection.getFirstElement() instanceof ENamedElement
+            && ((ENamedElement) treeSelection.getFirstElement()).getName() != null
+            && !((ENamedElement) treeSelection.getFirstElement()).getName().isEmpty()) {
+          URI uri = EcoreUtil.getURI((ENamedElement) treeSelection.getFirstElement());
+
+          IMarker selectedMarker;
+
+          selectedMarker = MarkerFactory.findMarkersByUri(file, uri.toString());
+
+          if (selectedMarker != null && selectedMarker.exists()) {
+            MessageDialog dialog = new MessageDialog(MarkerActivator.getShell(), "Marker Info",
+                null, "There is already a marker in this position", MessageDialog.INFORMATION,
+                new String[] {"OK"}, 0);
+            dialog.open();
+          } else {
+            MarkerWizard markerWizard = new MarkerWizard(selection, file);
+
+            WizardDialog dialog = new WizardDialog(MarkerActivator.getShell(), markerWizard);
+
+            if (dialog.open() == org.eclipse.jface.window.Window.OK) {
+              System.out.println("Ok pressed");
+            } else {
+              System.out.println("Cancel pressed");
+            }
+
+          }
+        } else if (!((EObject) treeSelection.getFirstElement() instanceof EModelElement)) {
+          URI uri = EcoreUtil.getURI((EObject) treeSelection.getFirstElement());
+
+          IMarker selectedMarker;
+          selectedMarker = MarkerFactory.findMarkersByUri(file, uri.toString());
+
+          if (selectedMarker != null && selectedMarker.exists()) {
+            MarkerWizard markerWizard = new MarkerWizard(selectedMarker);
+
+            WizardDialog dialog = new WizardDialog(MarkerActivator.getShell(), markerWizard);
+
+            if (dialog.open() == org.eclipse.jface.window.Window.OK) {
+              System.out.println("Ok pressed");
+            } else {
+              System.out.println("Cancel pressed");
+            }
+
+            MarkerUpdater.updateTargets(selectedMarker);
+            MarkerUpdater.updateSources(selectedMarker);
+          } else {
+            MessageDialog dialog = new MessageDialog(MarkerActivator.getShell(),
+                "There is no marker in this position", null, "Please select valid marker",
+                MessageDialog.INFORMATION, new String[] {"OK"}, 0);
+            dialog.open();
+          }
+        }
+      }
     }
 
     return null;
