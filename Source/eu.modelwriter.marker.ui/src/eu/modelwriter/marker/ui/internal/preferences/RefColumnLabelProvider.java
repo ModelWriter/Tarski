e7ce2b2cdbffb7e1aab013390e4bd5c288e7122d
Serhat Çelik <serhat.celik@unitbilisim.com>
2015-08-25 16:36:02 +0300

    Relation and Sig icons have been added

--- Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/preferences/RefColumnLabelProvider.java
+++ Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/preferences/RefColumnLabelProvider.java
@@ -1,12 +1,22 @@
 package eu.modelwriter.marker.ui.internal.preferences;
 
+import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.TableColumn;
 
+import eu.modelwriter.marker.MarkerActivator;
+
 public abstract class RefColumnLabelProvider extends ColumnLabelProvider {
+
+  @Override
+  public Image getImage(Object element) {
+    ImageRegistry imageRegistry = MarkerActivator.getDefault().getImageRegistry();
+    return imageRegistry.get(MarkerActivator.Images.IMAGE_RELATION_ID);
+  }
 
   public abstract String getText(Object element);
 
