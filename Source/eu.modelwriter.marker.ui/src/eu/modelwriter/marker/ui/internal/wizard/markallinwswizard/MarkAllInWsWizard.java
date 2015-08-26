be7bee056b86b864601edcefbd12ac08563f8f39
Emre Kirmizi <emre.kirmizi@unitbilisim.com>
2015-08-26 15:04:16 +0300

    Progress Monitor Wizard has been added when marking in all workspace.

--- Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/wizard/markallinwswizard/MarkAllInWsWizard.java
+++ Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/wizard/markallinwswizard/MarkAllInWsWizard.java
@@ -6,12 +6,15 @@ import java.util.UUID;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.text.TextSelection;
 import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.ide.IDE;
 
@@ -52,64 +55,82 @@ public class MarkAllInWsWizard extends Wizard {
     String text = textSelection.getText();
     String leader_id = UUID.randomUUID().toString();
     boolean success = false;
-    for (Object object : checkedElements) {
-      try {
-        if (object instanceof IFile) {
-          IFile iFile = (IFile) object;
-          if (!iFile.getFileExtension().equals("xmi") && !iFile.getFileExtension().equals("ecore")
-              && !iFile.getFileExtension().equals("reqif")) {
-            String charSet = iFile.getCharset();
-            Scanner scanner = new Scanner(iFile.getContents(), charSet);
-            IDocument document = new Document(scanner.useDelimiter("\\A").next());
-            scanner.close();
 
-            String fullText = document.get();
-            boolean hasLeader = false;
-            int index = 0;
-            int offset = 0;
-            int lenght = textSelection.getLength();
-            String id = UUID.randomUUID().toString();
+    Image image = MarkerActivator.getDefault().getImageRegistry()
+        .get(MarkerActivator.Images.IMAGE_MODELWRITER_ID);
+    ProgressMonitorDialog pmd = new ProgressMonitorDialog(
+        Activator.getDefault().getWorkbench().getActiveWorkbenchWindow().getShell());
+    ProgressMonitorDialog.setDefaultImage(image);
+    pmd.open();
+    IProgressMonitor pmdmoni = pmd.getProgressMonitor();
+    try {
+      pmdmoni.beginTask("Marking", checkedElements.length);
+      for (int i = 0; i < checkedElements.length && !pmdmoni.isCanceled(); i++) {
+        try {
+          if (checkedElements[i] instanceof IFile) {
+            IFile iFile = (IFile) checkedElements[i];
+            pmdmoni.subTask(iFile.getName());
+            pmdmoni.worked(1);
 
-            IEditorPart editor =
-                IDE.openEditor(Activator.getActiveWorkbenchWindow().getActivePage(), iFile);
+            if (!iFile.getFileExtension().equals("xmi") && !iFile.getFileExtension().equals("ecore")
+                && !iFile.getFileExtension().equals("reqif")) {
+              String charSet = iFile.getCharset();
+              Scanner scanner = new Scanner(iFile.getContents(), charSet);
+              IDocument document = new Document(scanner.useDelimiter("\\A").next());
+              scanner.close();
 
-            if (lenght != 0) {
-              while ((offset = fullText.indexOf(text, index)) != -1) {
-                TextSelection nextSelection = new TextSelection(document, offset, lenght);
-                if (MarkerFactory.findMarkerByOffset(iFile, offset) == null) {
-                  IMarker mymarker = MarkerFactory.createMarker(iFile, nextSelection);
-                  MarkElementUtilities.setGroupId(mymarker, id);
-                  if (!iFile.equals(this.file)) {
-                    if (hasLeader == false) {
-                      MarkElementUtilities.setLeaderId(mymarker, leader_id);
-                      hasLeader = true;
+              String fullText = document.get();
+              boolean hasLeader = false;
+              int index = 0;
+              int offset = 0;
+              int lenght = textSelection.getLength();
+              String id = UUID.randomUUID().toString();
+
+              IEditorPart editor =
+                  IDE.openEditor(Activator.getActiveWorkbenchWindow().getActivePage(), iFile);
+
+              if (lenght != 0) {
+                while ((offset = fullText.indexOf(text, index)) != -1) {
+                  TextSelection nextSelection = new TextSelection(document, offset, lenght);
+                  if (MarkerFactory.findMarkerByOffset(iFile, offset) == null) {
+                    IMarker mymarker = MarkerFactory.createMarker(iFile, nextSelection);
+                    MarkElementUtilities.setGroupId(mymarker, id);
+                    if (!iFile.equals(this.file)) {
+                      if (hasLeader == false) {
+                        MarkElementUtilities.setLeaderId(mymarker, leader_id);
+                        hasLeader = true;
+                      }
+                    } else {
+                      if (textSelection.getOffset() == offset) {
+                        MarkElementUtilities.setLeaderId(mymarker, leader_id);
+                      }
                     }
-                  } else {
-                    if (textSelection.getOffset() == offset) {
-                      MarkElementUtilities.setLeaderId(mymarker, leader_id);
-                    }
+
+                    AnnotationFactory.addAnnotation(mymarker, editor,
+                        AnnotationFactory.ANNOTATION_MARKING);
                   }
-
-                  AnnotationFactory.addAnnotation(mymarker, editor,
-                      AnnotationFactory.ANNOTATION_MARKING);
+                  index = offset + lenght;
                 }
-                index = offset + lenght;
               }
+              success = true;
+            } else {
+              MessageDialog dialog =
+                  new MessageDialog(MarkerActivator.getShell(), "Mark All In Workspace", null,
+                      iFile.getName() + " doesn't supported for this command.",
+                      MessageDialog.INFORMATION, new String[] {"OK"}, 0);
+              dialog.open();
+              page.checkboxTreeViewer.setChecked(iFile, false);
             }
-            success = true;
-          } else {
-            MessageDialog dialog =
-                new MessageDialog(MarkerActivator.getShell(), "Mark All In Workspace", null,
-                    iFile.getName() + " doesn't supported for this command.",
-                    MessageDialog.INFORMATION, new String[] {"OK"}, 0);
-            dialog.open();
-            page.checkboxTreeViewer.setChecked(iFile, false);
           }
+        } catch (CoreException e) {
+          e.printStackTrace();
         }
-      } catch (CoreException e) {
-        e.printStackTrace();
       }
+    } finally {
+      pmdmoni.done();
     }
+    pmd.close();
+
     if (success == true) {
       MessageDialog dialog = new MessageDialog(MarkerActivator.getShell(), "Mark All In Workspace",
           null, "Selected project(s) have been marked by selected text", MessageDialog.INFORMATION,
