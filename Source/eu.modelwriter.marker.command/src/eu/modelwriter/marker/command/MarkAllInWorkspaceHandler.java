7d9be1b306d5e91c5649d5ec46b84422bbac0022
Ümit Anıl Öztürk <anil.ozturk@unitbilisim.com>
2015-09-04 14:47:52 +0300

    Mark All In Workspace Handler class has been updated to code re-factoring.

--- Source/eu.modelwriter.marker.command/src/eu/modelwriter/marker/command/MarkAllInWorkspaceHandler.java
+++ Source/eu.modelwriter.marker.command/src/eu/modelwriter/marker/command/MarkAllInWorkspaceHandler.java
@@ -1,15 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2015 UNIT Information Technologies R&D Ltd
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * Copyright (c) 2015 UNIT Information Technologies R&D Ltd All rights reserved. This program and
+ * the accompanying materials are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *     Ferhat Erata - initial API and implementation
- *     H. Emre Kirmizi - initial API and implementation
- *     Serhat Celik - initial API and implementation
- *     U. Anil Ozturk - initial API and implementation
+ * Contributors: Ferhat Erata - initial API and implementation H. Emre Kirmizi - initial API and
+ * implementation Serhat Celik - initial API and implementation U. Anil Ozturk - initial API and
+ * implementation
  *******************************************************************************/
 package eu.modelwriter.marker.command;
 
@@ -22,38 +19,45 @@ import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.window.Window;
 import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.PlatformUI;
 
 import eu.modelwriter.marker.MarkerActivator;
 import eu.modelwriter.marker.internal.MarkerFactory;
 import eu.modelwriter.marker.ui.internal.wizard.markallinwswizard.MarkAllInWsWizard;
 
 public class MarkAllInWorkspaceHandler extends AbstractHandler {
+  IFile file;
+  ISelection selection;
+
+  private void createMarkersWs() {
+    ITextSelection textSelection = (ITextSelection) this.selection;
+    if (MarkerFactory.findMarkerWithAbsolutePosition(this.file, textSelection.getOffset(),
+        textSelection.getOffset() + textSelection.getLength()) != null) {
+      MessageDialog dialog = new MessageDialog(MarkerActivator.getShell(), "Mark Information", null,
+          "In these area, there is already a marker", MessageDialog.WARNING, new String[] {"OK"},
+          0);
+      dialog.open();
+    } else {
+      MarkAllInWsWizard markAllWsWizard = new MarkAllInWsWizard(textSelection, this.file);
+      WizardDialog selectionDialog = new WizardDialog(MarkerActivator.getShell(), markAllWsWizard);
+      if (selectionDialog.open() == Window.CANCEL) {
+      }
+    }
+  }
 
   @Override
   public Object execute(ExecutionEvent event) throws ExecutionException {
-    ISelection selection = Activator.getDefault().getWorkbench().getActiveWorkbenchWindow()
-        .getSelectionService().getSelection();
-    IFile file = (IFile) MarkerActivator.getEditor().getEditorInput().getAdapter(IFile.class);
-    if (selection instanceof ITextSelection) {
-      ITextSelection textSelection = (ITextSelection) selection;
-      if (MarkerFactory.findMarkerWithAbsolutePosition(file, textSelection.getOffset(),
-          textSelection.getOffset() + textSelection.getLength()) != null) {
+    this.file = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
+        .getActiveEditor().getEditorInput().getAdapter(IFile.class);
+    this.selection =
+        PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection();
 
-        MessageDialog dialog = new MessageDialog(MarkerActivator.getShell(), "Mark Information",
-            null, "In these area, there is already a marker", MessageDialog.WARNING,
-            new String[] {"OK"}, 0);
-        dialog.open();
-
-        return null;
-      } else {
-        MarkAllInWsWizard markAllWsWizard = new MarkAllInWsWizard(textSelection, file);
-        WizardDialog selectionDialog =
-            new WizardDialog(MarkerActivator.getShell(), markAllWsWizard);
-        if (selectionDialog.open() == Window.CANCEL) {
-          return null;
-        }
-      }
-    }
+    this.createMarkersWs();
+    this.refresh();
     return null;
+  }
+
+  private void refresh() {
+    MarkerFactory.refreshProjectExp();
   }
 }
