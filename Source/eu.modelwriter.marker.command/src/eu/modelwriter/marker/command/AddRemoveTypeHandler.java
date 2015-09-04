ddfc31faca0cc3ded93de57713c054d167f46dec
Ümit Anıl Öztürk <anil.ozturk@unitbilisim.com>
2015-09-04 14:16:03 +0300

    Add/Remove Type Handler class has been updated to code re-factoring.

--- Source/eu.modelwriter.marker.command/src/eu/modelwriter/marker/command/AddRemoveTypeHandler.java
+++ Source/eu.modelwriter.marker.command/src/eu/modelwriter/marker/command/AddRemoveTypeHandler.java
@@ -1,15 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2015 UNIT Information Technologies R&D Ltd
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * Copyright (c) 2015 UNIT Information Technologies R&D Ltd All rights reserved. This program and
+ * the accompanying materials are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *     Ferhat Erata - initial API and implementation
- *     H. Emre Kirmizi - initial API and implementation
- *     Serhat Celik - initial API and implementation
- *     U. Anil Ozturk - initial API and implementation
+ * Contributors: Ferhat Erata - initial API and implementation H. Emre Kirmizi - initial API and
+ * implementation Serhat Celik - initial API and implementation U. Anil Ozturk - initial API and
+ * implementation
  *******************************************************************************/
 package eu.modelwriter.marker.command;
 
@@ -31,7 +28,6 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ITreeSelection;
-import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PlatformUI;
@@ -40,23 +36,27 @@ import eu.modelwriter.marker.MarkerActivator;
 import eu.modelwriter.marker.internal.MarkElementUtilities;
 import eu.modelwriter.marker.internal.MarkerFactory;
 import eu.modelwriter.marker.internal.MarkerUpdater;
-import eu.modelwriter.marker.typing.internal.AlloyUtilities;
 import eu.modelwriter.marker.ui.internal.wizards.markerwizard.MarkerPage;
 import eu.modelwriter.marker.ui.internal.wizards.markerwizard.MarkerWizard;
 import eu.modelwriter.marker.ui.internal.wizards.selectionwizard.SelectionWizard;
 
 public class AddRemoveTypeHandler extends AbstractHandler {
+  IFile file;
+  ISelection selection;
 
-  @Override
-  public Object execute(ExecutionEvent event) throws ExecutionException {
+  private void addRemoveType() {
+    this.file = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
+        .getActiveEditor().getEditorInput().getAdapter(IFile.class);
+    this.selection =
+        PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection();
 
     if (!MarkerPage.isParsed()) {
-      MessageDialog dialog = new MessageDialog(MarkerActivator.getShell(), "Type Information", null,
-          "You dont have any marker type registered to system! \n"
-              + "Please parse an alloy file first",
-          MessageDialog.INFORMATION, new String[] {"OK"}, 0);
-      dialog.open();
-      return null;
+      MessageDialog parseCtrlDialog =
+          new MessageDialog(MarkerActivator.getShell(), "Type Information", null,
+              "You dont have any marker type registered to system! \n"
+                  + "Please parse an alloy file first",
+              MessageDialog.INFORMATION, new String[] {"OK"}, 0);
+      parseCtrlDialog.open();
     }
 
     MessageDialog actionSelectionDialog = new MessageDialog(MarkerActivator.getShell(),
@@ -64,158 +64,99 @@ public class AddRemoveTypeHandler extends AbstractHandler {
         new String[] {"Add Type", "Remove Current Type"}, 0);
     actionSelectionDialog.open();
 
-    ISelection selection =
-        PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection();
+    IMarker selectedMarker = this.getMarker();
 
-    IFile file = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
-        .getActiveEditor().getEditorInput().getAdapter(IFile.class);
+    if ((selectedMarker != null) && selectedMarker.exists()) {
+      if (actionSelectionDialog.getReturnCode() == 0) {
+        this.addType(selectedMarker);
+      } else {
+        this.removeType(selectedMarker);
+      }
+    } else {
+      MessageDialog dialog =
+          new MessageDialog(MarkerActivator.getShell(), "There is no marker in this position", null,
+              "Please select valid marker", MessageDialog.INFORMATION, new String[] {"OK"}, 0);
+      dialog.open();
+      return;
+    }
 
-    if (selection instanceof ITextSelection) {
-      ITextSelection textSelection = (ITextSelection) selection;
-      ArrayList<IMarker> markerList = MarkerFactory.findMarkersInSelection(file, textSelection);
+    MarkerUpdater.updateTargets(selectedMarker);
+    MarkerUpdater.updateSources(selectedMarker);
+  }
+
+  private void addType(IMarker selectedMarker) {
+    MarkerWizard markerWizard = new MarkerWizard(selectedMarker);
+
+    WizardDialog dialog = new WizardDialog(MarkerActivator.getShell(), markerWizard);
+
+    if (dialog.open() == org.eclipse.jface.window.Window.OK) {
+      System.out.println("Ok pressed");
+    } else {
+      System.out.println("Cancel pressed");
+    }
+  }
+
+  @Override
+  public Object execute(ExecutionEvent event) throws ExecutionException {
+    this.addRemoveType();
+    return null;
+
+  }
+
+  private IMarker getMarker() {
+    IMarker selectedMarker = null;
+    if (this.selection instanceof ITextSelection) {
+      ITextSelection textSelection = (ITextSelection) this.selection;
+
+      ArrayList<IMarker> markerList =
+          MarkerFactory.findMarkersInSelection(this.file, textSelection);
       if (markerList != null) {
-        IMarker selectedMarker = null;
         if (markerList.size() == 1) {
           selectedMarker = markerList.get(0);
         } else if (markerList.size() > 1) {
           SelectionWizard selectionWizard = new SelectionWizard(markerList);
           WizardDialog selectionDialog =
               new WizardDialog(MarkerActivator.getShell(), selectionWizard);
-          if (selectionDialog.open() == 1)
+          if (selectionDialog.open() == 1) {
             return null;
+          }
           selectedMarker = selectionWizard.getSelectedMarker();
         }
-        if (actionSelectionDialog.getReturnCode() == 0) {
-          MarkerWizard markerWizard = new MarkerWizard(selectedMarker);
-
-          WizardDialog dialog = new WizardDialog(MarkerActivator.getShell(), markerWizard);
-
-          if (dialog.open() == org.eclipse.jface.window.Window.OK) {
-            System.out.println("Ok pressed");
-          } else {
-            System.out.println("Cancel pressed");
-          }
-        } else {
-          if (MarkElementUtilities.getGroupId(selectedMarker) != null) {
-            List<IMarker> group = MarkerFactory.findMarkersByGroupId(selectedMarker.getResource(),
-                MarkElementUtilities.getGroupId(selectedMarker));
-            for (IMarker iMarker : group) {
-              AlloyUtilities.removeTypeFromMarker(iMarker);
-              MarkElementUtilities.setType(iMarker, null);
-            }
-          } else {
-            AlloyUtilities.removeTypeFromMarker(selectedMarker);
-            MarkElementUtilities.setType(selectedMarker, null);
-          }
-
-          MessageDialog removeSuccessDialog = new MessageDialog(MarkerActivator.getShell(),
-              "Removing Type Action", null, "Selected marker's type has been removed.",
-              MessageDialog.INFORMATION, new String[] {"OK"}, 0);
-          removeSuccessDialog.open();
-        }
-        MarkerUpdater.updateTargets(selectedMarker);
-        MarkerUpdater.updateSources(selectedMarker);
       }
-    } else if (selection instanceof TreeSelection) {
-      ITreeSelection treeSelection = (ITreeSelection) selection;
+    } else if (this.selection instanceof ITreeSelection) {
       IEditorPart editor =
           PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
-      if (selection != null && editor instanceof EcoreEditor) {
-        if (treeSelection.getFirstElement() instanceof ENamedElement
-            && ((ENamedElement) treeSelection.getFirstElement()).getName() != null
+      ITreeSelection treeSelection = (ITreeSelection) this.selection;
+      if ((this.selection != null) && (editor instanceof EcoreEditor)) {
+        if ((treeSelection.getFirstElement() instanceof ENamedElement)
+            && (((ENamedElement) treeSelection.getFirstElement()).getName() != null)
             && !((ENamedElement) treeSelection.getFirstElement()).getName().isEmpty()) {
+
           URI uri = EcoreUtil.getURI((ENamedElement) treeSelection.getFirstElement());
 
-          IMarker selectedMarker;
-
-          selectedMarker = MarkerFactory.findMarkersByUri(file, uri.toString());
-
-          if (selectedMarker != null && selectedMarker.exists()) {
-            if (actionSelectionDialog.getReturnCode() == 0) {
-              MarkerWizard markerWizard = new MarkerWizard(selectedMarker);
-
-              WizardDialog dialog = new WizardDialog(MarkerActivator.getShell(), markerWizard);
-
-              if (dialog.open() == org.eclipse.jface.window.Window.OK) {
-                System.out.println("Ok pressed");
-              } else {
-                System.out.println("Cancel pressed");
-              }
-            } else {
-              if (MarkElementUtilities.getGroupId(selectedMarker) != null) {
-                List<IMarker> group = MarkerFactory.findMarkersByGroupId(
-                    selectedMarker.getResource(), MarkElementUtilities.getGroupId(selectedMarker));
-                for (IMarker iMarker : group) {
-                  AlloyUtilities.removeTypeFromMarker(iMarker);
-                  MarkElementUtilities.setType(iMarker, null);
-                }
-              } else {
-                AlloyUtilities.removeTypeFromMarker(selectedMarker);
-                MarkElementUtilities.setType(selectedMarker, null);
-              }
-
-              MessageDialog removeSuccessDialog = new MessageDialog(MarkerActivator.getShell(),
-                  "Removing Type Action", null, "Selected marker's type has been removed.",
-                  MessageDialog.INFORMATION, new String[] {"OK"}, 0);
-              removeSuccessDialog.open();
-            }
-
-            MarkerUpdater.updateTargets(selectedMarker);
-            MarkerUpdater.updateSources(selectedMarker);
-          } else {
-            MessageDialog dialog = new MessageDialog(MarkerActivator.getShell(),
-                "There is no marker in this position", null, "Please select valid marker",
-                MessageDialog.INFORMATION, new String[] {"OK"}, 0);
-            dialog.open();
-          }
+          selectedMarker = MarkerFactory.findMarkersByUri(this.file, uri.toString());
         } else if (!((EObject) treeSelection.getFirstElement() instanceof EModelElement)) {
           URI uri = EcoreUtil.getURI((EObject) treeSelection.getFirstElement());
-
-          IMarker selectedMarker;
-          selectedMarker = MarkerFactory.findMarkersByUri(file, uri.toString());
-
-          if (selectedMarker != null && selectedMarker.exists()) {
-            if (actionSelectionDialog.getReturnCode() == 0) {
-              MarkerWizard markerWizard = new MarkerWizard(selectedMarker);
-
-              WizardDialog dialog = new WizardDialog(MarkerActivator.getShell(), markerWizard);
-
-              if (dialog.open() == org.eclipse.jface.window.Window.OK) {
-                System.out.println("Ok pressed");
-              } else {
-                System.out.println("Cancel pressed");
-              }
-            } else {
-              if (MarkElementUtilities.getGroupId(selectedMarker) != null) {
-                List<IMarker> group = MarkerFactory.findMarkersByGroupId(
-                    selectedMarker.getResource(), MarkElementUtilities.getGroupId(selectedMarker));
-                for (IMarker iMarker : group) {
-                  AlloyUtilities.removeTypeFromMarker(iMarker);
-                  MarkElementUtilities.setType(iMarker, null);
-                }
-              } else {
-                AlloyUtilities.removeTypeFromMarker(selectedMarker);
-                MarkElementUtilities.setType(selectedMarker, null);
-              }
-
-              MessageDialog removeSuccessDialog = new MessageDialog(MarkerActivator.getShell(),
-                  "Removing Type Action", null, "Selected marker's type has been removed.",
-                  MessageDialog.INFORMATION, new String[] {"OK"}, 0);
-              removeSuccessDialog.open();
-            }
-
-            MarkerUpdater.updateTargets(selectedMarker);
-            MarkerUpdater.updateSources(selectedMarker);
-          } else {
-            MessageDialog dialog = new MessageDialog(MarkerActivator.getShell(),
-                "There is no marker in this position", null, "Please select valid marker",
-                MessageDialog.INFORMATION, new String[] {"OK"}, 0);
-            dialog.open();
-          }
+          selectedMarker = MarkerFactory.findMarkersByUri(this.file, uri.toString());
         }
       }
     }
+    return selectedMarker;
+  }
 
-    return null;
+  private void removeType(IMarker selectedMarker) {
+    if (MarkElementUtilities.getGroupId(selectedMarker) != null) {
+      List<IMarker> group = MarkerFactory.findMarkersByGroupId(selectedMarker.getResource(),
+          MarkElementUtilities.getGroupId(selectedMarker));
+      for (IMarker iMarker : group) {
+        MarkElementUtilities.setType(iMarker, null);
+      }
+    } else {
+      MarkElementUtilities.setType(selectedMarker, null);
+    }
+    MessageDialog removeSuccessDialog = new MessageDialog(MarkerActivator.getShell(),
+        "Removing Type Action", null, "Selected marker's type has been removed.",
+        MessageDialog.INFORMATION, new String[] {"OK"}, 0);
+    removeSuccessDialog.open();
   }
 }
