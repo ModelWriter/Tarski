b2a9c6214bcf530fb5a70bc4a04e1234d87e8e4f
Ümit Anıl Öztürk <anil.ozturk@unitbilisim.com>
2015-09-04 14:52:29 +0300

    Mark Handler class has been updated to code re-factoring.

--- Source/eu.modelwriter.marker.command/src/eu/modelwriter/marker/command/MarkHandler.java
+++ Source/eu.modelwriter.marker.command/src/eu/modelwriter/marker/command/MarkHandler.java
@@ -1,15 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2015 UNIT Information Technologies R&D Ltd
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * Copyright (c) 2015 UNIT Information Technologies R&D Ltd All rights reserved. This program and
+ * the accompanying materials are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *     Ferhat Erata - initial API and implementation
- *     H. Emre Kirmizi - initial API and implementation
- *     Serhat Celik - initial API and implementation
- *     U. Anil Ozturk - initial API and implementation
+ * Contributors: Ferhat Erata - initial API and implementation H. Emre Kirmizi - initial API and
+ * implementation Serhat Celik - initial API and implementation U. Anil Ozturk - initial API and
+ * implementation
  *******************************************************************************/
 package eu.modelwriter.marker.command;
 
@@ -34,52 +31,65 @@ import eu.modelwriter.marker.internal.MarkElementUtilities;
 import eu.modelwriter.marker.internal.MarkerFactory;
 
 public class MarkHandler extends AbstractHandler {
+  IEditorPart editor;
+  IFile file;
+  ISelection selection;
 
-  @Override
-  public Object execute(ExecutionEvent event) throws ExecutionException {
-    ISelection selection =
+  private void createMarker() {
+    this.editor =
+        PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+    this.file = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
+        .getActiveEditor().getEditorInput().getAdapter(IFile.class);
+    this.selection =
         PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection();
 
-    IFile file = (IFile) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
-        .getActiveEditor().getEditorInput().getAdapter(IFile.class);
-
-    IEditorPart editor =
-        PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
-
-    IMarker myMarker = null;
-
-    if (selection instanceof ITextSelection) {
-
-      myMarker = MarkerFactory.createMarker(file, (ITextSelection) selection);
-
-      if (myMarker != null && myMarker.exists()) {
-        AnnotationFactory.addAnnotation(myMarker, editor, AnnotationFactory.ANNOTATION_MARKING);
-        MessageDialog dialog =
-            new MessageDialog(MarkerActivator.getShell(), "Mark Information", null,
-                "\"" + ((ITextSelection) selection).getText()
-                    + "\" has been seleceted to be marked",
-                MessageDialog.INFORMATION, new String[] {"OK"}, 0);
-        dialog.open();
+    IMarker beAdded = this.getMarker();
+    String message = "";
+    if (this.selection instanceof ITextSelection) {
+      if ((beAdded != null) && beAdded.exists()) {
+        message = ((ITextSelection) this.selection).getText();
+        AnnotationFactory.addAnnotation(beAdded, this.editor, AnnotationFactory.ANNOTATION_MARKING);
       }
-    } else if (selection instanceof ITreeSelection) {
-      if (editor instanceof EcoreEditor) {
-        ITreeSelection treeSelection = (ITreeSelection) selection;
-        myMarker = MarkerFactory.createMarker(file, treeSelection);
-
-        if (myMarker != null && myMarker.exists()) {
-          String message = null;
-          if (treeSelection.getFirstElement() instanceof EModelElement)
+    } else if (this.selection instanceof ITreeSelection) {
+      if (this.editor instanceof EcoreEditor) {
+        ITreeSelection treeSelection = (ITreeSelection) this.selection;
+        if ((beAdded != null) && beAdded.exists()) {
+          if (treeSelection.getFirstElement() instanceof EModelElement) {
             message = ((ENamedElement) treeSelection.getFirstElement()).getName();
-          else
-            message = MarkElementUtilities.getMessage(myMarker);
-          MessageDialog dialog = new MessageDialog(MarkerActivator.getShell(), "Mark Information",
-              null, "\"" + message + "\" has been seleceted to be marked",
-              MessageDialog.INFORMATION, new String[] {"OK"}, 0);
-          dialog.open();
+          } else {
+            message = MarkElementUtilities.getMessage(beAdded);
+          }
         }
       }
     }
-    MarkerFactory.refreshProjectExp();
+
+    MessageDialog dialog = new MessageDialog(MarkerActivator.getShell(), "Mark Information", null,
+        "\"" + message + "\" has been selected to be marked", MessageDialog.INFORMATION,
+        new String[] {"OK"}, 0);
+    dialog.open();
+  }
+
+  @Override
+  public Object execute(ExecutionEvent event) throws ExecutionException {
+    this.createMarker();
+    this.refresh();
     return null;
+  }
+
+  private IMarker getMarker() {
+    IMarker beAdded = null;
+    if (this.selection instanceof ITextSelection) {
+      beAdded = MarkerFactory.createMarker(this.file, (ITextSelection) this.selection);
+    } else if (this.selection instanceof ITreeSelection) {
+      if (this.editor instanceof EcoreEditor) {
+        ITreeSelection treeSelection = (ITreeSelection) this.selection;
+        beAdded = MarkerFactory.createMarker(this.file, treeSelection);
+      }
+    }
+    return beAdded;
+  }
+
+  private void refresh() {
+    MarkerFactory.refreshProjectExp();
   }
 }
