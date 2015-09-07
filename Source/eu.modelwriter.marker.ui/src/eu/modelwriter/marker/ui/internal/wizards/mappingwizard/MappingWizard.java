8b622718871cec61c8bf49cdba1cc528cad7a846
Serhat Çelik <serhat.celik@unitbilisim.com>
2015-09-07 11:48:38 +0300

    Relation adding to file has been provided.

--- Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/wizards/mappingwizard/MappingWizard.java
+++ Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/wizards/mappingwizard/MappingWizard.java
@@ -1,19 +1,17 @@
 /*******************************************************************************
- * Copyright (c) 2015 UNIT Information Technologies R&D Ltd
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * Copyright (c) 2015 UNIT Information Technologies R&D Ltd All rights reserved. This program and
+ * the accompanying materials are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *     Ferhat Erata - initial API and implementation
- *     H. Emre Kirmizi - initial API and implementation
- *     Serhat Celik - initial API and implementation
- *     U. Anil Ozturk - initial API and implementation
+ * Contributors: Ferhat Erata - initial API and implementation H. Emre Kirmizi - initial API and
+ * implementation Serhat Celik - initial API and implementation U. Anil Ozturk - initial API and
+ * implementation
  *******************************************************************************/
 package eu.modelwriter.marker.ui.internal.wizards.mappingwizard;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -41,6 +39,7 @@ public class MappingWizard extends Wizard {
   public static IMarker selectedMarker;
   public ArrayList<MarkElement> beforeCheckedElements;
   public ArrayList<MarkElement> checkedElements;
+  public static Map<IMarker, String> relationMap;
 
   public MappingWizard(IMarker selectedMarker) {
     super();
@@ -50,6 +49,7 @@ public class MappingWizard extends Wizard {
     setNeedsProgressMonitor(true);
     beforeCheckedElements = MarkElementUtilities.getTargetList(selectedMarker);
     checkedElements = new ArrayList<MarkElement>();
+    relationMap = new HashMap<IMarker, String>();
 
   }
 
@@ -69,189 +69,196 @@ public class MappingWizard extends Wizard {
 
   @Override
   public boolean performFinish() {
-    try {
-      Object[] checkedObjects = MarkerMatchPage.markTreeViewer.getCheckedElements();
-      ArrayList<MarkElement> listOfSome = MarkerMatchPage.checkedElements;
-
-      for (int i = 0; i < checkedObjects.length; i++) {
-        if (checkedObjects[i] instanceof IMarker) {
-          checkedElements.add(new MarkElement((IMarker) checkedObjects[i]));
-          AlloyUtilities.addRelation2Markers(selectedMarker, (IMarker) checkedObjects[i], "refs");
-        }
-      }
-      targetElementsOfSelected = MarkElementUtilities.getTargetList(selectedMarker);
-
-      for (MarkElement checkedElement : checkedElements) {
-        boolean flag = false;
-        @SuppressWarnings("rawtypes")
-        Iterator iter = beforeCheckedElements.iterator();
-        while (iter.hasNext()) {
-          MarkElement markElement = (MarkElement) iter.next();
-          if (MarkElementUtilities.compare(markElement.getiMarker(), checkedElement.getiMarker())) {
-            flag = true;
-            break;
-          }
-        }
-        if (flag == false) {
-          if (MarkElementUtilities.getLeaderId(checkedElement.getiMarker()) != null) {
-            List<IMarker> groupMarkersOfCheckedElement =
-                MarkerFactory.findMarkersByGroupId(checkedElement.getiMarker().getResource(),
-                    MarkElementUtilities.getGroupId(checkedElement.getiMarker()));
-
-            Iterator<IMarker> groupMarkersIteratorOfCheckedElement =
-                groupMarkersOfCheckedElement.iterator();
-
-            while (groupMarkersIteratorOfCheckedElement.hasNext()) {
-              IMarker nextCheckedGroupElement =
-                  (IMarker) groupMarkersIteratorOfCheckedElement.next();
-              ArrayList<MarkElement> sourcesOfGroupElement =
-                  MarkElementUtilities.getSourceList(nextCheckedGroupElement);
-              sourcesOfGroupElement.add(new MarkElement(selectedMarker));
-              MarkElementUtilities.setSourceList(nextCheckedGroupElement, sourcesOfGroupElement);
-            }
-          } else {
-            ArrayList<MarkElement> sourceElementsOfChecked =
-                MarkElementUtilities.getSourceList(checkedElement.getiMarker());
-            sourceElementsOfChecked.add(new MarkElement(selectedMarker));
-            MarkElementUtilities.setSourceList(checkedElement.getiMarker(),
-                sourceElementsOfChecked);
-          }
-
-          if (MarkElementUtilities.getLeaderId(selectedMarker) != null) {
-            List<IMarker> groupMarkersOfSelectedElement = MarkerFactory.findMarkersByGroupId(
-                selectedMarker.getResource(), MarkElementUtilities.getGroupId(selectedMarker));
-
-            Iterator<IMarker> groupMarkersIteratorOfSelectedElement =
-                groupMarkersOfSelectedElement.iterator();
-
-            while (groupMarkersIteratorOfSelectedElement.hasNext()) {
-              IMarker nextSelectedGroupElement =
-                  (IMarker) groupMarkersIteratorOfSelectedElement.next();
-
-              ArrayList<MarkElement> targetElementsOfNextSelected =
-                  MarkElementUtilities.getTargetList(nextSelectedGroupElement);
-
-              targetElementsOfNextSelected.add(checkedElement);
-              MarkElementUtilities.setTargetList(nextSelectedGroupElement,
-                  targetElementsOfNextSelected);
-            }
-          } else {
-            targetElementsOfSelected.add(checkedElement);
-            MarkElementUtilities.setTargetList(selectedMarker, targetElementsOfSelected);
-          }
-        }
-      }
-
-      for (MarkElement beforeCheckedElement : beforeCheckedElements) {
-        if (listOfSome.contains(beforeCheckedElement)) {
-          continue;
-        } else {
-          if (MarkElementUtilities.getLeaderId(beforeCheckedElement.getiMarker()) != null) {
-
-            List<IMarker> groupMarkersOfBeforeCheckedElement =
-                MarkerFactory.findMarkersByGroupId(beforeCheckedElement.getiMarker().getResource(),
-                    MarkElementUtilities.getGroupId(beforeCheckedElement.getiMarker()));
-
-            Iterator<IMarker> groupMarkersIteratorOfBeforeCheckedElement =
-                groupMarkersOfBeforeCheckedElement.iterator();
-
-            while (groupMarkersIteratorOfBeforeCheckedElement.hasNext()) {
-              IMarker nextBeforeCheckedGroupElement =
-                  (IMarker) groupMarkersIteratorOfBeforeCheckedElement.next();
-              ArrayList<MarkElement> sourceElementsOfBeforeChecked =
-                  MarkElementUtilities.getSourceList(nextBeforeCheckedGroupElement);
-
-              Iterator<MarkElement> sourceIteratorOfBeforeCheckedGroupElement =
-                  sourceElementsOfBeforeChecked.iterator();
-
-              while (sourceIteratorOfBeforeCheckedGroupElement.hasNext()) {
-                MarkElement sourceElementOfBeforeCheckedElement =
-                    (MarkElement) sourceIteratorOfBeforeCheckedGroupElement.next();
-                if (MarkElementUtilities.compare(sourceElementOfBeforeCheckedElement.getiMarker(),
-                    selectedMarker)) {
-                  sourceIteratorOfBeforeCheckedGroupElement.remove();
-                  break;
-                }
-              }
-
-              MarkElementUtilities.setSourceList(nextBeforeCheckedGroupElement,
-                  sourceElementsOfBeforeChecked);
-            }
-          } else {
-            ArrayList<MarkElement> sourceElementsOfBeforeChecked =
-                MarkElementUtilities.getSourceList(beforeCheckedElement.getiMarker());
-
-            Iterator<MarkElement> sourceIteratorOfBeforeChecked =
-                sourceElementsOfBeforeChecked.iterator();
-
-            while (sourceIteratorOfBeforeChecked.hasNext()) {
-              MarkElement sourceElementOfBeforeCheckedElement =
-                  (MarkElement) sourceIteratorOfBeforeChecked.next();
-              if (MarkElementUtilities.compare(sourceElementOfBeforeCheckedElement.getiMarker(),
-                  selectedMarker)) {
-                sourceIteratorOfBeforeChecked.remove();
-                break;
-              }
-            }
-            MarkElementUtilities.setSourceList(beforeCheckedElement.getiMarker(),
-                sourceElementsOfBeforeChecked);
-          }
-
-          if (MarkElementUtilities.getLeaderId(selectedMarker) != null) {
-            List<IMarker> groupMarkersOfSelectedElement = MarkerFactory.findMarkersByGroupId(
-                selectedMarker.getResource(), MarkElementUtilities.getGroupId(selectedMarker));
-
-            Iterator<IMarker> groupMarkersIteratorOfSelectedElement =
-                groupMarkersOfSelectedElement.iterator();
-
-            while (groupMarkersIteratorOfSelectedElement.hasNext()) {
-              IMarker nextSelectedGroupElement =
-                  (IMarker) groupMarkersIteratorOfSelectedElement.next();
-
-              ArrayList<MarkElement> targetElementsOfNextSelected =
-                  MarkElementUtilities.getTargetList(nextSelectedGroupElement);
-
-              Iterator<MarkElement> targetIteratorOfSelectedGroupElement =
-                  targetElementsOfNextSelected.iterator();
-
-              while (targetIteratorOfSelectedGroupElement.hasNext()) {
-                MarkElement targetElementOfNextSelectedElement =
-                    (MarkElement) targetIteratorOfSelectedGroupElement.next();
-                if (MarkElementUtilities.compare(targetElementOfNextSelectedElement.getiMarker(),
-                    beforeCheckedElement.getiMarker())) {
-                  targetIteratorOfSelectedGroupElement.remove();
-                  break;
-                }
-              }
-              MarkElementUtilities.setTargetList(nextSelectedGroupElement,
-                  targetElementsOfNextSelected);
-            }
-          } else {
-            Iterator<MarkElement> targetIteratorOfSelected = targetElementsOfSelected.iterator();
-
-            while (targetIteratorOfSelected.hasNext()) {
-              MarkElement targetElementOfSelected = (MarkElement) targetIteratorOfSelected.next();
-              if (MarkElementUtilities.compare(targetElementOfSelected.getiMarker(),
-                  beforeCheckedElement.getiMarker())) {
-                targetIteratorOfSelected.remove();
-                break;
-              }
-            }
-            MarkElementUtilities.setTargetList(selectedMarker, targetElementsOfSelected);
-          }
-        }
-      }
-
-      PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(TargetView.ID);
-      TargetView.setColumns(targetElementsOfSelected);
-
-      convertToMappingMarker();
-
-    } catch (CoreException e) {
-      e.printStackTrace();
-    }
+    Object[] checkedObjects = MarkerMatchPage.markTreeViewer.getCheckedElements();
+    AlloyUtilities.addRelation2Markers(selectedMarker, checkedObjects, relationMap);
     return true;
   }
+  //
+  // @Override
+  // public boolean performFinish() {
+  // try {
+  // Object[] checkedObjects = MarkerMatchPage.markTreeViewer.getCheckedElements();
+  // ArrayList<MarkElement> listOfSome = MarkerMatchPage.checkedElements;
+  //
+  // for (int i = 0; i < checkedObjects.length; i++) {
+  // if (checkedObjects[i] instanceof IMarker) {
+  // checkedElements.add(new MarkElement((IMarker) checkedObjects[i]));
+  // AlloyUtilities.addRelation2Markers(selectedMarker, (IMarker) checkedObjects[i], "refs");
+  // }
+  // }
+  // targetElementsOfSelected = MarkElementUtilities.getTargetList(selectedMarker);
+  //
+  // for (MarkElement checkedElement : checkedElements) {
+  // boolean flag = false;
+  // @SuppressWarnings("rawtypes")
+  // Iterator iter = beforeCheckedElements.iterator();
+  // while (iter.hasNext()) {
+  // MarkElement markElement = (MarkElement) iter.next();
+  // if (MarkElementUtilities.compare(markElement.getiMarker(), checkedElement.getiMarker())) {
+  // flag = true;
+  // break;
+  // }
+  // }
+  // if (flag == false) {
+  // if (MarkElementUtilities.getLeaderId(checkedElement.getiMarker()) != null) {
+  // List<IMarker> groupMarkersOfCheckedElement =
+  // MarkerFactory.findMarkersByGroupId(checkedElement.getiMarker().getResource(),
+  // MarkElementUtilities.getGroupId(checkedElement.getiMarker()));
+  //
+  // Iterator<IMarker> groupMarkersIteratorOfCheckedElement =
+  // groupMarkersOfCheckedElement.iterator();
+  //
+  // while (groupMarkersIteratorOfCheckedElement.hasNext()) {
+  // IMarker nextCheckedGroupElement =
+  // (IMarker) groupMarkersIteratorOfCheckedElement.next();
+  // ArrayList<MarkElement> sourcesOfGroupElement =
+  // MarkElementUtilities.getSourceList(nextCheckedGroupElement);
+  // sourcesOfGroupElement.add(new MarkElement(selectedMarker));
+  // MarkElementUtilities.setSourceList(nextCheckedGroupElement, sourcesOfGroupElement);
+  // }
+  // } else {
+  // ArrayList<MarkElement> sourceElementsOfChecked =
+  // MarkElementUtilities.getSourceList(checkedElement.getiMarker());
+  // sourceElementsOfChecked.add(new MarkElement(selectedMarker));
+  // MarkElementUtilities.setSourceList(checkedElement.getiMarker(),
+  // sourceElementsOfChecked);
+  // }
+  //
+  // if (MarkElementUtilities.getLeaderId(selectedMarker) != null) {
+  // List<IMarker> groupMarkersOfSelectedElement = MarkerFactory.findMarkersByGroupId(
+  // selectedMarker.getResource(), MarkElementUtilities.getGroupId(selectedMarker));
+  //
+  // Iterator<IMarker> groupMarkersIteratorOfSelectedElement =
+  // groupMarkersOfSelectedElement.iterator();
+  //
+  // while (groupMarkersIteratorOfSelectedElement.hasNext()) {
+  // IMarker nextSelectedGroupElement =
+  // (IMarker) groupMarkersIteratorOfSelectedElement.next();
+  //
+  // ArrayList<MarkElement> targetElementsOfNextSelected =
+  // MarkElementUtilities.getTargetList(nextSelectedGroupElement);
+  //
+  // targetElementsOfNextSelected.add(checkedElement);
+  // MarkElementUtilities.setTargetList(nextSelectedGroupElement,
+  // targetElementsOfNextSelected);
+  // }
+  // } else {
+  // targetElementsOfSelected.add(checkedElement);
+  // MarkElementUtilities.setTargetList(selectedMarker, targetElementsOfSelected);
+  // }
+  // }
+  // }
+  //
+  // for (MarkElement beforeCheckedElement : beforeCheckedElements) {
+  // if (listOfSome.contains(beforeCheckedElement)) {
+  // continue;
+  // } else {
+  // if (MarkElementUtilities.getLeaderId(beforeCheckedElement.getiMarker()) != null) {
+  //
+  // List<IMarker> groupMarkersOfBeforeCheckedElement =
+  // MarkerFactory.findMarkersByGroupId(beforeCheckedElement.getiMarker().getResource(),
+  // MarkElementUtilities.getGroupId(beforeCheckedElement.getiMarker()));
+  //
+  // Iterator<IMarker> groupMarkersIteratorOfBeforeCheckedElement =
+  // groupMarkersOfBeforeCheckedElement.iterator();
+  //
+  // while (groupMarkersIteratorOfBeforeCheckedElement.hasNext()) {
+  // IMarker nextBeforeCheckedGroupElement =
+  // (IMarker) groupMarkersIteratorOfBeforeCheckedElement.next();
+  // ArrayList<MarkElement> sourceElementsOfBeforeChecked =
+  // MarkElementUtilities.getSourceList(nextBeforeCheckedGroupElement);
+  //
+  // Iterator<MarkElement> sourceIteratorOfBeforeCheckedGroupElement =
+  // sourceElementsOfBeforeChecked.iterator();
+  //
+  // while (sourceIteratorOfBeforeCheckedGroupElement.hasNext()) {
+  // MarkElement sourceElementOfBeforeCheckedElement =
+  // (MarkElement) sourceIteratorOfBeforeCheckedGroupElement.next();
+  // if (MarkElementUtilities.compare(sourceElementOfBeforeCheckedElement.getiMarker(),
+  // selectedMarker)) {
+  // sourceIteratorOfBeforeCheckedGroupElement.remove();
+  // break;
+  // }
+  // }
+  //
+  // MarkElementUtilities.setSourceList(nextBeforeCheckedGroupElement,
+  // sourceElementsOfBeforeChecked);
+  // }
+  // } else {
+  // ArrayList<MarkElement> sourceElementsOfBeforeChecked =
+  // MarkElementUtilities.getSourceList(beforeCheckedElement.getiMarker());
+  //
+  // Iterator<MarkElement> sourceIteratorOfBeforeChecked =
+  // sourceElementsOfBeforeChecked.iterator();
+  //
+  // while (sourceIteratorOfBeforeChecked.hasNext()) {
+  // MarkElement sourceElementOfBeforeCheckedElement =
+  // (MarkElement) sourceIteratorOfBeforeChecked.next();
+  // if (MarkElementUtilities.compare(sourceElementOfBeforeCheckedElement.getiMarker(),
+  // selectedMarker)) {
+  // sourceIteratorOfBeforeChecked.remove();
+  // break;
+  // }
+  // }
+  // MarkElementUtilities.setSourceList(beforeCheckedElement.getiMarker(),
+  // sourceElementsOfBeforeChecked);
+  // }
+  //
+  // if (MarkElementUtilities.getLeaderId(selectedMarker) != null) {
+  // List<IMarker> groupMarkersOfSelectedElement = MarkerFactory.findMarkersByGroupId(
+  // selectedMarker.getResource(), MarkElementUtilities.getGroupId(selectedMarker));
+  //
+  // Iterator<IMarker> groupMarkersIteratorOfSelectedElement =
+  // groupMarkersOfSelectedElement.iterator();
+  //
+  // while (groupMarkersIteratorOfSelectedElement.hasNext()) {
+  // IMarker nextSelectedGroupElement =
+  // (IMarker) groupMarkersIteratorOfSelectedElement.next();
+  //
+  // ArrayList<MarkElement> targetElementsOfNextSelected =
+  // MarkElementUtilities.getTargetList(nextSelectedGroupElement);
+  //
+  // Iterator<MarkElement> targetIteratorOfSelectedGroupElement =
+  // targetElementsOfNextSelected.iterator();
+  //
+  // while (targetIteratorOfSelectedGroupElement.hasNext()) {
+  // MarkElement targetElementOfNextSelectedElement =
+  // (MarkElement) targetIteratorOfSelectedGroupElement.next();
+  // if (MarkElementUtilities.compare(targetElementOfNextSelectedElement.getiMarker(),
+  // beforeCheckedElement.getiMarker())) {
+  // targetIteratorOfSelectedGroupElement.remove();
+  // break;
+  // }
+  // }
+  // MarkElementUtilities.setTargetList(nextSelectedGroupElement,
+  // targetElementsOfNextSelected);
+  // }
+  // } else {
+  // Iterator<MarkElement> targetIteratorOfSelected = targetElementsOfSelected.iterator();
+  //
+  // while (targetIteratorOfSelected.hasNext()) {
+  // MarkElement targetElementOfSelected = (MarkElement) targetIteratorOfSelected.next();
+  // if (MarkElementUtilities.compare(targetElementOfSelected.getiMarker(),
+  // beforeCheckedElement.getiMarker())) {
+  // targetIteratorOfSelected.remove();
+  // break;
+  // }
+  // }
+  // MarkElementUtilities.setTargetList(selectedMarker, targetElementsOfSelected);
+  // }
+  // }
+  // }
+  //
+  // PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(TargetView.ID);
+  // TargetView.setColumns(targetElementsOfSelected);
+  //
+  // convertToMappingMarker();
+  //
+  // } catch (CoreException e) {
+  // e.printStackTrace();
+  // }
+  // return true;
+  // }
 
   public void convertToMappingMarker() throws CoreException {
     if (selectedMarker.getType().equals(MarkerFactory.MARKER_MARKING)) {
--- Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/wizards/mappingwizard/RelationWizard.java
+++ Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/wizards/mappingwizard/RelationWizard.java
@@ -1,19 +1,21 @@
 /*******************************************************************************
- * Copyright (c) 2015 UNIT Information Technologies R&D Ltd
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * Copyright (c) 2015 UNIT Information Technologies R&D Ltd All rights reserved. This program and
+ * the accompanying materials are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *     Ferhat Erata - initial API and implementation
- *     H. Emre Kirmizi - initial API and implementation
- *     Serhat Celik - initial API and implementation
- *     U. Anil Ozturk - initial API and implementation
+ * Contributors: Ferhat Erata - initial API and implementation H. Emre Kirmizi - initial API and
+ * implementation Serhat Celik - initial API and implementation U. Anil Ozturk - initial API and
+ * implementation
  *******************************************************************************/
 package eu.modelwriter.marker.ui.internal.wizards.mappingwizard;
 
+import javax.management.relation.Relation;
+
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.swt.widgets.TableItem;
 
 public class RelationWizard extends Wizard {
 
@@ -28,7 +30,11 @@ public class RelationWizard extends Wizard {
 
   @Override
   public boolean performFinish() {
-    // TODO Auto-generated method stub
+    TreeSelection treeSelection = ((TreeSelection) MarkerMatchPage.markTreeViewer.getSelection());
+    IMarker selectedMarker = (IMarker) treeSelection.getFirstElement();
+    TableItem item = relationWizardPage.getTable().getSelection()[0];
+    String relation = item.getText(0);
+    MappingWizard.relationMap.put(selectedMarker, relation.substring(0, relation.indexOf(" ")));
     return true;
   }
 
