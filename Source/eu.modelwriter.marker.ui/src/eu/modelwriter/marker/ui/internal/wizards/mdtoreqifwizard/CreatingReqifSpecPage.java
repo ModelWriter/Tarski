73ca007cd4116b951d7a96c067d6a79af9e4731e
Ümit Anıl Öztürk <anil.ozturk@unitbilisim.com>
2015-09-28 17:47:28 +0300

    to be continued.

--- Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/wizards/mdtoreqifwizard/CreatingReqifSpecPage.java
+++ Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/wizards/mdtoreqifwizard/CreatingReqifSpecPage.java
@@ -1,12 +1,8 @@
 package eu.modelwriter.marker.ui.internal.wizards.mdtoreqifwizard;
 
-import java.util.ArrayList;
-
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.viewers.CheckboxTreeViewer;
+import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
@@ -16,11 +12,9 @@ import org.eclipse.swt.widgets.Composite;
 public class CreatingReqifSpecPage extends WizardPage {
 
   public static CheckboxTreeViewer markTreeViewer = null;
-  private ArrayList<IProject> inputArray;
 
   public CreatingReqifSpecPage() {
     super("Creating Reqif");
-    inputArray = new ArrayList<IProject>();
   }
 
   @Override
@@ -35,24 +29,9 @@ public class CreatingReqifSpecPage extends WizardPage {
 
     markTreeViewer.setLabelProvider(new MarkdownToReqifLabelProvider());
     markTreeViewer.setContentProvider(treeViewerContentProvider);
-    try {
-      for (IProject iProject : ResourcesPlugin.getWorkspace().getRoot().getProjects()) {
-        if (iProject.isAccessible()) {
-          IResource[] resources = iProject.members();
-          int length = resources.length;
-          for (int i = 0; i < length; i++) {
-            if (resources[i].getType() == IResource.FILE && resources[i].getFileExtension() != null
-                && resources[i].getFileExtension().equals("reqif")) {
-              inputArray.add(iProject);
-              break;
-            }
-          }
-        }
-      }
-      markTreeViewer.setInput(inputArray.toArray());
-    } catch (CoreException e) {
-      e.printStackTrace();
-    }
+    ViewerFilter[] filter = new ViewerFilter[] {new MarkdownToReqifFilter()};
+    markTreeViewer.setInput(ResourcesPlugin.getWorkspace().getRoot().getProjects());
+    markTreeViewer.setFilters(filter);
 
     setPageComplete(true);
     setControl(composite);
--- Source/eu.modelwriter.marker/src/eu/modelwriter/marker/internal/MarkdownToReqIfDocumentBuilder.java
+++ Source/eu.modelwriter.marker/src/eu/modelwriter/marker/internal/MarkdownToReqIfDocumentBuilder.java
@@ -22,46 +22,98 @@ public class MarkdownToReqIfDocumentBuilder extends DocumentBuilder {
   Stack<SpecHierarchy> stack = new Stack<SpecHierarchy>();
   int startLevel, beforeLevel;
   ArrayList<SpecHierarchy> rootSet = new ArrayList<SpecHierarchy>();
-  
-  public ArrayList<SpecHierarchy> getRootSet() {
-    return rootSet;
-  }
 
   String blockSpec;
+
   SpecHierarchy specHierarchy = null;
   SpecObject specObject = null;
 
   @Override
-  public void link(Attributes attributes, String hrefOrHashName, String text) {
+  public void acronym(String text, String definition) {}
+
+  @Override
+  public void beginBlock(BlockType type, Attributes attributes) {
+    this.blockSpec = "";
+  }
+
+  @Override
+  public void beginDocument() {}
+
+  @Override
+  public void beginHeading(int level, Attributes attributes) {
+    this.specHierarchy = ReqIF10Factory.eINSTANCE.createSpecHierarchy();
+    this.specObject = ReqIF10Factory.eINSTANCE.createSpecObject();
+    this.specHierarchy.setObject(this.specObject);
+
+    if (this.stack.isEmpty()) {// if the new coming is root push to stack
+      this.startLevel = level;
+      this.map.put(this.specHierarchy, level);
+      this.stack.push(this.specHierarchy);
+    } else if (this.beforeLevel == level) {// if the newcoming's level equals to before's , pop
+      // before push new
+      this.stack.pop();
+      this.map.put(this.specHierarchy, level);
+      this.stack.push(this.specHierarchy);
+    } else if (level > this.beforeLevel) {// if the new coming is before's child , set before's
+      // child , and push new
+      this.stack.peek().getChildren().add(this.specHierarchy);
+      this.map.put(this.specHierarchy, level);
+      this.stack.push(this.specHierarchy);
+    } else if ((level < this.beforeLevel) && (level >= this.startLevel)) {// if the new coming is
+                                                                          // child of
+      // before any element except of
+      // before element
+      for (;;) {
+        if (this.map.get(this.stack.peek()) >= level) {
+          this.stack.pop();
+        } else {
+          break;
+        }
+      }
+      this.map.put(this.specHierarchy, level);
+      this.stack.push(this.specHierarchy);
+    } else if (level < this.startLevel) {// if the new coming's level bigger than root
+      throw new RuntimeErrorException(null, "Root must be lowest level.");
+    }
+    this.beforeLevel = level;
+  }
+
+  @Override
+  public void beginSpan(SpanType type, Attributes attributes) {
 
   }
 
   @Override
-  public void lineBreak() {}
-
-  @Override
-  public void imageLink(Attributes linkAttributes, Attributes imageAttributes, String href,
-      String imageUrl) {}
-
-  @Override
-  public void image(Attributes attributes, String url) {}
-
-  @Override
-  public void entityReference(String entity) {}
-
-  @Override
-  public void endSpan() {
-
+  public void characters(String text) {
+    this.blockSpec += text;
+    AttributeValueString attribute = ReqIF10Factory.eINSTANCE.createAttributeValueString();
+    attribute.setTheValue(text);
+    this.specObject.getValues().add(attribute);
+    this.specObject.setIdentifier(ReqIF10Factory.eINSTANCE.createAlternativeID().toString());
   }
 
   @Override
-  public void endHeading() {}
+  public void charactersUnescaped(String literal) {}
+
+  @Override
+  public void endBlock() {
+
+    this.specHierarchy = ReqIF10Factory.eINSTANCE.createSpecHierarchy();
+    this.specObject = ReqIF10Factory.eINSTANCE.createSpecObject();
+    this.specHierarchy.setObject(this.specObject);
+    AttributeValueString attribute = ReqIF10Factory.eINSTANCE.createAttributeValueString();
+    attribute.setTheValue(this.blockSpec);
+    this.specObject.getValues().add(attribute);
+    this.specObject.setIdentifier(ReqIF10Factory.eINSTANCE.createAlternativeID().toString());
+    this.stack.peek().getChildren().add(this.specHierarchy); // paragraph has been set its own
+                                                             // heading's child.
+  }
 
   @Override
   public void endDocument() {// TODO map ine g�re �ekelim
-    for (Map.Entry<SpecHierarchy, Integer> entry : map.entrySet()) {
-      if (entry.getValue() == startLevel) {
-        rootSet.add(entry.getKey());
+    for (Map.Entry<SpecHierarchy, Integer> entry : this.map.entrySet()) {
+      if (entry.getValue() == this.startLevel) {
+        this.rootSet.add(entry.getKey());
       }
     }
     // try {
@@ -81,83 +133,30 @@ public class MarkdownToReqIfDocumentBuilder extends DocumentBuilder {
   }
 
   @Override
-  public void endBlock() {
+  public void endHeading() {}
 
-    specHierarchy = ReqIF10Factory.eINSTANCE.createSpecHierarchy();
-    specObject = ReqIF10Factory.eINSTANCE.createSpecObject();
-    specHierarchy.setObject(specObject);
-    AttributeValueString attribute = ReqIF10Factory.eINSTANCE.createAttributeValueString();
-    attribute.setTheValue(blockSpec);
-    specObject.getValues().add(attribute);
-    specObject.setIdentifier(ReqIF10Factory.eINSTANCE.createAlternativeID().toString());
-    stack.peek().getChildren().add(specHierarchy); // paragraph has been set its own heading's
-                                                   // child.
+  @Override
+  public void endSpan() {
 
   }
 
   @Override
-  public void charactersUnescaped(String literal) {}
+  public void entityReference(String entity) {}
 
-  @Override
-  public void characters(String text) {
-    blockSpec += text;
-    AttributeValueString attribute = ReqIF10Factory.eINSTANCE.createAttributeValueString();
-    attribute.setTheValue(text);
-    specObject.getValues().add(attribute);
-    specObject.setIdentifier(ReqIF10Factory.eINSTANCE.createAlternativeID().toString());
+  public ArrayList<SpecHierarchy> getRootSet() {
+    return this.rootSet;
   }
 
   @Override
-  public void beginSpan(SpanType type, Attributes attributes) {
-
-  }
+  public void image(Attributes attributes, String url) {}
 
   @Override
-  public void beginHeading(int level, Attributes attributes) {
-    specHierarchy = ReqIF10Factory.eINSTANCE.createSpecHierarchy();
-    specObject = ReqIF10Factory.eINSTANCE.createSpecObject();
-    specHierarchy.setObject(specObject);
-
-    if (stack.isEmpty()) {// if the new coming is root push to stack
-      startLevel = level;
-      map.put(specHierarchy, level);
-      stack.push(specHierarchy);
-    } else if (beforeLevel == level) {// if the newcoming's level equals to before's , pop
-                                      // before push new
-      stack.pop();
-      map.put(specHierarchy, level);
-      stack.push(specHierarchy);
-    } else if (level > beforeLevel) {// if the new coming is before's child , set before's
-                                     // child , and push new
-      stack.peek().getChildren().add(specHierarchy);
-      map.put(specHierarchy, level);
-      stack.push(specHierarchy);
-    } else if (level < beforeLevel && level >= startLevel) {// if the new coming is child of
-                                                            // before any element except of
-                                                            // before element
-      for (;;) {
-        if (map.get(stack.peek()) >= level) {
-          stack.pop();
-        } else {
-          break;
-        }
-      }
-      map.put(specHierarchy, level);
-      stack.push(specHierarchy);
-    } else if (level < startLevel) {// if the new coming's level bigger than root
-      throw new RuntimeErrorException(null, "Root must be lowest level.");
-    }
-    beforeLevel = level;
-  }
+  public void imageLink(Attributes linkAttributes, Attributes imageAttributes, String href,
+      String imageUrl) {}
 
   @Override
-  public void beginDocument() {}
+  public void lineBreak() {}
 
   @Override
-  public void beginBlock(BlockType type, Attributes attributes) {
-    blockSpec = "";
-  }
-
-  @Override
-  public void acronym(String text, String definition) {}
+  public void link(Attributes attributes, String hrefOrHashName, String text) {}
 }
