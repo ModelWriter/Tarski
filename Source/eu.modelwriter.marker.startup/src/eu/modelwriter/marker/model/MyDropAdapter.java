1ec4fc6da8a5f38e61026cefc66782e36d1f69f7
Ümit Anıl Öztürk <anil.ozturk@unitbilisim.com>
2015-10-09 12:15:10 +0300

    drag and drop finally done.

--- Source/eu.modelwriter.marker.startup/src/eu/modelwriter/marker/model/MyDropAdapter.java
+++ Source/eu.modelwriter.marker.startup/src/eu/modelwriter/marker/model/MyDropAdapter.java
@@ -1,23 +1,102 @@
 package eu.modelwriter.marker.model;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
 
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.edit.ui.dnd.EditingDomainViewerDropAdapter;
 import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.mylyn.wikitext.core.parser.MarkupParser;
+import org.eclipse.mylyn.wikitext.markdown.core.MarkdownLanguage;
+import org.eclipse.rmf.reqif10.AttributeDefinitionString;
+import org.eclipse.rmf.reqif10.AttributeValueString;
+import org.eclipse.rmf.reqif10.DatatypeDefinitionString;
+import org.eclipse.rmf.reqif10.ReqIF10Factory;
+import org.eclipse.rmf.reqif10.ReqIFContent;
+import org.eclipse.rmf.reqif10.SpecHierarchy;
+import org.eclipse.rmf.reqif10.SpecObject;
+import org.eclipse.rmf.reqif10.SpecObjectType;
+import org.eclipse.rmf.reqif10.impl.ReqIFImpl;
 import org.eclipse.swt.dnd.DropTargetEvent;
 import org.eclipse.swt.dnd.TextTransfer;
 import org.eclipse.swt.widgets.Widget;
 
+import eu.modelwriter.marker.internal.MarkdownToReqIfDocumentBuilder;
+import eu.modelwriter.marker.internal.MarkerFactory;
+
 public class MyDropAdapter extends EditingDomainViewerDropAdapter {
+
+  ArrayList<SpecObject> listOfObjects;
+  ArrayList<SpecHierarchy> listOfSpecs;
 
   public MyDropAdapter(EditingDomain domain, Viewer viewer) {
     super(domain, viewer);
+    this.listOfObjects = new ArrayList<SpecObject>();
+    this.listOfSpecs = new ArrayList<SpecHierarchy>();
   }
 
   @Override
   protected Object clone() throws CloneNotSupportedException {
     return super.clone();
+  }
+
+  private void convertToReqif(DropTargetEvent event) {
+    if (event.item.getData() instanceof SpecHierarchy) {
+
+      SpecHierarchy specHier = (SpecHierarchy) event.item.getData();
+      Resource resource = specHier.eResource();
+      try {
+        resource.load(null);
+      } catch (IOException e1) {
+        e1.printStackTrace();
+      }
+
+      specHier.getChildren().addAll(this.listOfSpecs);
+      ReqIFImpl impl = (ReqIFImpl) resource.getContents().get(0);
+      ReqIFContent reqIFContent = impl.getCoreContent();
+      reqIFContent.getSpecObjects().addAll(this.listOfObjects);
+
+      AttributeDefinitionString attributeDefinitionStringDescription;
+
+      DatatypeDefinitionString id = ReqIF10Factory.eINSTANCE.createDatatypeDefinitionString();
+      id.setLongName("T_ID");
+
+      DatatypeDefinitionString description =
+          ReqIF10Factory.eINSTANCE.createDatatypeDefinitionString();
+      description.setLongName("T_Description");
+
+      reqIFContent.getDatatypes().add(description);
+      reqIFContent.getDatatypes().add(id);
+
+      attributeDefinitionStringDescription =
+          ReqIF10Factory.eINSTANCE.createAttributeDefinitionString();
+      attributeDefinitionStringDescription.setLongName("Description");
+      attributeDefinitionStringDescription.setType(description);
+
+      SpecObjectType specObjectType = ReqIF10Factory.eINSTANCE.createSpecObjectType();
+      specObjectType.setIdentifier("id");
+      specObjectType.setLongName("Custom Type");
+
+      specObjectType.getSpecAttributes().add(attributeDefinitionStringDescription);
+
+      reqIFContent.getSpecTypes().add(specObjectType);
+
+      for (SpecObject specObject : this.listOfObjects) {
+        ((AttributeValueString) specObject.getValues().get(0))
+            .setDefinition(attributeDefinitionStringDescription);
+        specObject.setType(specObjectType);
+      }
+
+      try {
+        resource.save(null);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
+      MarkerFactory.refreshProjectExp();
+    }
   }
 
   @Override
@@ -45,7 +124,9 @@ public class MyDropAdapter extends EditingDomainViewerDropAdapter {
     super.drop(event);
 
     if (TextTransfer.getInstance().isSupportedType(event.currentDataType)) {
-      System.out.println("Mission okay.");
+
+      this.parseMd(event);
+      this.convertToReqif(event);
     }
   }
 
@@ -97,6 +178,15 @@ public class MyDropAdapter extends EditingDomainViewerDropAdapter {
   @Override
   protected void helper(DropTargetEvent event) {
     super.helper(event);
+  }
+
+  private void parseMd(DropTargetEvent event) {
+    MarkdownToReqIfDocumentBuilder builder = new MarkdownToReqIfDocumentBuilder();
+    MarkupParser markupParser = new MarkupParser(new MarkdownLanguage());
+    markupParser.setBuilder(builder);
+    markupParser.parse(event.data.toString());
+    this.listOfObjects = builder.getNewlySpecObjects();
+    this.listOfSpecs = builder.getRootSet();
   }
 
   @Override
