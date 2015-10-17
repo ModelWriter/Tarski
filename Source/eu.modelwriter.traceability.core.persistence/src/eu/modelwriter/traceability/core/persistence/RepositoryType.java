e14b8a92ff319cfc414c58cec44d7c10714085ed
Serhat Çelik <serhat.celik@unitbilisim.com>
2015-10-17 17:31:12 +0300

    Generating Id System has been changed.

--- Source/eu.modelwriter.traceability.core.persistence/src/eu/modelwriter/traceability/core/persistence/RepositoryType.java
+++ Source/eu.modelwriter.traceability.core.persistence/src/eu/modelwriter/traceability/core/persistence/RepositoryType.java
@@ -29,6 +29,7 @@ import org.eclipse.emf.ecore.EObject;
  * </p>
  * <ul>
  *   <li>{@link eu.modelwriter.traceability.core.persistence.RepositoryType#getItem <em>Item</em>}</li>
+ *   <li>{@link eu.modelwriter.traceability.core.persistence.RepositoryType#getNextId <em>Next Id</em>}</li>
  * </ul>
  *
  * @see eu.modelwriter.traceability.core.persistence.persistencePackage#getRepositoryType()
@@ -52,5 +53,58 @@ public interface RepositoryType extends EObject {
    * @generated
    */
   EList<ItemType> getItem();
+
+  /**
+   * Returns the value of the '<em><b>Next Id</b></em>' attribute.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Next Id</em>' attribute isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Next Id</em>' attribute.
+   * @see #isSetNextId()
+   * @see #unsetNextId()
+   * @see #setNextId(int)
+   * @see eu.modelwriter.traceability.core.persistence.persistencePackage#getRepositoryType_NextId()
+   * @model unsettable="true" dataType="org.eclipse.emf.ecore.xml.type.Int"
+   * @generated
+   */
+  int getNextId();
+
+  /**
+   * Sets the value of the '{@link eu.modelwriter.traceability.core.persistence.RepositoryType#getNextId <em>Next Id</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @param value the new value of the '<em>Next Id</em>' attribute.
+   * @see #isSetNextId()
+   * @see #unsetNextId()
+   * @see #getNextId()
+   * @generated
+   */
+  void setNextId(int value);
+
+  /**
+   * Unsets the value of the '{@link eu.modelwriter.traceability.core.persistence.RepositoryType#getNextId <em>Next Id</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #isSetNextId()
+   * @see #getNextId()
+   * @see #setNextId(int)
+   * @generated
+   */
+  void unsetNextId();
+
+  /**
+   * Returns whether the value of the '{@link eu.modelwriter.traceability.core.persistence.RepositoryType#getNextId <em>Next Id</em>}' attribute is set.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return whether the value of the '<em>Next Id</em>' attribute is set.
+   * @see #unsetNextId()
+   * @see #getNextId()
+   * @see #setNextId(int)
+   * @generated
+   */
+  boolean isSetNextId();
 
 } // RepositoryType
