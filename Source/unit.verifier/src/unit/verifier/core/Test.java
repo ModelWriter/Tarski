eddd3e011c6511602d6d9e2d88b0ccc9b7f76a35
Emre Kirmizi <emre.kirmizi@unitbilisim.com>
2015-10-24 15:27:37 +0300

    window size of antlr ast.

--- Source/unit.verifier/src/unit/verifier/core/Test.java
+++ Source/unit.verifier/src/unit/verifier/core/Test.java
@@ -1,12 +1,11 @@
 package unit.verifier.core;
 
-import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 import org.antlr.v4.gui.TreeViewer;
@@ -35,14 +34,15 @@ public class Test {
     JScrollPane scrollPane = new JScrollPane();
     scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
     scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-    scrollPane.setBounds(50, 30, 300, 50);
+    scrollPane.setBounds(0, 0, 500, 500);
 
     TreeViewer viewr = new TreeViewer(Arrays.asList(parser.getRuleNames()), t);
-    viewr.setScale(1.0); // scale a little
+    viewr.setScale(0.8); // scale a little
     scrollPane.getViewport().add(viewr);
+
     frame.add(scrollPane);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(200, 200);
+    frame.setSize(Toolkit.getDefaultToolkit().getScreenSize().width, 500);
     frame.setVisible(true);
 
     ArityCheckVisitor myVisitor = new ArityCheckVisitor();
