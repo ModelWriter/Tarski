e4ff4ec457ffdaeed2d2ad9791b07f7f3af348b1
Emre Kirmizi <emre.kirmizi@unitbilisim.com>
2015-10-24 14:51:24 +0300

    minor changes.

--- Source/unit.verifier/src/unit/verifier/core/ArityCheckVisitor.java
+++ Source/unit.verifier/src/unit/verifier/core/ArityCheckVisitor.java
@@ -1,10 +1,13 @@
 package unit.verifier.core;
 
-import org.antlr.v4.runtime.tree.TerminalNodeImpl;
+import java.util.List;
+
+import unit.verifier.core.CoreParser.TupleContext;
 
 /**
  * Created by emre.kirmizi on 23.10.2015.
  */
+@SuppressWarnings("rawtypes")
 public class ArityCheckVisitor extends CoreBaseVisitor {
   public ArityCheckVisitor() {
     super();
@@ -13,31 +16,21 @@ public class ArityCheckVisitor extends CoreBaseVisitor {
   @Override
   public Object visitSet(CoreParser.SetContext ctx) {
     int ac = 0;
-    for (int i = 0; i < ctx.getChildCount(); i++) {
-      if (ctx.getChild(0).getText().equals("M")) {
-        int z = 0;
-      }
-      if (ctx.getChild(i) instanceof CoreParser.TupleContext) {
-        CoreParser.TupleContext context = (CoreParser.TupleContext) ctx.getChild(i);
-        int count = 0;
-        int offset = 0;
-        int line = 0;
-        for (int j = 0; j < context.getChildCount(); j++) {
-          if (((TerminalNodeImpl) context.getChild(j)).getSymbol()
-              .getType() == CoreParser.CONSTANT) {
-            count++;
-            offset = ((TerminalNodeImpl) context.getChild(j)).getSymbol().getCharPositionInLine();
-            line = ((TerminalNodeImpl) context.getChild(j)).getSymbol().getLine();
-          }
-        }
-        if (ac == 0) {
-          // arityCount.put(ctx,count);
-          ac = count;
-        } else if (ac != count) {
-          String errorString =
-              "\nArity Problem. Check it! [Line:{" + line + "}, Position:{" + offset + "}]";
-          System.err.print(errorString);
-        }
+    List<TupleContext> tuples = ctx.tuple();
+
+    for (int i = 0; i < tuples.size(); i++) {
+      TupleContext context = tuples.get(i);
+
+      int count = context.IDENTIFIER().size();
+      int offset = context.IDENTIFIER().get(0).getSymbol().getCharPositionInLine();
+      int line = context.IDENTIFIER().get(0).getSymbol().getLine();
+
+      if (ac == 0) {
+        ac = count;
+      } else if (ac != count) {
+        String errorString =
+            "\nArity Problem. Check it! [Line:{" + line + "}, Position:{" + offset + "}]";
+        System.err.print(errorString);
       }
     }
     return super.visitSet(ctx);
--- Source/unit.verifier/src/unit/verifier/core/CoreBaseListener.java
+++ Source/unit.verifier/src/unit/verifier/core/CoreBaseListener.java
@@ -6,227 +6,154 @@ import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
 /**
- * This class provides an empty implementation of {@link CoreListener}, which can be extended to
- * create a listener which only needs to handle a subset of the available methods.
+ * This class provides an empty implementation of {@link CoreListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
  */
 public class CoreBaseListener implements CoreListener {
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void enterBinaryOp(CoreParser.BinaryOpContext ctx) {}
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterSpecification(CoreParser.SpecificationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitSpecification(CoreParser.SpecificationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterSet(CoreParser.SetContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitSet(CoreParser.SetContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterTuple(CoreParser.TupleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitTuple(CoreParser.TupleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterSentence(CoreParser.SentenceContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitSentence(CoreParser.SentenceContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterQuantification(CoreParser.QuantificationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitQuantification(CoreParser.QuantificationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterExpr(CoreParser.ExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitExpr(CoreParser.ExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterRelation(CoreParser.RelationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitRelation(CoreParser.RelationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterQuantifier(CoreParser.QuantifierContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitQuantifier(CoreParser.QuantifierContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterBinaryOp(CoreParser.BinaryOpContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitBinaryOp(CoreParser.BinaryOpContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterUnaryOp(CoreParser.UnaryOpContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitUnaryOp(CoreParser.UnaryOpContext ctx) { }
 
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void enterEveryRule(ParserRuleContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void enterExpr(CoreParser.ExprContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void enterQuantification(CoreParser.QuantificationContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void enterRelation(CoreParser.RelationContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void enterSentence(CoreParser.SentenceContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void enterSet(CoreParser.SetContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void enterSpecification(CoreParser.SpecificationContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void enterTuple(CoreParser.TupleContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void enterUnaryOp(CoreParser.UnaryOpContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void exitBinaryOp(CoreParser.BinaryOpContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void exitEveryRule(ParserRuleContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void exitExpr(CoreParser.ExprContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void exitQuantification(CoreParser.QuantificationContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void exitRelation(CoreParser.RelationContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void exitSentence(CoreParser.SentenceContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void exitSet(CoreParser.SetContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void exitSpecification(CoreParser.SpecificationContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void exitTuple(CoreParser.TupleContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void exitUnaryOp(CoreParser.UnaryOpContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void visitErrorNode(ErrorNode node) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void visitTerminal(TerminalNode node) {}
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
 }
