0729d0e0c42e8c743d8835691af5391cb949a815
Emre Kirmizi <emre.kirmizi@unitbilisim.com>
2015-10-24 14:51:24 +0300

    minor changes.

--- Source/unit.verifier/src/unit/verifier/core/Core.g4
+++ Source/unit.verifier/src/unit/verifier/core/Core.g4
@@ -2,27 +2,27 @@ grammar Core;
 
 specification:  'Model:' set+ 'Sentences:' sentence+;
 
-/** R = { (x, y, z), (a,b,c) } **/
 set:    RELATION_NAME '=' '{' (tuple (',' tuple)*)? '}' ';';
-/** (x, y, z) **/
-tuple:  '(' CONSTANT (',' CONSTANT)* ')';
+tuple:  '(' IDENTIFIER (',' IDENTIFIER)* ')';
 
-sentence:   (quantification CONSTANT (',' quantification? CONSTANT)* '|')? expr ';' ;
+sentence:  expr ';' ;
 
-expr:   expr binaryOp expr
+quantification: (quantifier IDENTIFIER (',' quantifier? IDENTIFIER)* '|') ;
+
+expr:   quantification expr
+    |   expr binaryOp expr
     |   unaryOp expr
     |   '(' expr ')'
     |   relation
     ;
 
-/** R (x, y, z)**/
 relation:   RELATION_NAME tuple;
 
-quantification: 'all' | 'no' | 'lone' | 'some' | 'one' ;
+quantifier: 'all' | 'no' | 'lone' | 'some' | 'one' ;
 binaryOp:   '||' | 'or' | '&&' | 'and' | '<=>' | 'iff' | '=>' | 'implies' | '=' | 'in';
 unaryOp:    '!' | 'not' ;
 
-CONSTANT:   [a-z0-9]+;
+IDENTIFIER:   [a-z0-9]+;
 RELATION_NAME:  [a-zA-Z]+;
 LINE_COMMENT:   '--' .*? '\r'? '\n' -> skip;
 MULTILINE_COMMENT:  '/**' .*? '**/' -> skip;
--- Source/unit.verifier/src/unit/verifier/core/Test.java
+++ Source/unit.verifier/src/unit/verifier/core/Test.java
@@ -1,5 +1,6 @@
 package unit.verifier.core;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.antlr.v4.runtime.ANTLRFileStream;
@@ -10,9 +11,9 @@ import org.antlr.v4.runtime.tree.ParseTree;
 public class Test {
   public static void main(String[] args) {
     ANTLRInputStream input = null;
+    File file = new File("../unit.verifier/src/unit/verifier/core/model.core");
     try {
-      input = new ANTLRFileStream(
-          "C:\\Users\\emre.kirmizi\\git\\WP3\\Source\\unit.verifier\\src\\unit\\verifier\\core\\model.core");
+      input = new ANTLRFileStream(file.getAbsolutePath());
     } catch (IOException e) {
       e.printStackTrace();
     }
@@ -20,7 +21,29 @@ public class Test {
     CommonTokenStream tokens = new CommonTokenStream(lexer);
     CoreParser parser = new CoreParser(tokens);
     ParseTree t = parser.specification();
-    System.out.println(t.toStringTree());
+
+    // // show AST in GUI
+    // JFrame frame = new JFrame("Antlr AST");
+    //
+    // JPanel panel = new JPanel();
+    // JScrollPane scrollPane = new JScrollPane(panel);
+    // scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+    // scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+    // scrollPane.setBounds(50, 30, 300, 50);
+    //
+    // JPanel contentPane = new JPanel(null);
+    // contentPane.setPreferredSize(new Dimension(500, 400));
+    // contentPane.add(scrollPane);
+    // contentPane.add(scrollPane);
+    //
+    // TreeViewer viewr = new TreeViewer(Arrays.asList(parser.getRuleNames()), t);
+    // viewr.setScale(1.0); // scale a little
+    // panel.add(viewr);
+    // frame.add(panel);
+    // frame.setContentPane(contentPane);
+    // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    // frame.setSize(200, 200);
+    // frame.setVisible(true);
 
     ArityCheckVisitor myVisitor = new ArityCheckVisitor();
     myVisitor.visit(t);
