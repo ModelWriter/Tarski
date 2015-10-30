4e64fd322c977f24c0b9eef96e50a46cbc65fed4
Serhat Çelik <serhat.celik@unitbilisim.com>
2015-10-30 15:23:25 +0200

    Semantic and Printer have been added.

--- Source/eu.modelwriter.traceability.validation.core/src/eu/modelwriter/traceability/validation/core/fol/CoreUtilities.java
+++ Source/eu.modelwriter.traceability.validation.core/src/eu/modelwriter/traceability/validation/core/fol/CoreUtilities.java
@@ -16,103 +16,120 @@ import eu.modelwriter.traceability.validation.core.fol.generated.CoreParser.Sent
 
 public class CoreUtilities {
 
-	public static NegationContext createNegationContext(ExprContext expr) {
-		NegationContext negationContext = new NegationContext(new ExprContext());
-		TerminalNodeImpl notNode = new TerminalNodeImpl(new CommonToken(11, "not"));
+  public static NegationContext createNegationContext(ExprContext expr) {
+    NegationContext negationContext = new NegationContext(new ExprContext());
+    TerminalNodeImpl notNode = new TerminalNodeImpl(new CommonToken(11, "not"));
 
-		notNode.parent = negationContext;
-		expr.parent = negationContext;
+    notNode.parent = negationContext;
+    expr.parent = negationContext;
 
-		negationContext.addChild(notNode);
-		negationContext.addChild(expr);
+    negationContext.addChild(notNode);
+    negationContext.addChild(expr);
 
-		return negationContext;
-	}
+    return negationContext;
+  }
 
-	public static ParenthesesContext createParenthesesContext(ExprContext expr) {
-		ParenthesesContext parenthesesContext = new ParenthesesContext(new ExprContext());
-		TerminalNodeImpl leftParenthes = new TerminalNodeImpl(new CommonToken(8, "("));
-		TerminalNodeImpl rightParenthes = new TerminalNodeImpl(new CommonToken(9, ")"));
+  public static ParenthesesContext createParenthesesContext(ExprContext expr) {
+    ParenthesesContext parenthesesContext = new ParenthesesContext(new ExprContext());
+    TerminalNodeImpl leftParenthes = new TerminalNodeImpl(new CommonToken(8, "("));
+    TerminalNodeImpl rightParenthes = new TerminalNodeImpl(new CommonToken(9, ")"));
 
-		leftParenthes.parent = parenthesesContext;
-		rightParenthes.parent = parenthesesContext;
-		expr.parent = parenthesesContext;
+    leftParenthes.parent = parenthesesContext;
+    rightParenthes.parent = parenthesesContext;
+    expr.parent = parenthesesContext;
 
-		parenthesesContext.addChild(leftParenthes);
-		parenthesesContext.addChild(expr);
-		parenthesesContext.addChild(rightParenthes);
+    parenthesesContext.addChild(leftParenthes);
+    parenthesesContext.addChild(expr);
+    parenthesesContext.addChild(rightParenthes);
 
-		return parenthesesContext;
-	}
+    return parenthesesContext;
+  }
 
-	public static DisjunctionContext createDisjunctionContext(ExprContext leftContext, ExprContext rightContext) {
-		DisjunctionContext disjunctionContext = new DisjunctionContext(new ExprContext());
-		TerminalNodeImpl orNode = new TerminalNodeImpl(new CommonToken(15, "or"));
+  public static DisjunctionContext createDisjunctionContext(ExprContext leftContext,
+      ExprContext rightContext) {
+    DisjunctionContext disjunctionContext = new DisjunctionContext(new ExprContext());
+    TerminalNodeImpl orNode = new TerminalNodeImpl(new CommonToken(15, "or"));
 
-		leftContext.parent = disjunctionContext;
-		rightContext.parent = disjunctionContext;
-		orNode.parent = disjunctionContext;
+    leftContext.parent = disjunctionContext;
+    rightContext.parent = disjunctionContext;
+    orNode.parent = disjunctionContext;
 
-		disjunctionContext.left = leftContext;
-		disjunctionContext.right = rightContext;
+    disjunctionContext.left = leftContext;
+    disjunctionContext.right = rightContext;
 
-		disjunctionContext.addChild(leftContext);
-		disjunctionContext.addChild(orNode);
-		disjunctionContext.addChild(rightContext);
+    disjunctionContext.addChild(leftContext);
+    disjunctionContext.addChild(orNode);
+    disjunctionContext.addChild(rightContext);
 
-		return disjunctionContext;
-	}
+    return disjunctionContext;
+  }
 
-	public static ConjunctionContext createConjunctionContext(ExprContext leftContext, ExprContext rightContext) {
-		ConjunctionContext conjunctionContext = new ConjunctionContext(new ExprContext());
-		TerminalNodeImpl andNode = new TerminalNodeImpl(new CommonToken(13, "and"));
+  public static ConjunctionContext createConjunctionContext(ExprContext leftContext,
+      ExprContext rightContext) {
+    ConjunctionContext conjunctionContext = new ConjunctionContext(new ExprContext());
+    TerminalNodeImpl andNode = new TerminalNodeImpl(new CommonToken(13, "and"));
 
-		leftContext.parent = conjunctionContext;
-		andNode.parent = conjunctionContext;
-		rightContext.parent = conjunctionContext;
+    leftContext.parent = conjunctionContext;
+    andNode.parent = conjunctionContext;
+    rightContext.parent = conjunctionContext;
 
-		conjunctionContext.left = leftContext;
-		conjunctionContext.right = rightContext;
+    conjunctionContext.left = leftContext;
+    conjunctionContext.right = rightContext;
 
-		conjunctionContext.addChild(leftContext);
-		conjunctionContext.addChild(andNode);
-		conjunctionContext.addChild(rightContext);
+    conjunctionContext.addChild(leftContext);
+    conjunctionContext.addChild(andNode);
+    conjunctionContext.addChild(rightContext);
 
-		return conjunctionContext;
-	}
+    return conjunctionContext;
+  }
 
-	public static void moveUp(ExprContext ctx, ExprContext child) {
-		RuleContext parent = new RuleContext();
-		parent = ctx.parent;
-		child.parent = parent;
-		if (parent instanceof ExprContext) {
-			ExprContext expr = (ExprContext) parent;
-			int index = expr.children.indexOf(ctx);
-			expr.children.remove(index);
-			expr.children.add(index, child);
-		} else if (parent instanceof SentenceContext) {
-			SentenceContext sent = (SentenceContext) parent;
-			int index = sent.children.indexOf(ctx);
-			sent.children.remove(index);
-			sent.children.add(index, child);
-		}
-	}
+  public static void moveUp(ExprContext ctx, ExprContext child) {
+    RuleContext parent = new RuleContext();
+    parent = ctx.parent;
+    child.parent = parent;
+    if (parent instanceof ExprContext) {
+      ExprContext expr = (ExprContext) parent;
+      int index = expr.children.indexOf(ctx);
+      expr.children.remove(index);
+      expr.children.add(index, child);
+      setChild(expr, child, index);
+    } else if (parent instanceof SentenceContext) {
+      SentenceContext sent = (SentenceContext) parent;
+      int index = sent.children.indexOf(ctx);
+      sent.children.remove(index);
+      sent.children.add(index, child);
+    }
+  }
 
-	public static ExprContext cloneExprContext(ExprContext expr) {
-		ExprContext clone = new ExprContext();
-		clone.copyFrom(expr);
-		expr.children.size();
+  private static void setChild(ExprContext parent, ExprContext child, int index) {
+    if (parent instanceof DisjunctionContext) {
+      if (index == 0)
+        ((DisjunctionContext) parent).left = child;
+      else
+        ((DisjunctionContext) parent).right = child;
+    } else if (parent instanceof ConjunctionContext) {
+      if (index == 0)
+        ((ConjunctionContext) parent).left = child;
+      else
+        ((ConjunctionContext) parent).right = child;
+    }
+  }
 
-		for (ParseTree child : expr.children) {
-			if (child instanceof TerminalNode) {
-				clone.addChild(new TerminalNodeImpl((((TerminalNode) child).getSymbol())));
-			} else if (child instanceof ExprContext) {
-				clone.addChild(cloneExprContext((ExprContext) child));
-			} else if (child instanceof Token) {
-				clone.addChild(new CommonToken((Token) child));
-			}
-		}
-		return clone;
-	}
+  public static ExprContext cloneExprContext(ExprContext expr) {
+    ExprContext clone = new ExprContext();
+    clone.copyFrom(expr);
+    expr.children.size();
+
+    for (ParseTree child : expr.children) {
+      if (child instanceof TerminalNode) {
+        clone.addChild(new TerminalNodeImpl((((TerminalNode) child).getSymbol())));
+      } else if (child instanceof ExprContext) {
+        clone.addChild(cloneExprContext((ExprContext) child));
+      } else if (child instanceof Token) {
+        clone.addChild(new CommonToken((Token) child));
+      }
+    }
+    return clone;
+  }
 
 }
---
+++ Source/eu.modelwriter.traceability.validation.core/src/eu/modelwriter/traceability/validation/core/fol/Printer.java
@@ -0,0 +1,158 @@
+package eu.modelwriter.traceability.validation.core.fol;
+
+import java.util.List;
+
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+import eu.modelwriter.traceability.validation.core.fol.generated.CoreBaseVisitor;
+import eu.modelwriter.traceability.validation.core.fol.generated.CoreParser.ConjunctionContext;
+import eu.modelwriter.traceability.validation.core.fol.generated.CoreParser.DisjunctionContext;
+import eu.modelwriter.traceability.validation.core.fol.generated.CoreParser.EquivalanceContext;
+import eu.modelwriter.traceability.validation.core.fol.generated.CoreParser.ImplicationContext;
+import eu.modelwriter.traceability.validation.core.fol.generated.CoreParser.NegationContext;
+import eu.modelwriter.traceability.validation.core.fol.generated.CoreParser.ParenthesesContext;
+import eu.modelwriter.traceability.validation.core.fol.generated.CoreParser.QuantificationContext;
+import eu.modelwriter.traceability.validation.core.fol.generated.CoreParser.RelationContext;
+import eu.modelwriter.traceability.validation.core.fol.generated.CoreParser.SentenceContext;
+import eu.modelwriter.traceability.validation.core.fol.generated.CoreParser.SetContext;
+import eu.modelwriter.traceability.validation.core.fol.generated.CoreParser.SpecificationContext;
+import eu.modelwriter.traceability.validation.core.fol.generated.CoreParser.TupleContext;
+
+public class Printer extends CoreBaseVisitor<String> {
+  private StringBuilder builder;
+
+  public Printer() {
+    this.builder = new StringBuilder();
+  }
+
+  public StringBuilder getBuilder() {
+    return this.builder;
+  }
+
+  @Override
+  public String visitConjunction(ConjunctionContext ctx) {
+    String left = this.visit(ctx.left);
+    String right = this.visit(ctx.right);
+    String str;
+    if (!(ctx.parent instanceof ParenthesesContext))
+      str = "(" + left + " and " + right + ")";
+    else
+      str = left + " and " + right;
+    return str;
+  }
+
+  @Override
+  public String visitDisjunction(DisjunctionContext ctx) {
+    String left = this.visit(ctx.left);
+    String right = this.visit(ctx.right);
+    String str;
+    if (!(ctx.parent instanceof ParenthesesContext))
+      str = "(" + left + " or " + right + ")";
+    else
+      str = left + " or " + right;
+    return str;
+  }
+
+  @Override
+  public String visitEquivalance(EquivalanceContext ctx) {
+    String left = this.visit(ctx.left);
+    String right = this.visit(ctx.right);
+    String str;
+    if (!(ctx.parent instanceof ParenthesesContext))
+      str = "(" + left + " <-> " + right + ")";
+    else
+      str = left + " <-> " + right;
+    return str;
+  }
+
+  @Override
+  public String visitImplication(ImplicationContext ctx) {
+    String left = this.visit(ctx.left);
+    String right = this.visit(ctx.right);
+    String str;
+    if (!(ctx.parent instanceof ParenthesesContext))
+      str = "(" + left + " -> " + right + ")";
+    else
+      str = left + " -> " + right;
+    return str;
+  }
+
+  @Override
+  public String visitNegation(NegationContext ctx) {
+    String str;
+    if (ctx.expr() instanceof RelationContext || ctx.expr() instanceof ParenthesesContext) {
+      str = "not " + visit(ctx.expr());
+    } else {
+      str = "not" + "(" + visit(ctx.expr()) + ")";
+    }
+    return str;
+  }
+
+  @Override
+  public String visitParentheses(ParenthesesContext ctx) {
+    String str;
+    if (ctx.expr() instanceof RelationContext) {
+      str = this.visit(ctx.expr());
+    } else {
+      str = "(" + this.visit(ctx.expr()) + ")";
+    }
+    return str;
+  }
+
+  @Override
+  public String visitQuantification(QuantificationContext ctx) {
+    String op = ctx.quantifer().op.getText() + " ";
+    String identifiers = "";
+
+    List<TerminalNode> identifierList = ctx.quantifer().IDENTIFIER();
+    for (int i = 0; i < identifierList.size() - 1; i++) {
+      identifiers += identifierList.get(i).getText() + ", ";
+    }
+    identifiers += identifierList.get(identifierList.size() - 1).getText();
+    String str = op + identifiers + " | " + this.visit(ctx.expr());
+    return str;
+  }
+
+  @Override
+  public String visitRelation(RelationContext ctx) {
+    String str = ctx.getText();
+    return str;
+  }
+
+  @Override
+  public String visitSentence(SentenceContext ctx) {
+    String str = "\t" + this.visit(ctx.expr()) + ";\n";
+    return str;
+  }
+
+  @Override
+  public String visitSet(SetContext ctx) {
+    String str = "\t" + ctx.getText() + "\n";
+    return str;
+  }
+
+  @Override
+  public String visitSpecification(SpecificationContext ctx) {
+    String model = "Model:\n";
+    for (SetContext set : ctx.set()) {
+      model += this.visit(set);
+    }
+
+    String sentences = "\nSentences:\n";
+    for (SentenceContext sentence : ctx.sentence()) {
+      sentences += this.visit(sentence);
+    }
+
+    String specification = model + sentences;
+    this.builder.append(specification);
+    System.out.println(specification);
+    return sentences;
+  }
+
+  @Override
+  public String visitTuple(TupleContext ctx) {
+    String str = ctx.getText();
+    return str;
+  }
+
+}
