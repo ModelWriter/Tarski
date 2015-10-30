7ce6b433eb416840d65267eac6a52a8f1c3696f3
Emre Kirmizi <emre.kirmizi@unitbilisim.com>
2015-10-30 15:28:04 +0200

    CnfConverter Class has been changed as ParseTreePrinter.
    ParseTreePrinter class only prints original parse tree like 
    Model:
    	set*... 
    Sentences: 
    	sentence*... .
    
    CnfConverter has been fragmented as EquivalanceConverter,
    ImplicationConverter, NegationConverter. These classes extend
    ParseTreePrinter and they override their associated methods.
    It means;
    EquivalanceConverter extends ParseTreePrinter{
    ...
    @override visitEquivalance(EquivalanceContext ctx){
    	...
    }
    So it prints original parse tree with parent class ParseTreePrinter, and
    it prints equivalances as implications. (It converts equivalance to
    implication.)
    
    And in the Test class, we run this classes sequentially.
    First run EquivalanceConverter,
    after that ImplicationConverter,
    and last NegationConverter.

--- Source/eu.modelwriter.traceability.validation.core/src/eu/modelwriter/traceability/validation/core/fol/generated/CoreBaseListener.java
+++ Source/eu.modelwriter.traceability.validation.core/src/eu/modelwriter/traceability/validation/core/fol/generated/CoreBaseListener.java
@@ -1,3 +1,4 @@
+// Generated from C:/Users/emre.kirmizi/IdeaProjects/Core/src/Antlr\Core.g4 by ANTLR 4.5.1
 package eu.modelwriter.traceability.validation.core.fol.generated;
 
 import org.antlr.v4.runtime.ParserRuleContext;
@@ -17,157 +18,7 @@ public class CoreBaseListener implements CoreListener {
    * </p>
    */
   @Override
-  public void enterSpecification(CoreParser.SpecificationContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void exitSpecification(CoreParser.SpecificationContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void enterSet(CoreParser.SetContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void exitSet(CoreParser.SetContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void enterTuple(CoreParser.TupleContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void exitTuple(CoreParser.TupleContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void enterSentence(CoreParser.SentenceContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void exitSentence(CoreParser.SentenceContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void enterParentheses(CoreParser.ParenthesesContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void exitParentheses(CoreParser.ParenthesesContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void enterNegation(CoreParser.NegationContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void exitNegation(CoreParser.NegationContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void enterQuantification(CoreParser.QuantificationContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void exitQuantification(CoreParser.QuantificationContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
   public void enterConjunction(CoreParser.ConjunctionContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void exitConjunction(CoreParser.ConjunctionContext ctx) {}
 
   /**
    * {@inheritDoc}
@@ -187,87 +38,7 @@ public class CoreBaseListener implements CoreListener {
    * </p>
    */
   @Override
-  public void exitDisjunction(CoreParser.DisjunctionContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void enterImplication(CoreParser.ImplicationContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void exitImplication(CoreParser.ImplicationContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
   public void enterEquivalance(CoreParser.EquivalanceContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void exitEquivalance(CoreParser.EquivalanceContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void enterRelation(CoreParser.RelationContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void exitRelation(CoreParser.RelationContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void enterQuantifer(CoreParser.QuantiferContext ctx) {}
-
-  /**
-   * {@inheritDoc}
-   *
-   * <p>
-   * The default implementation does nothing.
-   * </p>
-   */
-  @Override
-  public void exitQuantifer(CoreParser.QuantiferContext ctx) {}
 
   /**
    * {@inheritDoc}
@@ -287,6 +58,136 @@ public class CoreBaseListener implements CoreListener {
    * </p>
    */
   @Override
+  public void enterImplication(CoreParser.ImplicationContext ctx) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
+  public void enterNegation(CoreParser.NegationContext ctx) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
+  public void enterParentheses(CoreParser.ParenthesesContext ctx) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
+  public void enterQuantifer(CoreParser.QuantiferContext ctx) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
+  public void enterQuantification(CoreParser.QuantificationContext ctx) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
+  public void enterRelation(CoreParser.RelationContext ctx) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
+  public void enterSentence(CoreParser.SentenceContext ctx) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
+  public void enterSet(CoreParser.SetContext ctx) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
+  public void enterSpecification(CoreParser.SpecificationContext ctx) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
+  public void enterTuple(CoreParser.TupleContext ctx) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
+  public void exitConjunction(CoreParser.ConjunctionContext ctx) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
+  public void exitDisjunction(CoreParser.DisjunctionContext ctx) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
+  public void exitEquivalance(CoreParser.EquivalanceContext ctx) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
   public void exitEveryRule(ParserRuleContext ctx) {}
 
   /**
@@ -297,7 +198,97 @@ public class CoreBaseListener implements CoreListener {
    * </p>
    */
   @Override
-  public void visitTerminal(TerminalNode node) {}
+  public void exitImplication(CoreParser.ImplicationContext ctx) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
+  public void exitNegation(CoreParser.NegationContext ctx) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
+  public void exitParentheses(CoreParser.ParenthesesContext ctx) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
+  public void exitQuantifer(CoreParser.QuantiferContext ctx) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
+  public void exitQuantification(CoreParser.QuantificationContext ctx) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
+  public void exitRelation(CoreParser.RelationContext ctx) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
+  public void exitSentence(CoreParser.SentenceContext ctx) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
+  public void exitSet(CoreParser.SetContext ctx) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
+  public void exitSpecification(CoreParser.SpecificationContext ctx) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
+  public void exitTuple(CoreParser.TupleContext ctx) {}
 
   /**
    * {@inheritDoc}
@@ -308,4 +299,14 @@ public class CoreBaseListener implements CoreListener {
    */
   @Override
   public void visitErrorNode(ErrorNode node) {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * <p>
+   * The default implementation does nothing.
+   * </p>
+   */
+  @Override
+  public void visitTerminal(TerminalNode node) {}
 }
