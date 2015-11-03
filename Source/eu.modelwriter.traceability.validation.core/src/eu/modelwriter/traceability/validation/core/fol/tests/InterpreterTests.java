b9337216fd327997ce5d513df9f85fdc4cad1381
Ferhat Erata <ferhat.erata@unitbilisim.com>
2015-11-03 12:07:37 +0200

    JUnit tests folder and example classes created

---
+++ Source/eu.modelwriter.traceability.validation.core/src/eu/modelwriter/traceability/validation/core/fol/tests/InterpreterTests.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package eu.modelwriter.traceability.validation.core.fol.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Ferhat Erata
+ *
+ */
+public class InterpreterTests {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
