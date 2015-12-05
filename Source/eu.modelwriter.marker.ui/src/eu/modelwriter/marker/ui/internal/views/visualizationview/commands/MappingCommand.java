024ec0e931242e6f558721e9a122560676133ce3
Emre Kirmizi <emre.kirmizi@unitbilisim.com>
2015-12-05 11:37:22 +0200

    DeleteCommand, AddRemoveTypeCommand, ResolveCommand, MappingCommand
    classes have been added. They are for VisualizationView. It is not
    necessary now to focus on marker and get selection.

--- Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/views/visualizationview/commands/MappingCommand.java
+++ Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/views/visualizationview/commands/MappingCommand.java
@@ -12,29 +12,58 @@ package eu.modelwriter.marker.ui.internal.views.visualizationview.commands;
 
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
 import eu.modelwriter.configuration.internal.AlloyUtilities;
-import eu.modelwriter.marker.ui.Activator;
+import eu.modelwriter.marker.internal.MarkUtilities;
 import eu.modelwriter.marker.ui.internal.views.visualizationview.Visualization;
-import eu.modelwriter.marker.ui.internal.wizards.mappingwizard.MarkerMapping;
+import eu.modelwriter.marker.ui.internal.wizards.mappingwizard.MappingWizard;
 
 public class MappingCommand {
-  static IMarker marker;
+  private static void chooseForAction(final IMarker iMarker) {
+    if (MarkUtilities.getType(iMarker) != null) {
+      MappingCommand.goForRelPage(iMarker);
+    } else {
+      MappingCommand.goForMapPage(iMarker);
+    }
+  }
+
+  private static void goForMapPage(final IMarker iMarker) {
+    final MappingWizard relationWizard = new MappingWizard(iMarker, false);
+    final WizardDialog dialog = new WizardDialog(new Shell(), relationWizard);
+    dialog.open();
+  }
+
+  private static void goForRelPage(final IMarker iMarker) {
+    final MappingWizard relationWizard = new MappingWizard(iMarker, true);
+    final WizardDialog dialog = new WizardDialog(new Shell(), relationWizard);
+    dialog.open();
+  }
 
   public static void run(final IMarker marker) {
-    MappingCommand.marker = marker;
-    if (AlloyUtilities.isExists()) {
-      MarkerMapping.getInstance().run();
-      if (Activator.getDefault().getWorkbench().getWorkbenchWindows()[0].getActivePage()
-          .findView(Visualization.ID) != null) {
-        Visualization.showViz(Visualization.container);
+    Display.getDefault().syncExec(new Runnable() {
+      @Override
+      public void run() {
+        if (AlloyUtilities.isExists()) {
+          if (marker != null && marker.exists()) {
+            MappingCommand.chooseForAction(marker);
+            Visualization.showViz(Visualization.container);
+          } else {
+            final MessageDialog dialog = new MessageDialog(new Shell(),
+                "There is no marker in this position", null, "Please select valid marker",
+                MessageDialog.INFORMATION, new String[] {"OK"}, 0);
+            dialog.open();
+          }
+        } else {
+          final MessageDialog infoDialog = new MessageDialog(new Shell(), "System Information",
+              null, "You dont have any registered alloy file to system.", MessageDialog.INFORMATION,
+              new String[] {"OK"}, 0);
+          infoDialog.open();
+        }
       }
-    } else {
-      final MessageDialog infoDialog = new MessageDialog(Activator.getShell(), "System Information",
-          null, "You dont have any registered alloy file to system.", MessageDialog.INFORMATION,
-          new String[] {"OK"}, 0);
-      infoDialog.open();
-    }
+    });
     return;
   }
 }
