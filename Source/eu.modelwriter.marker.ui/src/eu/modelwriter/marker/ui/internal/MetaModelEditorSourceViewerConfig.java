821618b1f58ba2d6a90d8875f172ff47e5b50c2c
Ümit Anıl Öztürk <anil.ozturk@unitbilisim.com>
2015-12-07 14:25:08 +0200

    text editor repairing has been done for single line.

--- Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/MetaModelEditorSourceViewerConfig.java
+++ Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/MetaModelEditorSourceViewerConfig.java
@@ -1,7 +1,7 @@
 package eu.modelwriter.marker.ui.internal;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.TextAttribute;
@@ -31,19 +31,28 @@ public class MetaModelEditorSourceViewerConfig extends SourceViewerConfiguration
       "check", "disj", "else", "exactly", "extends", "fact", "for", "fun", "iden", "iff", "implies",
       "in", "Int", "let", "lone", "module", "no", "none", "not", "one", "open", "or", "pred", "run",
       "set", "sig", "some", "sum", "univ"};
-  private final ArrayList<Character> terminals = new ArrayList<Character>(Arrays.asList('~', '^',
-      '*', '.', '[', ']', '<', ':', '>', '-', '&', '+', '#', '!', '=', '|', '(', ')', '{', '}'));
 
   private IRule[] createRules() {
     final IToken commentToken =
         new Token(new TextAttribute(new Color(Display.getCurrent(), new RGB(204, 0, 0))));
     final IToken keywordToken =
         new Token(new TextAttribute(new Color(Display.getCurrent(), new RGB(65, 105, 225))));
+    final IToken defaultToken =
+        new Token(new TextAttribute(new Color(Display.getCurrent(), new RGB(0, 0, 0))));
+
+    final List<IRule> rules = new ArrayList<IRule>();
+    rules.add(new EndOfLineRule("--", commentToken));
+    rules.add(new EndOfLineRule("//", commentToken));
+    rules.add(new MultiLineRule("/*", "*/", commentToken));
+    rules.add(new WhitespaceRule(new IWhitespaceDetector() {
+
+      @Override
+      public boolean isWhitespace(final char c) {
+        return Character.isWhitespace(c);
+      }
+    }));
 
     final WordRule keywordRule = new WordRule(new IWordDetector() {
-
-      boolean ctrl = false; // this controls coloring when keyword unified with any letter character
-      boolean first = true;
 
       @Override
       public boolean isWordPart(final char c) {
@@ -52,38 +61,17 @@ public class MetaModelEditorSourceViewerConfig extends SourceViewerConfiguration
 
       @Override
       public boolean isWordStart(final char c) {
-        if (this.first) { // document's first text -'module'- must be keyword
-          this.first = false;
-          return true;
-        }
-        if (Character.isWhitespace(c) || Character.isSpaceChar(c)
-            || MetaModelEditorSourceViewerConfig.this.terminals.contains(c)) {
-          this.ctrl = true;
-        } else {
-          for (int i = 0; i < MetaModelEditorSourceViewerConfig.this.keywords.length; i++) {
-            if (this.ctrl && MetaModelEditorSourceViewerConfig.this.keywords[i].charAt(0) == c) {
-              return true;
-            }
-          }
-          this.ctrl = false;
-        }
-        return false;
+        return Character.isLetter(c);
       }
-    });
+    }, defaultToken);
 
     for (int i = 0; i < this.keywords.length; i++) {
       keywordRule.addWord(this.keywords[i], keywordToken);
     }
-
-    return new IRule[] {new EndOfLineRule("//", commentToken),
-        new EndOfLineRule("--", commentToken), new MultiLineRule("/*", "*/", commentToken),
-        keywordRule, new WhitespaceRule(new IWhitespaceDetector() {
-
-          @Override
-          public boolean isWhitespace(final char c) {
-            return Character.isWhitespace(c);
-          }
-        })};
+    rules.add(keywordRule);
+    final IRule[] result = new IRule[rules.size()];
+    rules.toArray(result);
+    return result;
   }
 
   private ITokenScanner createTokenScanner() {
@@ -96,6 +84,7 @@ public class MetaModelEditorSourceViewerConfig extends SourceViewerConfiguration
   public IPresentationReconciler getPresentationReconciler(final ISourceViewer sourceViewer) {
     final PresentationReconciler reconciler = new PresentationReconciler();
     final DefaultDamagerRepairer repairer = new DefaultDamagerRepairer(this.createTokenScanner());
+    reconciler.setDamager(repairer, IDocument.DEFAULT_CONTENT_TYPE);
     reconciler.setRepairer(repairer, IDocument.DEFAULT_CONTENT_TYPE);
     return reconciler;
   }
