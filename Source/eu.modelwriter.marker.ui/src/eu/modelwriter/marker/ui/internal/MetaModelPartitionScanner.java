3201df80bdebaf26d5fae35d31f5c0ed0fdfaede
Ümit Anıl Öztürk <anil.ozturk@unitbilisim.com>
2015-12-14 14:09:49 +0200

    minor change on partitioning.

---
+++ Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/MetaModelPartitionScanner.java
@@ -0,0 +1,33 @@
+package eu.modelwriter.marker.ui.internal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.rules.EndOfLineRule;
+import org.eclipse.jface.text.rules.IPredicateRule;
+import org.eclipse.jface.text.rules.IRule;
+import org.eclipse.jface.text.rules.IToken;
+import org.eclipse.jface.text.rules.MultiLineRule;
+import org.eclipse.jface.text.rules.RuleBasedPartitionScanner;
+import org.eclipse.jface.text.rules.Token;
+
+public class MetaModelPartitionScanner extends RuleBasedPartitionScanner {
+
+  public final static String META_MODEL_COMMENT = "__META_MODEL_COMMENT";
+  public final static String[] PARTITION_TYPES =
+      new String[] {IDocument.DEFAULT_CONTENT_TYPE, MetaModelPartitionScanner.META_MODEL_COMMENT};
+
+  public MetaModelPartitionScanner() {
+    final IToken commentPartition = new Token(MetaModelPartitionScanner.META_MODEL_COMMENT);
+
+    final List<IRule> rules = new ArrayList<IRule>();
+    rules.add(new MultiLineRule("/*", "*/", commentPartition));
+    rules.add(new EndOfLineRule("--", commentPartition));
+    rules.add(new EndOfLineRule("//", commentPartition));
+
+    final IPredicateRule[] result = new IPredicateRule[rules.size()];
+    rules.toArray(result);
+    this.setPredicateRules(result);
+  }
+}
