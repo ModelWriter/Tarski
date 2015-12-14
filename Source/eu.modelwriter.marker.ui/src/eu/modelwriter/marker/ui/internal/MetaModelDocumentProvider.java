68e4863da49d530da0a0703b2290ce927dc753b9
Ümit Anıl Öztürk <anil.ozturk@unitbilisim.com>
2015-12-14 09:57:43 +0200

    multi line comment editing at text editor has been done.

---
+++ Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/MetaModelDocumentProvider.java
@@ -0,0 +1,22 @@
+package eu.modelwriter.marker.ui.internal;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IDocumentPartitioner;
+import org.eclipse.jface.text.rules.FastPartitioner;
+import org.eclipse.ui.editors.text.FileDocumentProvider;
+
+public class MetaModelDocumentProvider extends FileDocumentProvider {
+
+  @Override
+  protected IDocument createDocument(final Object element) throws CoreException {
+    final IDocument document = super.createDocument(element);
+    if (document != null) {
+      final IDocumentPartitioner partitioner = new FastPartitioner(new MetaModelPartitionScanner(),
+          MetaModelPartitionScanner.PARTITION_TYPES);
+      partitioner.connect(document);
+      document.setDocumentPartitioner(partitioner);
+    }
+    return document;
+  }
+}
