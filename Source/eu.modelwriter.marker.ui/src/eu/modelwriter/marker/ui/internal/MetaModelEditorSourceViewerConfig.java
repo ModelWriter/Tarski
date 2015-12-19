89890b9749ae3f7179d72f4ab2dd264ae3877f94
Ümit Anıl Öztürk <anil.ozturk@unitbilisim.com>
2015-12-19 10:15:33 +0200

    content assist has been expanded and annotating editor while typing has been added.

--- Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/MetaModelEditorSourceViewerConfig.java
+++ Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/MetaModelEditorSourceViewerConfig.java
@@ -1,15 +1,25 @@
 package eu.modelwriter.marker.ui.internal;
 
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.contentassist.ContentAssistant;
 import org.eclipse.jface.text.contentassist.IContentAssistant;
 import org.eclipse.jface.text.presentation.IPresentationReconciler;
 import org.eclipse.jface.text.presentation.PresentationReconciler;
+import org.eclipse.jface.text.reconciler.IReconciler;
+import org.eclipse.jface.text.reconciler.MonoReconciler;
 import org.eclipse.jface.text.rules.DefaultDamagerRepairer;
 import org.eclipse.jface.text.source.ISourceViewer;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.editors.text.TextSourceViewerConfiguration;
 
 public class MetaModelEditorSourceViewerConfig extends TextSourceViewerConfiguration {
+
+  private final IEditorPart editor;
+
+  public MetaModelEditorSourceViewerConfig(final IEditorPart editor) {
+    this.editor = editor;
+  }
 
   @Override
   public String[] getConfiguredContentTypes(final ISourceViewer sourceViewer) {
@@ -21,6 +31,7 @@ public class MetaModelEditorSourceViewerConfig extends TextSourceViewerConfigura
     final ContentAssistant assistant = new ContentAssistant();
     assistant.setContentAssistProcessor(new CodeCompletionProcessor(),
         IDocument.DEFAULT_CONTENT_TYPE);
+    assistant.setAutoActivationDelay(500);
     assistant.enableAutoActivation(true);
     assistant.enableAutoInsert(true);
     assistant.setProposalPopupOrientation(IContentAssistant.PROPOSAL_OVERLAY);
@@ -43,6 +54,18 @@ public class MetaModelEditorSourceViewerConfig extends TextSourceViewerConfigura
     reconciler.setDamager(dr, MetaModelPartitionScanner.META_MODEL_COMMENT);
     reconciler.setRepairer(dr, MetaModelPartitionScanner.META_MODEL_COMMENT);
 
+    return reconciler;
+  }
+
+  @Override
+  public IReconciler getReconciler(final ISourceViewer sourceViewer) {
+    MonoReconciler reconciler = null;
+    if (sourceViewer != null) {
+      reconciler = new MonoReconciler(
+          new MetaModelValidationReconcilingStrategy(sourceViewer, this.editor), false);
+      reconciler.setDelay(250);
+      reconciler.setProgressMonitor(new NullProgressMonitor());
+    }
     return reconciler;
   }
 }
