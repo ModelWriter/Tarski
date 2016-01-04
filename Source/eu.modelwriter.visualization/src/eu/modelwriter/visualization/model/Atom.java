a76610ba209e75d8f67d20cffa255bd36c4259f1
Serhat Çelik <serhat.celik@unitbilisim.com>
2016-01-04 13:02:52 +0200

    Get Last Universe from file in Viz has been added.

--- Source/eu.modelwriter.visualization/src/eu/modelwriter/visualization/model/Atom.java
+++ Source/eu.modelwriter.visualization/src/eu/modelwriter/visualization/model/Atom.java
@@ -1,7 +1,11 @@
 package eu.modelwriter.visualization.model;
 
+import java.io.Serializable;
+
 public class Atom {
   private String text;
+  private Serializable data;
+  private String id;
 
   public Atom(String text) {
     this.text = text;
@@ -10,4 +14,21 @@ public class Atom {
   public String getText() {
     return this.text;
   }
+
+  public Serializable getData() {
+    return data;
+  }
+
+  public void setData(Serializable data) {
+    this.data = data;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
 }
