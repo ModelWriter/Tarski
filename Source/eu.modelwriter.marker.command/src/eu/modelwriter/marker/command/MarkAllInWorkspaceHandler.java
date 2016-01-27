5752b68e8c7fc295c6afd99ad8b9bb69aa0759b6
Emre Kirmizi <emre.kirmizi@unitbilisim.com>
2016-01-27 20:24:20 +0200

    minor change.

--- Source/eu.modelwriter.marker.command/src/eu/modelwriter/marker/command/MarkAllInWorkspaceHandler.java
+++ Source/eu.modelwriter.marker.command/src/eu/modelwriter/marker/command/MarkAllInWorkspaceHandler.java
@@ -23,7 +23,7 @@ import org.eclipse.ui.PlatformUI;
 import eu.modelwriter.configuration.internal.AlloyUtilities;
 import eu.modelwriter.marker.MarkerActivator;
 import eu.modelwriter.marker.internal.MarkerFactory;
-import eu.modelwriter.marker.ui.internal.wizard.markallinwswizard.MarkAllInWsWizard;
+import eu.modelwriter.marker.ui.internal.wizards.markallinwswizard.MarkAllInWsWizard;
 
 public class MarkAllInWorkspaceHandler extends AbstractHandler {
   public static String COMMAND_ID = "eu.modelwriter.marker.command.markallinworkspace";
