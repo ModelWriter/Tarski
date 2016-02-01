7d026a48eb9a810b81f0a411e3eca6b3c57033ed
emrekirmiziunit <emre.kirmizi@unitbilisim.com>
2016-02-01 14:53:10 +0200

    rml file;
    is now reading from selected element on the project explorer of runtime
    eclipse.

--- eu.modelwriter.traceability.validation.core.crocopat/src/eu/modelwriter/traceability/validation/core/crocopat/handlers/CrocopatExecuteHandler.java
+++ eu.modelwriter.traceability.validation.core.crocopat/src/eu/modelwriter/traceability/validation/core/crocopat/handlers/CrocopatExecuteHandler.java
@@ -7,8 +7,12 @@ import java.io.InputStreamReader;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.emf.common.CommonPlugin;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ITreeSelection;
+import org.eclipse.ui.PlatformUI;
 
 public class CrocopatExecuteHandler extends AbstractHandler {
   /**
@@ -24,17 +28,25 @@ public class CrocopatExecuteHandler extends AbstractHandler {
   public Object execute(final ExecutionEvent event) throws ExecutionException {
     final Runtime rt = Runtime.getRuntime();
     try {
-      final URI exeUri = CommonPlugin.resolve(URI.createURI(
-          "platform:/plugin/eu.modelwriter.traceability.validation.core.crocopat/exec/crocopat-2.1.4_win32.exe"));
-      final URI fileUri = CommonPlugin.resolve(URI.createURI(
-          "platform:/plugin/eu.modelwriter.traceability.validation.core.crocopat/files/List.rml"));
-
-      final Process pr = rt.exec(exeUri.path() + " -e " + fileUri.path());
-      final BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-      String line = null;
-      while ((line = input.readLine()) != null) {
-        System.out.println(line);
-      }
+    	ISelection selection = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection();
+    	
+		if (selection instanceof ITreeSelection) {
+		      ITreeSelection treeSel = (ITreeSelection) selection;
+		      if (treeSel.getFirstElement() instanceof IFile) {
+		        IFile iFile = (IFile) treeSel.getFirstElement();
+		      final URI exeUri = CommonPlugin.resolve(URI.createURI(
+		          "platform:/plugin/eu.modelwriter.traceability.validation.core.crocopat/exec/crocopat-2.1.4_win32.exe"));
+		      final String exeAbsolutePath = exeUri.path();
+		      final String rmlAbsolutePath = iFile.getLocation().toString();
+		      
+		  final Process pr = rt.exec(exeAbsolutePath + " -e " + rmlAbsolutePath);
+			      final BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+			      String line = null;
+			      while ((line = input.readLine()) != null) {
+			        System.out.println(line);
+			      }
+		     }
+		}
     } catch (final IOException e) {
       e.printStackTrace();
     }
