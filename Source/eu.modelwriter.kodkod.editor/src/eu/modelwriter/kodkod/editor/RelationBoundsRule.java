9ea34b20d280a26d8847e00faed5165661a54ec3
Ümit Anıl Öztürk <anil.ozturk@unitbilisim.com>
2016-02-05 13:36:09 +0200

    kodkod editor to universe has been done.

--- Source/eu.modelwriter.kodkod.editor/src/eu/modelwriter/kodkod/editor/RelationBoundsRule.java
+++ Source/eu.modelwriter.kodkod.editor/src/eu/modelwriter/kodkod/editor/RelationBoundsRule.java
@@ -5,37 +5,36 @@ import org.eclipse.jface.text.rules.IToken;
 import org.eclipse.jface.text.rules.PatternRule;
 
 public class RelationBoundsRule extends PatternRule {
-	public RelationBoundsRule(String startSequence, String endSequence, IToken token, char escapeCharacter,
-			boolean breaksOnEOL) {
-		super(startSequence, endSequence, token, escapeCharacter, breaksOnEOL);
-	}
+  public RelationBoundsRule(final String startSequence, final String endSequence,
+      final IToken token, final char escapeCharacter, final boolean breaksOnEOL) {
+    super(startSequence, endSequence, token, escapeCharacter, breaksOnEOL);
+  }
 
-	// buraya iyi bak sikintili bir durum olabilir eger olursa burdandir
-	@Override
-	protected boolean endSequenceDetected(ICharacterScanner scanner) {
-		int c = scanner.read();
-		if (c != ICharacterScanner.EOF) {
-			do {
-				boolean isEqualEndSeq = true;
-				while (c != fEndSequence[0] && c != ICharacterScanner.EOF) {
-					c = scanner.read();
-				}
-				for (int i = 1; i < fEndSequence.length; i++) {
-					c = scanner.read();
-					if (c != fEndSequence[i]) {
-						isEqualEndSeq = false;
-						break;
-					}
-				}
-				if (isEqualEndSeq) {
-					for (int i = 0; i < fEndSequence.length; i++) {
-						scanner.unread();
-					}
-					return true;
-				}
-				c = scanner.read();
-			} while (c != ICharacterScanner.EOF);
-		}
-		return false;
-	}
+  @Override
+  protected boolean endSequenceDetected(final ICharacterScanner scanner) {
+    int c = scanner.read();
+    if (c != ICharacterScanner.EOF) {
+      do {
+        boolean isEqualEndSeq = true;
+        while (c != this.fEndSequence[0] && c != ICharacterScanner.EOF) {
+          c = scanner.read();
+        }
+        for (int i = 1; i < this.fEndSequence.length; i++) {
+          c = scanner.read();
+          if (c != this.fEndSequence[i]) {
+            isEqualEndSeq = false;
+            break;
+          }
+        }
+        if (isEqualEndSeq) {
+          for (int i = 0; i < this.fEndSequence.length; i++) {
+            scanner.unread();
+          }
+          return true;
+        }
+        c = scanner.read();
+      } while (c != ICharacterScanner.EOF);
+    }
+    return false;
+  }
 }
--- Source/eu.modelwriter.kodkod.editor/src/eu/modelwriter/kodkod/editor/RelationModelPartitionScanner.java
+++ Source/eu.modelwriter.kodkod.editor/src/eu/modelwriter/kodkod/editor/RelationModelPartitionScanner.java
@@ -14,35 +14,37 @@ import org.eclipse.jface.text.rules.Token;
 
 public class RelationModelPartitionScanner extends RuleBasedPartitionScanner {
 
-	public final static String RELATION_MODEL_COMMENT = "__RELATION_MODEL_COMMENT";
-	public final static String RELATION_MODEL_OPTION = "__RELATION_MODEL_OPTION";
-	public final static String RELATION_MODEL_UNIVERSE = "__RELATION_MODEL_UNIVERSE";
-	public final static String RELATION_MODEL_REL_BOUND = "__RELATION_MODEL_REL_BOUND";
+  public final static String RELATION_MODEL_COMMENT = "__RELATION_MODEL_COMMENT";
+  public final static String RELATION_MODEL_OPTION = "__RELATION_MODEL_OPTION";
+  public final static String RELATION_MODEL_UNIVERSE = "__RELATION_MODEL_UNIVERSE";
+  public final static String RELATION_MODEL_REL_BOUND = "__RELATION_MODEL_REL_BOUND";
 
-	public final static String[] PARTITION_TYPES = new String[] { IDocument.DEFAULT_CONTENT_TYPE,
-			RelationModelPartitionScanner.RELATION_MODEL_COMMENT, RelationModelPartitionScanner.RELATION_MODEL_OPTION,
-			RelationModelPartitionScanner.RELATION_MODEL_UNIVERSE,
-			RelationModelPartitionScanner.RELATION_MODEL_REL_BOUND };
+  public final static String[] PARTITION_TYPES = new String[] {IDocument.DEFAULT_CONTENT_TYPE,
+      RelationModelPartitionScanner.RELATION_MODEL_COMMENT,
+      RelationModelPartitionScanner.RELATION_MODEL_OPTION,
+      RelationModelPartitionScanner.RELATION_MODEL_UNIVERSE,
+      RelationModelPartitionScanner.RELATION_MODEL_REL_BOUND};
 
-	public RelationModelPartitionScanner() {
-		final IToken commentPartition = new Token(RelationModelPartitionScanner.RELATION_MODEL_COMMENT);
-		final IToken optionPartition = new Token(RelationModelPartitionScanner.RELATION_MODEL_OPTION);
-		final IToken universePartition = new Token(RelationModelPartitionScanner.RELATION_MODEL_UNIVERSE);
-		final IToken relBoundPartition = new Token(RelationModelPartitionScanner.RELATION_MODEL_REL_BOUND);
+  public RelationModelPartitionScanner() {
+    final IToken commentPartition = new Token(RelationModelPartitionScanner.RELATION_MODEL_COMMENT);
+    final IToken optionPartition = new Token(RelationModelPartitionScanner.RELATION_MODEL_OPTION);
+    final IToken universePartition =
+        new Token(RelationModelPartitionScanner.RELATION_MODEL_UNIVERSE);
+    final IToken relBoundPartition =
+        new Token(RelationModelPartitionScanner.RELATION_MODEL_REL_BOUND);
 
-		final List<IRule> rules = new ArrayList<IRule>();
-		rules.add(new MultiLineRule("/*", "*/", commentPartition));
-		rules.add(new EndOfLineRule("--", commentPartition));
-		rules.add(new EndOfLineRule("//", commentPartition));
-		rules.add(new EndOfLineRule("options", optionPartition));
-		rules.add(new MultiLineRule("universe {", "}", universePartition));
-		rules.add(new MultiLineRule("universe [", "]", universePartition));
-		rules.add(new RelationBoundsRule("Relations:", "Formulas:", relBoundPartition, (char) 0, false));
-		// rules.add(new MultiLineRule("Relations:", "Formulas:",
-		// relBoundPartition));
+    final List<IRule> rules = new ArrayList<IRule>();
+    rules.add(new MultiLineRule("/*", "*/", commentPartition));
+    rules.add(new EndOfLineRule("--", commentPartition));
+    rules.add(new EndOfLineRule("//", commentPartition));
+    rules.add(new EndOfLineRule("options", optionPartition));
+    rules.add(new MultiLineRule("universe {", "}", universePartition));
+    rules.add(new MultiLineRule("universe [", "]", universePartition));
+    // rules.add(new RelationBoundsRule("Relations", "}", relBoundPartition, (char) 0, false));
+    rules.add(new MultiLineRule("Relations", "}", relBoundPartition));
 
-		final IPredicateRule[] result = new IPredicateRule[rules.size()];
-		rules.toArray(result);
-		this.setPredicateRules(result);
-	}
+    final IPredicateRule[] result = new IPredicateRule[rules.size()];
+    rules.toArray(result);
+    this.setPredicateRules(result);
+  }
 }
