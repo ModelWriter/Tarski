fac82e40de4d76a85aac7579fbf5d6179cc236dc
Ümit Anıl Öztürk <anil.ozturk@unitbilisim.com>
2016-02-17 11:31:59 +0200

    relation bounds' damager&repairer error has been fixed.

--- Source/eu.modelwriter.kodkod.editor/src/eu/modelwriter/kodkod/editor/rules/RelationBoundsRule.java
+++ Source/eu.modelwriter.kodkod.editor/src/eu/modelwriter/kodkod/editor/rules/RelationBoundsRule.java
@@ -3,6 +3,7 @@ package eu.modelwriter.kodkod.editor.rules;
 import org.eclipse.jface.text.rules.ICharacterScanner;
 import org.eclipse.jface.text.rules.IToken;
 import org.eclipse.jface.text.rules.MultiLineRule;
+import org.eclipse.jface.text.rules.Token;
 
 public class RelationBoundsRule extends MultiLineRule {
 
@@ -12,12 +13,35 @@ public class RelationBoundsRule extends MultiLineRule {
   }
 
   @Override
+  protected IToken doEvaluate(final ICharacterScanner scanner, final boolean resume) {
+
+    if (resume) {
+      if (this.endSequenceDetected(scanner)) {
+        return this.fToken;
+      }
+    } else {
+      final int c = scanner.read();
+      if (c == this.fStartSequence[0]) {
+        if (this.sequenceDetected(scanner, this.fStartSequence, false)) {
+          if (this.endSequenceDetected(scanner)) {
+            return this.fToken;
+          }
+        }
+      }
+    }
+
+    scanner.unread();
+    return Token.UNDEFINED;
+  }
+
+  @Override
   protected boolean endSequenceDetected(final ICharacterScanner scanner) {
 
     int readCount = 1;
     int c;
     int open = 0;
     while ((c = scanner.read()) != ICharacterScanner.EOF) {
+
       if (c == '{') {
         open++;
       } else if (c == '}') {
