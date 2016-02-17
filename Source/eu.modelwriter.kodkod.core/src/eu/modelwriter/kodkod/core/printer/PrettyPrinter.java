b24e653f51bceaf27788b8b2d86756454cd7be27
Emre Kirmizi <emre.kirmizi@unitbilisim.com>
2016-02-17 14:27:58 +0200

    Implementation of visualization notifies has been completed.

--- Source/eu.modelwriter.kodkod.core/src/eu/modelwriter/kodkod/core/printer/PrettyPrinter.java
+++ Source/eu.modelwriter.kodkod.core/src/eu/modelwriter/kodkod/core/printer/PrettyPrinter.java
@@ -1,22 +1,21 @@
 package eu.modelwriter.kodkod.core.printer;
 
-import java.util.Iterator;
 import java.util.List;
 
-import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.RuleNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
-import eu.modelwriter.kodkod.core.KodkodAnalyzer;
 import eu.modelwriter.kodkod.core.recognizer.KodkodBaseVisitor;
-import eu.modelwriter.kodkod.core.recognizer.KodkodLexer;
-import eu.modelwriter.kodkod.core.recognizer.KodkodParser;
 import eu.modelwriter.kodkod.core.recognizer.KodkodParser.ArityContext;
 import eu.modelwriter.kodkod.core.recognizer.KodkodParser.AtomContext;
 import eu.modelwriter.kodkod.core.recognizer.KodkodParser.BitWidthContext;
 import eu.modelwriter.kodkod.core.recognizer.KodkodParser.FormulaContext;
+import eu.modelwriter.kodkod.core.recognizer.KodkodParser.IntegerContext;
 import eu.modelwriter.kodkod.core.recognizer.KodkodParser.OptionContext;
 import eu.modelwriter.kodkod.core.recognizer.KodkodParser.OptionsContext;
 import eu.modelwriter.kodkod.core.recognizer.KodkodParser.ProblemContext;
+import eu.modelwriter.kodkod.core.recognizer.KodkodParser.RelationContext;
 import eu.modelwriter.kodkod.core.recognizer.KodkodParser.RelationsContext;
 import eu.modelwriter.kodkod.core.recognizer.KodkodParser.SharingContext;
 import eu.modelwriter.kodkod.core.recognizer.KodkodParser.SkolemDepthContext;
@@ -24,136 +23,33 @@ import eu.modelwriter.kodkod.core.recognizer.KodkodParser.SymmetryBreakingContex
 import eu.modelwriter.kodkod.core.recognizer.KodkodParser.TupleContext;
 import eu.modelwriter.kodkod.core.recognizer.KodkodParser.TupleSetContext;
 import eu.modelwriter.kodkod.core.recognizer.KodkodParser.UniverseContext;
+import eu.modelwriter.kodkod.core.recognizer.KodkodParser.VariableContext;
 
 public class PrettyPrinter extends KodkodBaseVisitor<String> {
-  enum PRINT_MODE {
-    PRINT_MODEL, ADD_TUPLE, REMOVE_TUPLE, MOVE_TO_UPPER, MOVE_TO_LOWER;
-    static String relationName = null;
-    static List<String> tuple = null;
-    static String bound = null;
-  }
-
-  String problem = "";
-  PRINT_MODE CURRENT_MODE = PRINT_MODE.PRINT_MODEL;
-  String currentRelationName;
-  String currentBound;
-
-  public String addTuple(final String kodkodString, final String relationName,
-      final List<String> tuple, final String bound) {
-    this.CURRENT_MODE = PRINT_MODE.ADD_TUPLE;
-    PRINT_MODE.relationName = relationName;
-    PRINT_MODE.tuple = tuple;
-    PRINT_MODE.bound = bound;
-    // final KodkodParser parser = this.getParser(kodkodString);
-    this.visit(KodkodAnalyzer.tree);
-    return this.problem;
-  }
-
-  KodkodParser getParser(final String kodkodString) {
-    final ANTLRInputStream input = new ANTLRInputStream(kodkodString);
-    final KodkodLexer lexer = new KodkodLexer(input);
-    final CommonTokenStream tokens = new CommonTokenStream(lexer);
-    return new KodkodParser(tokens);
-  }
-
-  String getStandartTupleSetString(final List<TupleContext> tuples) {
-    String tupleSetString = "";
-    if (tuples.size() > 0) {
-      TupleContext tupleContext = tuples.get(0);
-      if (tupleContext != null) {
-        tupleSetString += "\t\t" + this.visitTuple(tupleContext);
-      }
-
-      for (int i = 1; i < tuples.size(); i++) {
-        tupleContext = tuples.get(i);
-        tupleSetString += ",\n\t\t" + this.visitTuple(tupleContext);
-      }
-    }
-    return tupleSetString;
-  }
-
-  String makeTupleString(final List<String> atoms) {
-    String tupleString = "";
-    tupleString += "[";
-
-    final String atom = atoms.get(0);
-    if (atom != null) {
-      tupleString += atom;
-    }
-
-    for (int i = 1; i < atoms.size(); i++) {
-      tupleString += ", " + atoms.get(i);
-    }
-
-    tupleString += "]";
-    return tupleString;
-  }
-
-  public String moveToLower(final String kodkodString, final String relationName,
-      final List<String> tuple) {
-    this.CURRENT_MODE = PRINT_MODE.MOVE_TO_LOWER;
-    PRINT_MODE.relationName = relationName;
-    PRINT_MODE.tuple = tuple;
-    PRINT_MODE.bound = null;
-    final KodkodParser parser = this.getParser(kodkodString);
-    this.visitProblem(parser.problem());
-    return this.problem;
-  }
-
-  public String moveToUpper(final String kodkodString, final String relationName,
-      final List<String> tuple) {
-    this.CURRENT_MODE = PRINT_MODE.MOVE_TO_UPPER;
-    PRINT_MODE.relationName = relationName;
-    PRINT_MODE.tuple = tuple;
-    PRINT_MODE.bound = null;
-    final KodkodParser parser = this.getParser(kodkodString);
-    this.visitProblem(parser.problem());
-    return this.problem;
-  }
-
-  public String removeTuple(final String kodkodString, final String relationName,
-      final List<String> tuple, final String bound) {
-    this.CURRENT_MODE = PRINT_MODE.REMOVE_TUPLE;
-    PRINT_MODE.relationName = relationName;
-    PRINT_MODE.tuple = tuple;
-    PRINT_MODE.bound = bound;
-    final KodkodParser parser = this.getParser(kodkodString);
-    this.visitProblem(parser.problem());
-    return this.problem;
-  }
-
-  List<TupleContext> removeTupleContextFromList(final List<TupleContext> tuples) {
-    final Iterator<TupleContext> iterator = tuples.iterator();
-    TupleContext tupleContext;
-
-    while (iterator.hasNext()) {
-      tupleContext = iterator.next();
-      final List<AtomContext> atoms = tupleContext.atom();
-      boolean trueTuple = true;
-      for (int i = 0; i < atoms.size(); i++) {
-        final AtomContext atomContext = atoms.get(i);
-        final String atomContextString = atomContext.INT() == null
-            ? atomContext.IDENTIFIER().getText() : atomContext.INT().getText();
-        if (!atomContextString.equals(PRINT_MODE.tuple.get(i))) {
-          trueTuple = false;
-        }
-      }
-      if (trueTuple) {
-        iterator.remove();
-        break;
-      }
-    }
-    return tuples;
-  }
+  private String problem;
 
   @Override
   public String visitArity(final ArityContext ctx) {
-    return ctx != null ? ctx.getText() : "";
+    return ctx != null ? ctx.INT().getText() : "";
   }
 
   @Override
   public String visitAtom(final AtomContext ctx) {
     return ctx.IDENTIFIER() == null ? ctx.INT().getText() : ctx.IDENTIFIER().getText();
+  }
+
+  @Override
+  public String visitChildren(final RuleNode arg0) {
+    String visitString = "";
+    for (int i = 0; i < arg0.getChildCount(); i++) {
+      visitString += this.visit(arg0.getChild(i));
+    }
+    return visitString;
+  }
+
+  @Override
+  public String visitInteger(final IntegerContext ctx) {
+    return ctx != null ? ctx.INT().getText() : "";
   }
 
   @Override
@@ -195,15 +91,13 @@ public class PrettyPrinter extends KodkodBaseVisitor<String> {
     return optionsString;
   }
 
-
-
   @Override
   public String visitProblem(final ProblemContext ctx) {
-    if (ctx.options() != null) {
-      this.problem += "options {\n";
-      this.problem += this.visitOptions(ctx.options());
-      this.problem += "\n}\n\n";
-    }
+    // if (ctx.options() != null) {
+    // this.problem += "options {\n";
+    // this.problem += this.visitOptions(ctx.options());
+    // this.problem += "\n}\n\n";
+    // }
 
     this.problem += "universe {\n";
     if (ctx.universe() != null) {
@@ -217,26 +111,29 @@ public class PrettyPrinter extends KodkodBaseVisitor<String> {
         this.problem += this.visitRelations(relation) + "\n";
       }
     }
-    this.problem += "\n}\n\n";
+    this.problem += "}";
 
     if (ctx.formula() != null) {
       for (final FormulaContext formula : ctx.formula()) {
-        this.problem += formula.getText() + "\n";
+        this.problem += this.visit(formula) + "\n";
       }
     }
     return this.problem;
   }
 
   @Override
+  public String visitRelation(final RelationContext ctx) {
+    return ctx != null ? ctx.IDENTIFIER().getText() : "";
+  }
+
+  @Override
   public String visitRelations(final RelationsContext ctx) {
     String relationsString = "";
-    this.currentRelationName = ctx.relation().getText();
-    relationsString += "\t" + this.currentRelationName;
+    relationsString += "\t" + ctx.relation().getText();
     relationsString += " " + this.visitArity(ctx.arity()) + ": ";
-    // relationsString += ctx.expression() == null ? "" : ctx.expression().getText();
+    relationsString += ctx.expression() == null ? "" : this.visit(ctx.expression());
     relationsString += "[";
 
-    this.currentBound = "lower";
     TupleSetContext lowerBoundTupleSet = ctx.tupleSet(0);
     if (lowerBoundTupleSet == null) {
       lowerBoundTupleSet = new TupleSetContext(ctx, ctx.invokingState);
@@ -244,31 +141,55 @@ public class PrettyPrinter extends KodkodBaseVisitor<String> {
     }
     relationsString += this.visitTupleSet(lowerBoundTupleSet);
 
-    this.currentBound = "upper";
-    TupleSetContext upperBoundTupleSet = ctx.tupleSet(1);
-    if (upperBoundTupleSet == null && PRINT_MODE.relationName.equals(this.currentRelationName)) {
-      switch (this.CURRENT_MODE) {
-        case MOVE_TO_UPPER:
-          upperBoundTupleSet = ctx.tupleSet(0);
-          break;
-        case ADD_TUPLE:
-        case REMOVE_TUPLE:
-          if (PRINT_MODE.bound.equals("upper")) {
-            upperBoundTupleSet = ctx.tupleSet(0);
-          }
-          break;
-        default:
-          // upperBoundTupleSet = new TupleSetContext(ctx, ctx.invokingState);
-          // ctx.tupleSet().add(upperBoundTupleSet);
-          break;
-      }
-    }
+    final TupleSetContext upperBoundTupleSet = ctx.tupleSet(1);
     if (upperBoundTupleSet != null) {
       relationsString += "," + this.visitTupleSet(upperBoundTupleSet);
     }
 
     relationsString += "]";
     return relationsString;
+  }
+
+  @Override
+  public String visitTerminal(final TerminalNode node) {
+    String nodeText = node == null ? "" : node.getText();
+    switch (nodeText) {
+      case "[":
+      case "{":
+      case "(":
+      case "!":
+      case "~":
+      case "^":
+      case "*":
+      case ".":
+      case "]":
+      case "}":
+      case ")":
+        break;
+      case ",":
+      case ":":
+      case "|":
+        nodeText = nodeText + " ";
+        break;
+      case "#":
+        nodeText = " " + nodeText;
+        break;
+      default:
+        final ParseTree parent = node.getParent();
+        for (int i = 0; i < parent.getChildCount(); i++) {
+          final ParseTree child = parent.getChild(i);
+          if (child.getText().equals(node.getText())) {
+            if (parent.getChild(i + 1) instanceof TerminalNode) {
+              nodeText = i == 0 ? "" + nodeText : " " + nodeText;
+            } else {
+              nodeText = i == 0 ? "" + nodeText + " " : " " + nodeText + " ";
+            }
+            break;
+          }
+        }
+        break;
+    }
+    return nodeText;
   }
 
   @Override
@@ -295,68 +216,14 @@ public class PrettyPrinter extends KodkodBaseVisitor<String> {
     String tupleSetString = "";
     tupleSetString += "{\n";
 
-    List<TupleContext> tuples = ctx.tuple();
+    TupleContext tupleContext = ctx.tuple(0);
+    if (tupleContext != null) {
+      tupleSetString += "\t\t" + this.visitTuple(tupleContext);
+    }
 
-    boolean OK = false;
-    switch (this.CURRENT_MODE) {
-      case ADD_TUPLE:
-        tupleSetString += this.getStandartTupleSetString(tuples);
-
-        if (PRINT_MODE.relationName.equals(this.currentRelationName)) {
-          final String tupleString = this.makeTupleString(PRINT_MODE.tuple);
-          if (PRINT_MODE.bound.equals("lower")) {
-            OK = true;
-          } else if (this.currentBound.equals("upper")) {
-            OK = true;
-          }
-          if (OK) {
-            if (tuples.size() < 1) {
-              tupleSetString += "\t\t" + tupleString;
-            } else {
-              tupleSetString += ",\n\t\t" + tupleString;
-            }
-          }
-        }
-        break;
-      case REMOVE_TUPLE:
-        if (PRINT_MODE.relationName.equals(this.currentRelationName)) {
-          if (PRINT_MODE.bound.equals("lower")) {
-            OK = true;
-          } else if (this.currentBound.equals("upper")) {
-            OK = true;
-          }
-          if (OK) {
-            tuples = this.removeTupleContextFromList(tuples);
-          }
-        }
-        tupleSetString += this.getStandartTupleSetString(tuples);
-        break;
-      case MOVE_TO_LOWER:
-        tupleSetString += this.getStandartTupleSetString(tuples);
-        if (PRINT_MODE.relationName.equals(this.currentRelationName)) {
-          if (this.currentBound.equals("lower")) {
-            final String tupleString = this.makeTupleString(PRINT_MODE.tuple);
-            if (tuples.size() < 1) {
-              tupleSetString += "\t\t" + tupleString;
-            } else {
-              tupleSetString += ",\n\t\t" + tupleString;
-            }
-          }
-        }
-        break;
-      case MOVE_TO_UPPER:
-        if (this.currentBound.equals("lower")) {
-          if (PRINT_MODE.relationName.equals(this.currentRelationName)) {
-            tuples = this.removeTupleContextFromList(tuples);
-          }
-          tupleSetString += this.getStandartTupleSetString(tuples);
-        } else if (this.currentBound.equals("upper")) {
-          tupleSetString += this.getStandartTupleSetString(tuples);
-        }
-        break;
-      default:
-        tupleSetString += this.getStandartTupleSetString(tuples);
-        break;
+    for (int i = 1; i < ctx.tuple().size(); i++) {
+      tupleContext = ctx.tuple(i);
+      tupleSetString += ",\n\t\t" + this.visitTuple(tupleContext);
     }
 
     tupleSetString += "\n\t}";
@@ -375,5 +242,10 @@ public class PrettyPrinter extends KodkodBaseVisitor<String> {
       universeString += ", " + this.visitAtom(atomContext);
     }
     return universeString;
+  }
+
+  @Override
+  public String visitVariable(final VariableContext ctx) {
+    return ctx != null ? ctx.IDENTIFIER().getText() : "";
   }
 }
