9c3cd0374151fbeb7707aec81f8faab89ab7829b
Ümit Anıl Öztürk <anil.ozturk@unitbilisim.com>
2016-06-24 10:19:38 +0300

    reasoning and discovering parsing has been changed, regex parsing has been added.

--- Source/eu.modelwriter.visualization.test/src/eu/modelwriter/visualization/model/Atom.java
+++ Source/eu.modelwriter.visualization.test/src/eu/modelwriter/visualization/model/Atom.java
@@ -1,33 +1,37 @@
 package eu.modelwriter.visualization.model;
 
 public class Atom {
-  private String text;
+
+  private final String text;
+
   private Relation locatedIn;
 
-  public Atom(String text) {
+  public Atom(final String text) {
     this.text = text;
+  }
+
+  @Override
+  public boolean equals(final Object obj) {
+    if (obj == null) {
+      return false;
+    }
+    if (obj == this) {
+      return true;
+    }
+
+    return this.getText().equals(((Atom) obj).getText());
+  }
+
+  public Relation getLocatedIn() {
+    return this.locatedIn;
   }
 
   public String getText() {
     return this.text;
   }
 
-  public Relation getLocatedIn() {
-    return locatedIn;
-  }
-
-  public void setLocatedIn(Relation locatedIn) {
+  public void setLocatedIn(final Relation locatedIn) {
     this.locatedIn = locatedIn;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (obj == null)
-      return false;
-    if (obj == this)
-      return true;
-
-    return this.getText().equals(((Atom) obj).getText());
   }
 
 }
