33363b99a5f360a8dbe0710f7a0314aaf1369b20
Ümit Anıl Öztürk <anil.ozturk@unitbilisim.com>
2016-07-13 11:33:44 +0300

    code completion in discover partition has been added.

---
+++ Source/eu.modelwriter.specification.editor/src/eu/modelwriter/specification/editor/completion/DiscoverCompletionProcessor.java
@@ -0,0 +1,72 @@
+package eu.modelwriter.specification.editor.completion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.contentassist.CompletionProposal;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+
+import eu.modelwriter.configuration.alloy.AlloyParserForMetamodel;
+
+public class DiscoverCompletionProcessor extends MetaModelCompletionProcessor {
+
+  private final char[] activationChars = new char[] {'@'};
+
+  @Override
+  public ICompletionProposal[] computeCompletionProposals(final ITextViewer viewer,
+      final int offset) {
+    final List<ICompletionProposal> proposals = new ArrayList<>();
+
+    final IDocument document = viewer.getDocument();
+
+    Character c = null;
+    try {
+      c = document.getChar(offset - 1);
+    } catch (final BadLocationException e) {
+      e.printStackTrace();
+    }
+    int temp = offset - 1;
+    String s = "";
+
+    if (Character.isAlphabetic(c)) {
+      while (Character.isAlphabetic(c)) {
+        s += c;
+        temp--;
+        try {
+          c = document.getChar(temp);
+        } catch (final BadLocationException e) {
+          e.printStackTrace();
+        }
+      }
+      s = new StringBuilder(s).reverse().toString();
+
+      for (final String sig : AlloyParserForMetamodel.getSigs()) {
+        if (sig.startsWith(s)) {
+          proposals.add(new CompletionProposal(sig, temp + 1, s.length(), sig.length()));
+        }
+      }
+    } else {
+      // if the last edited char is non-alphabetic then may be user wants the sig list.
+      for (int i = 0; i < this.activationChars.length; i++) {
+        if (this.activationChars[i] == c) {
+          for (final String sig : AlloyParserForMetamodel.getSigs()) {
+            proposals
+                .add(new CompletionProposal(sig, temp + 1, s.length(), sig.length()));
+          }
+        }
+      }
+    }
+
+    final ICompletionProposal[] result = new ICompletionProposal[proposals.size()];
+    proposals.toArray(result);
+    return result;
+  }
+
+  @Override
+  public char[] getCompletionProposalAutoActivationCharacters() {
+    return this.activationChars;
+  }
+}
