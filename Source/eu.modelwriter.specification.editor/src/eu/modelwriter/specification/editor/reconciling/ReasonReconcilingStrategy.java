5685a594adf29d0f2bb29e48a7f1e200140fef2e
Ümit Anıl Öztürk <anil.ozturk@unitbilisim.com>
2016-07-14 14:49:29 +0300

    reason reconcile has been set because of same relation name development.

--- Source/eu.modelwriter.specification.editor/src/eu/modelwriter/specification/editor/reconciling/ReasonReconcilingStrategy.java
+++ Source/eu.modelwriter.specification.editor/src/eu/modelwriter/specification/editor/reconciling/ReasonReconcilingStrategy.java
@@ -1,79 +1,49 @@
 package eu.modelwriter.specification.editor.reconciling;
 
-import java.util.HashMap;
 import java.util.Iterator;
 
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IMarker;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.ITypedRegion;
 import org.eclipse.jface.text.Position;
 import org.eclipse.jface.text.reconciler.DirtyRegion;
 import org.eclipse.jface.text.source.Annotation;
 import org.eclipse.jface.text.source.IAnnotationModel;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.texteditor.MarkerUtilities;
-import org.eclipse.ui.texteditor.SimpleMarkerAnnotation;
 
 import eu.modelwriter.configuration.alloy.AlloyParserForMetamodel;
+import eu.modelwriter.specification.editor.scanner.MetaModelPartitionScanner;
 
 public class ReasonReconcilingStrategy extends MetaModelReconcilingStrategy {
+
+  private IAnnotationModel annotationModel;
+
+  private boolean isMatch = false;
 
   public ReasonReconcilingStrategy(final ISourceViewer sourceViewer, final IEditorPart editor) {
     super(sourceViewer, editor);
   }
 
-  private void addNewMarker(final String errored, final int line,
+  private void addNewAnnotation(final String errored, final int line,
       final IRegion lineInformationOfOffset) {
     final int offset = lineInformationOfOffset.getOffset();
     final int length = lineInformationOfOffset.getLength();
     final String message = errored + " cannot be resolved as a relation";
 
-    IMarker marker = null;
-    try {
-      marker = this.createErrorMarker(line, offset, length, message);
-    } catch (final CoreException e1) {
-      e1.printStackTrace();
-    }
-    final SimpleMarkerAnnotation ma = new SimpleMarkerAnnotation(this.MME_ANNOT_TYPE, marker);
-
-    this.getAnnotationModel().connect(this.document);
-    this.getAnnotationModel().addAnnotation(ma, new Position(offset, length));
-    this.getAnnotationModel().disconnect(this.document);
-  }
-
-  private IMarker createErrorMarker(final int line, final int offset, final int length,
-      final String message) throws CoreException {
-    final HashMap<String, Object> map = new HashMap<>();
-    MarkerUtilities.setLineNumber(map, line);
-    MarkerUtilities.setCharStart(map, offset);
-    MarkerUtilities.setCharEnd(map, offset + length);
-    MarkerUtilities.setMessage(map, message);
-
-    MarkerUtilities.createMarker(this.file, map, this.MME_MARKER_TYPE);
-
-    return this.getErrorMarker(this.file);
-  }
-
-  private IMarker getErrorMarker(final IFile file) throws CoreException {
-    final IMarker[] markers =
-        file.findMarkers(this.MME_MARKER_TYPE, true, IResource.DEPTH_INFINITE);
-    if (markers.length == 0) {
-      return null;
-    } else {
-      return markers[0];
-    }
+    final Annotation annotation = new Annotation(this.MME_ANNOT_TYPE, true, message);
+    this.annotationModel.connect(this.document);
+    this.annotationModel.addAnnotation(annotation, new Position(offset, length));
+    this.annotationModel.disconnect(this.document);
   }
 
   @Override
   public void reconcile(final DirtyRegion dirtyRegion, final IRegion subRegion) {
-    this.removeOldMarker();
+    this.removeOldAnnotation(subRegion.getOffset());
     try {
       final IRegion lineInformationOfOffset =
-          this.document.getLineInformationOfOffset(dirtyRegion.getOffset());
+          this.document.getLineInformationOfOffset(subRegion.getOffset());
       final String dirtyLine = this.document.get(lineInformationOfOffset.getOffset(),
           lineInformationOfOffset.getLength());
       final int line = this.document.getLineOfOffset(lineInformationOfOffset.getOffset());
@@ -84,13 +54,40 @@ public class ReasonReconcilingStrategy extends MetaModelReconcilingStrategy {
         errored = dirtyLine.substring(at + 1).trim();
       }
 
-      for (final String relation : AlloyParserForMetamodel.getRels()) {
-        if (errored.equals(relation)) {
-          this.removeOldMarker();
-          return;
+      if (errored.indexOf(".") == -1) {
+        this.addNewAnnotation(errored, line, lineInformationOfOffset);
+        this.isMatch = false;
+        return;
+      }
+
+      final String sigName = errored.substring(0, errored.indexOf("."));
+      final String relName = errored.substring(errored.indexOf(".") + 1);
+
+      this.isMatch = false;
+      for (final String sig : AlloyParserForMetamodel.getSigs()) {
+        if (sigName.equals(sig)) {
+          this.isMatch = true;
+          break;
         }
       }
-      this.addNewMarker(errored, line, lineInformationOfOffset);
+
+      if (this.isMatch == false) {
+        this.addNewAnnotation(sigName, line, lineInformationOfOffset);
+        return;
+      }
+
+      this.isMatch = false;
+      for (final String relation : AlloyParserForMetamodel.getRels()) {
+        if (relName.equals(relation)) {
+          this.isMatch = true;
+          break;
+        }
+      }
+
+      if (this.isMatch == false) {
+        this.addNewAnnotation(relName, line, lineInformationOfOffset);
+        return;
+      }
     } catch (final BadLocationException e) {
       e.printStackTrace();
     }
@@ -101,32 +98,41 @@ public class ReasonReconcilingStrategy extends MetaModelReconcilingStrategy {
     if (this.document == null) {
       return;
     }
-  }
-
-  private void removeOldMarker() {
-    final IAnnotationModel annoModel = this.getAnnotationModel();
-    annoModel.connect(this.document);
-
-    IMarker oldMarker = null;
     try {
-      oldMarker = this.getErrorMarker(this.file);
-    } catch (final CoreException e) {
+      final ITypedRegion[] partitionRegions =
+          this.document.computePartitioning(partition.getOffset(), partition.getLength());
+      for (int i = 0; i < partitionRegions.length; i++) {
+        if (partitionRegions[i].getType().equals(MetaModelPartitionScanner.META_MODEL_REASON)) {
+          this.reconcile(null, partitionRegions[i]);
+        }
+      }
+    } catch (final BadLocationException e) {
       e.printStackTrace();
     }
+  }
 
-    if (oldMarker != null) {
-      final Iterator<Annotation> iter = annoModel.getAnnotationIterator();
-      Annotation beRemoved = null;
-      while (iter.hasNext()) {
-        beRemoved = iter.next();
-        annoModel.removeAnnotation(beRemoved);
+  private void removeOldAnnotation(final int offset) {
+    this.annotationModel.connect(this.document);
+
+    final Iterator<Annotation> iter = this.annotationModel.getAnnotationIterator();
+    Annotation beRemoved = null;
+    while (iter.hasNext()) {
+      beRemoved = iter.next();
+      if (!beRemoved.getType().equals(this.MME_ANNOT_TYPE)) {
+        continue;
       }
-      annoModel.disconnect(this.document);
-      try {
-        oldMarker.delete();
-      } catch (final CoreException e) {
-        e.printStackTrace();
+      final Position position = this.annotationModel.getPosition(beRemoved);
+
+      if (position.getOffset() + position.getLength() == offset || position.includes(offset)) {
+        this.annotationModel.removeAnnotation(beRemoved);
       }
     }
+    this.annotationModel.disconnect(this.document);
+  }
+
+  @Override
+  public void setDocument(final IDocument document) {
+    super.setDocument(document);
+    this.annotationModel = this.getAnnotationModel();
   }
 }
