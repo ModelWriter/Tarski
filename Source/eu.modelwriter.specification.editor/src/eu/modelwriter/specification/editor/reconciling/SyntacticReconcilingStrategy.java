b50c3dbf49fef997ad6e1216b30ee1a7ed6e41c3
Ümit Anıl Öztürk <anil.ozturk@unitbilisim.com>
2016-07-14 15:51:43 +0300

    error marker removed, all reconciling actions will be controlled by annotations (parse annotation and reason annotation).

--- Source/eu.modelwriter.specification.editor/src/eu/modelwriter/specification/editor/reconciling/SyntacticReconcilingStrategy.java
+++ Source/eu.modelwriter.specification.editor/src/eu/modelwriter/specification/editor/reconciling/SyntacticReconcilingStrategy.java
@@ -3,14 +3,10 @@ package eu.modelwriter.specification.editor.reconciling;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.HashMap;
 import java.util.Iterator;
 
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IMarker;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.Position;
 import org.eclipse.jface.text.reconciler.DirtyRegion;
@@ -18,25 +14,26 @@ import org.eclipse.jface.text.source.Annotation;
 import org.eclipse.jface.text.source.IAnnotationModel;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.texteditor.MarkerUtilities;
-import org.eclipse.ui.texteditor.SimpleMarkerAnnotation;
 
 import edu.mit.csail.sdg.alloy4.Err;
 import eu.modelwriter.configuration.alloy.AlloyParserForMetamodel;
 import eu.modelwriter.specification.editor.MetaModelEditor;
+import eu.modelwriter.specification.editor.scanner.MetaModelPartitionScanner;
 
 public class SyntacticReconcilingStrategy extends MetaModelReconcilingStrategy {
+
+  private IAnnotationModel annotationModel;
 
   public SyntacticReconcilingStrategy(final ISourceViewer sourceViewer, final IEditorPart editor) {
     super(sourceViewer, editor);
   }
 
   /**
-   * We add new error marker and annotation related to error which alloy parser is giving us.
+   * We add new error annotation related to error which alloy parser is giving us.
    *
    * @param e the exception which is parse operation occurred
    */
-  private void addNewMarker(final Err e) {
+  private void addNewAnnotation(final Err e) {
     final int line = e.pos.y;
     int offset = 0;
     final int length = e.pos.x2 - e.pos.x + 1;
@@ -47,56 +44,25 @@ public class SyntacticReconcilingStrategy extends MetaModelReconcilingStrategy {
       e1.printStackTrace();
     }
 
-    IMarker marker = null;
-    try {
-      marker = this.createErrorMarker(line, offset, length, message);
-    } catch (final CoreException e1) {
-      e1.printStackTrace();
-    }
-    final SimpleMarkerAnnotation ma = new SimpleMarkerAnnotation(this.MME_ANNOT_TYPE, marker);
-
-    this.getAnnotationModel().connect(this.document);
-    this.getAnnotationModel().addAnnotation(ma, new Position(offset, length));
-    this.getAnnotationModel().disconnect(this.document);
-  }
-
-  private IMarker createErrorMarker(final int line, final int offset, final int length,
-      final String message) throws CoreException {
-    final HashMap<String, Object> map = new HashMap<>();
-    MarkerUtilities.setLineNumber(map, line);
-    MarkerUtilities.setCharStart(map, offset);
-    MarkerUtilities.setCharEnd(map, offset + length);
-    MarkerUtilities.setMessage(map, message);
-
-    MarkerUtilities.createMarker(this.file, map, this.MME_MARKER_TYPE);
-
-    return this.getErrorMarker(this.file);
-  }
-
-  /**
-   * Because of Alloy Parser stop parsing when it found an error the document might has just one
-   * error marker
-   *
-   * @param file 'the resource which is used for searching error marker'
-   * @return founded error marker
-   * @throws CoreException - if this method fails. Reasons include:
-   *         <ul>
-   *         <li>This resource does not exist.</li>
-   *         <li>This resource is a project that is not open.</li>
-   *         </ul>
-   */
-  private IMarker getErrorMarker(final IFile file) throws CoreException {
-    final IMarker[] markers =
-        file.findMarkers(this.MME_MARKER_TYPE, true, IResource.DEPTH_INFINITE);
-    if (markers.length == 0) {
-      return null;
-    } else {
-      return markers[0];
-    }
+    final Annotation annotation = new Annotation(this.MME_PARSE_ANNOT_TYPE, true, message);
+    this.annotationModel.connect(this.document);
+    this.annotationModel.addAnnotation(annotation, new Position(offset, length));
+    this.annotationModel.disconnect(this.document);
   }
 
   @Override
   public void reconcile(final DirtyRegion dirtyRegion, final IRegion subRegion) {
+    if (this.document == null) {
+      return;
+    }
+    try {
+      if (this.document.getPartition(dirtyRegion.getOffset() - dirtyRegion.getLength()).getType()
+          .equals(MetaModelPartitionScanner.META_MODEL_REASON)) {
+        return;
+      }
+    } catch (final BadLocationException e) {
+      e.printStackTrace();
+    }
     this.reconcile(subRegion);
   }
 
@@ -119,11 +85,11 @@ public class SyntacticReconcilingStrategy extends MetaModelReconcilingStrategy {
       }
 
       new AlloyParserForMetamodel(tempFile.getAbsolutePath(), this.file.getName());
-      this.removeOldMarker();
+      this.removeOldAnnotation();
       MetaModelEditor.refreshMetamodel(true);
     } catch (final Err e) {
-      this.removeOldMarker();
-      this.addNewMarker(e);
+      this.removeOldAnnotation();
+      this.addNewAnnotation(e);
     } finally {
       if (tempFile.exists()) {
         tempFile.delete();
@@ -133,32 +99,28 @@ public class SyntacticReconcilingStrategy extends MetaModelReconcilingStrategy {
 
   /**
    * If parser parse specification without finding errors or if reconcile method called again then
-   * remove old error marker.
+   * remove old error annotation.
+   *
+   * @param offset
    */
-  private void removeOldMarker() {
-    final IAnnotationModel annoModel = this.getAnnotationModel();
-    annoModel.connect(this.document);
+  private void removeOldAnnotation() {
+    this.annotationModel.connect(this.document);
 
-    IMarker oldMarker = null;
-    try {
-      oldMarker = this.getErrorMarker(this.file);
-    } catch (final CoreException e) {
-      e.printStackTrace();
+    final Iterator<Annotation> iter = this.annotationModel.getAnnotationIterator();
+    Annotation beRemoved = null;
+    while (iter.hasNext()) {
+      beRemoved = iter.next();
+      if (!beRemoved.getType().equals(this.MME_PARSE_ANNOT_TYPE)) {
+        continue;
+      }
+      this.annotationModel.removeAnnotation(beRemoved);
     }
+    this.annotationModel.disconnect(this.document);
+  }
 
-    if (oldMarker != null) {
-      final Iterator<Annotation> iter = annoModel.getAnnotationIterator();
-      Annotation beRemoved = null;
-      while (iter.hasNext()) {
-        beRemoved = iter.next();
-        annoModel.removeAnnotation(beRemoved);
-      }
-      annoModel.disconnect(this.document);
-      try {
-        oldMarker.delete();
-      } catch (final CoreException e) {
-        e.printStackTrace();
-      }
-    }
+  @Override
+  public void setDocument(final IDocument document) {
+    super.setDocument(document);
+    this.annotationModel = this.getAnnotationModel();
   }
 }
