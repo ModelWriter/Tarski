42427f41e7002e64ca71b6b86da045c4fbd6b9bc
Emre Kirmizi <emre.kirmizi@unitbilisim.com>
2016-07-24 14:23:55 +0300

    same relation name feature on reasoning

--- Source/eu.modelwriter.configuration/src/eu/modelwriter/configuration/alloy/reasoning/AlloyReasoning.java
+++ Source/eu.modelwriter.configuration/src/eu/modelwriter/configuration/alloy/reasoning/AlloyReasoning.java
@@ -19,20 +19,6 @@ public class AlloyReasoning {
 
   static String filename = InstanceTranslatorReasoning.baseFileDirectory + "reasoning.als";
 
-  private static AtomType getOriginalAtomType(final String name_R) {
-
-    final String name = name_R.substring(0, name_R.indexOf("_"));
-    final int id = Integer.parseInt(name_R.substring(name_R.indexOf("_") + 1, name_R.indexOf("$")));
-
-    return AlloyUtilities.getSigTypeById(AlloyUtilities.getSigTypeIdByName(name)).getAtom().get(id);
-  }
-
-  public static void main(final String[] args) {
-    final AlloyReasoning alloyReasoning = new AlloyReasoning();
-
-    alloyReasoning.reasoning();
-  }
-
   public void reasoning() {
     if (!AlloyValidator.validate()) {
       JOptionPane.showMessageDialog(null,
@@ -76,46 +62,70 @@ public class AlloyReasoning {
 
     int reasonCount = 0;
     for (final FieldType fieldType_R : documentRootReasoning.getAlloy().getInstance().getField()) {
-      final int sourceId = fieldType_R.getParentID();
-      final String sourceSigName = AlloyUtilities.getSigNameById(sourceId, documentRootReasoning);
-      if (!reasonRelations.containsKey(sourceSigName)
-          || !reasonRelations.get(sourceSigName).contains(fieldType_R.getLabel())) {
-        continue;
-      }
-
       for (final FieldType fieldType_O : documentRootOriginal.getAlloy().getInstance().getField()) {
-        if (fieldType_R.getLabel().equals(fieldType_O.getLabel())) {
-          if (fieldType_O.getTuple().size() != fieldType_R.getTuple().size()) {
-            for (final TupleType tuple_R : fieldType_R.getTuple()) {
-              final AtomType atomType0_R =
-                  AlloyReasoning.getOriginalAtomType(tuple_R.getAtom().get(0).getLabel());
-              final AtomType atomType1_R =
-                  AlloyReasoning.getOriginalAtomType(tuple_R.getAtom().get(1).getLabel());
-              final List<TupleType> tuples = new ArrayList<>();
-              boolean exists = false;
-              for (final TupleType tuple_O : fieldType_O.getTuple()) {
-                if (atomType0_R.getLabel().equals(tuple_O.getAtom().get(0).getLabel())
-                    && atomType1_R.getLabel().equals(tuple_O.getAtom().get(1).getLabel())) {
-                  exists = true;
-                }
-              }
-              if (!exists || fieldType_O.getTuple().size() == 0) {
-                final TupleType tupleType = persistenceFactory.eINSTANCE.createTupleType();
-                tupleType.getAtom().add(atomType0_R);
-                tupleType.getAtom().add(atomType1_R);
-                tupleType.setReasoned(true);
+        if (!fieldType_R.getLabel().equals(fieldType_O.getLabel())) {
+          continue;
+        }
 
-                tuples.add(tupleType);
-                reasonCount++;
-              }
-              if (AlloyNextSolution.getInstance().getOldReasons().get(fieldType_O) == null) {
-                AlloyNextSolution.getInstance().getOldReasons().put(fieldType_O, tuples);
-              } else {
-                AlloyNextSolution.getInstance().getOldReasons().get(fieldType_O).addAll(tuples);
-              }
-              fieldType_O.getTuple().addAll(tuples);
+        final int sourceId_R = fieldType_R.getParentID();
+        final String sourceSigName_R =
+            AlloyUtilities.getSigNameById(sourceId_R, documentRootReasoning);
+        if (!reasonRelations.containsKey(sourceSigName_R)
+            || !reasonRelations.get(sourceSigName_R).contains(fieldType_R.getLabel())) {
+          continue;
+        }
+
+        final int sourceId_O = fieldType_O.getParentID();
+        final String sourceSigName_O =
+            AlloyUtilities.getSigNameById(sourceId_O, documentRootOriginal);
+        if (!reasonRelations.containsKey(sourceSigName_O)
+            || !reasonRelations.get(sourceSigName_O).contains(fieldType_O.getLabel())) {
+          continue;
+        }
+
+        if (!sourceSigName_O.equals(sourceSigName_R)) {
+          continue;
+        }
+
+        if (fieldType_O.getTuple().size() == fieldType_R.getTuple().size()) {
+          continue;
+        }
+
+        for (final TupleType tuple_R : fieldType_R.getTuple()) {
+          final AtomType atomType0_R =
+              AlloyReasoning.getOriginalAtomType(tuple_R.getAtom().get(0).getLabel());
+          final AtomType atomType1_R =
+              AlloyReasoning.getOriginalAtomType(tuple_R.getAtom().get(1).getLabel());
+
+          if (atomType0_R == null || atomType1_R == null) {
+            continue;
+          }
+
+          final List<TupleType> tuples = new ArrayList<>();
+          boolean exists = false;
+          for (final TupleType tuple_O : fieldType_O.getTuple()) {
+            if (atomType0_R.getLabel().equals(tuple_O.getAtom().get(0).getLabel())
+                && atomType1_R.getLabel().equals(tuple_O.getAtom().get(1).getLabel())) {
+              exists = true;
+              break;
             }
           }
+
+          if (!exists || fieldType_O.getTuple().size() == 0) {
+            final TupleType tupleType = persistenceFactory.eINSTANCE.createTupleType();
+            tupleType.getAtom().add(atomType0_R);
+            tupleType.getAtom().add(atomType1_R);
+            tupleType.setReasoned(true);
+
+            tuples.add(tupleType);
+            reasonCount++;
+          }
+          if (AlloyNextSolution.getInstance().getOldReasons().get(fieldType_O) == null) {
+            AlloyNextSolution.getInstance().getOldReasons().put(fieldType_O, tuples);
+          } else {
+            AlloyNextSolution.getInstance().getOldReasons().get(fieldType_O).addAll(tuples);
+          }
+          fieldType_O.getTuple().addAll(tuples);
         }
       }
 
@@ -138,7 +148,15 @@ public class AlloyReasoning {
       JOptionPane.showMessageDialog(null, "Successfully added " + reasonCount + " reason.",
           "Reason on Relations", JOptionPane.WARNING_MESSAGE);
     }
-
   }
 
+  private static AtomType getOriginalAtomType(final String name_R) {
+    if (name_R.contains("/")) {
+      return null;
+    }
+    final String name = name_R.substring(0, name_R.indexOf("_"));
+    final int id = Integer.parseInt(name_R.substring(name_R.indexOf("_") + 1, name_R.indexOf("$")));
+
+    return AlloyUtilities.getSigTypeById(AlloyUtilities.getSigTypeIdByName(name)).getAtom().get(id);
+  }
 }
