7c10c43d8eb43e1f45b54235d3e667e161346ca9
y3seker <y3seker@gmail.com>
2016-08-23 15:32:18 +0300

    Load feature has been updated

---
+++ Source/eu.modelwriter.configuration/src/eu/modelwriter/configuration/internal/EditorUtilities.java
@@ -0,0 +1,114 @@
+package eu.modelwriter.configuration.internal;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.Region;
+
+public class EditorUtilities {
+
+  public static List<String> getPartitionsByType(IDocument document, String partitionType) {
+    List<String> lines = new ArrayList<String>();
+    final Scanner scanner = new Scanner(document.get());
+    int lineNumber = 0;
+    try {
+      while (scanner.hasNextLine()) {
+        final String line = scanner.nextLine();
+        final int offset = document.getLineOffset(lineNumber);
+        if (document.getPartition(offset).getType().equals(partitionType)) {
+          lines.add(line);
+        }
+        lineNumber++;
+      }
+    } catch (BadLocationException e) {
+      e.printStackTrace();
+    } finally {
+      if (scanner != null)
+        scanner.close();
+    }
+    return lines;
+  }
+
+  public static HashMap<String, IRegion> getPartitionInfoByType(IDocument document,
+      String partitionType) {
+    HashMap<String, IRegion> lines = new HashMap<String, IRegion>();
+    final Scanner scanner = new Scanner(document.get());
+    int lineNumber = 0;
+    try {
+      while (scanner.hasNextLine()) {
+        final String line = scanner.nextLine();
+        final int offset = document.getLineOffset(lineNumber);
+        if (document.getPartition(offset).getType().equals(partitionType)) {
+          lines.put(line, new Region(offset, line.length()));
+        }
+        lineNumber++;
+      }
+    } catch (BadLocationException e) {
+      e.printStackTrace();
+    } finally {
+      if (scanner != null)
+        scanner.close();
+    }
+
+    return lines;
+  }
+
+  public static List<String> getPartitionsByRules(File file, String... partitionStrings) {
+    List<String> lines = new ArrayList<String>();
+    Scanner scanner = null;
+    try {
+      scanner = new Scanner(file);
+      while (scanner.hasNextLine()) {
+        final String line = scanner.nextLine();
+        for (String partition : partitionStrings) {
+          if (line.toLowerCase().contains(partition.toLowerCase())) {
+            lines.add(line);
+            break;
+          }
+        }
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } finally {
+      if (scanner != null)
+        scanner.close();
+    }
+    return lines;
+  }
+
+  public static IFile getIFileFromPath(String path) {
+    final Path filePath = new Path(path);
+    return ResourcesPlugin.getWorkspace().getRoot().getFile(filePath);
+  }
+
+  public static String getLineByOffset(IDocument document, int offset) {
+    final Scanner scanner = new Scanner(document.get());
+    int lineNumber = 0;
+    try {
+      while (scanner.hasNextLine()) {
+        final String line = scanner.nextLine();
+        if (lineNumber == document.getLineOfOffset(offset)) {
+          return line;
+        }
+        lineNumber++;
+      }
+    } catch (BadLocationException e) {
+      e.printStackTrace();
+    } finally {
+      if (scanner != null)
+        scanner.close();
+    }
+    return "";
+  }
+
+}
