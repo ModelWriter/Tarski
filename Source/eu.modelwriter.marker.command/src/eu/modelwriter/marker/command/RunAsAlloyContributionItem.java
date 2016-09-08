dcb9e82096f92ba5d3f45b1ea8b2c4923623b252
Yunus Emre Şeker <y3seker@gmail.com>
2016-09-08 17:04:11 +0300

    RunAsAlloy window maximized

--- Source/eu.modelwriter.marker.command/src/eu/modelwriter/marker/command/RunAsAlloyContributionItem.java
+++ Source/eu.modelwriter.marker.command/src/eu/modelwriter/marker/command/RunAsAlloyContributionItem.java
@@ -4,15 +4,13 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.action.ContributionItem;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.FileEditorInput;
 
 import edu.mit.csail.sdg.alloy4.ConstList;
@@ -35,24 +33,13 @@ public class RunAsAlloyContributionItem extends ContributionItem implements Sele
     super(id);
   }
 
-  public IFile getFile() {
-    final ISelection selection =
-        PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection();
-    if (selection != null && selection instanceof TreeSelection) {
-      final TreeSelection treeSelection = (TreeSelection) selection;
-      final IFile file = (IFile) treeSelection.getFirstElement();
-      return file;
-    }
-    return null;
-  }
-
   @Override
   public void fill(Menu menu, int index) {
     super.fill(menu, index);
     int i = index;
 
     String filePath = "";
-    IFile selectedFile = getFile();
+    IFile selectedFile = AlloyParseUtil.getSelectedFile();
     if (selectedFile == null) {
       editor = Activator.getActiveWorkbenchWindow().getActivePage().getActiveEditor();
       editor.doSave(new NullProgressMonitor());
@@ -71,16 +58,9 @@ public class RunAsAlloyContributionItem extends ContributionItem implements Sele
           menuItem.setData("command", com);
           menuItem.addSelectionListener(this);
         }
-      } else {
-        final MenuItem menuItem = new MenuItem(menu, SWT.PUSH, i++);
-        menuItem.setText("There is no command to execute");
-        return;
       }
     } catch (Err e) {
       e.printStackTrace();
-      final MenuItem menuItem = new MenuItem(menu, SWT.PUSH, i++);
-      menuItem.setText("There is no command to execute");
-      menuItem.setData("command", null);
       return;
     }
   }
@@ -95,12 +75,16 @@ public class RunAsAlloyContributionItem extends ContributionItem implements Sele
     try {
       A4Solution solution = alloyExecuter.executeCommand(command);
       if (solution != null) {
-        solution.writeXML("alloy_example_output.xml");
         showWizard(solution, command.toString());
       } else {
+        String message = "";
+        if (command.check) {
+          message = "No counterexample found. Assertion may be valid.";
+        } else {
+          message = "No instance found. Predicate may be inconsistent.";
+        }
         final MessageDialog warningDialog = new MessageDialog(Activator.getShell(), "Result", null,
-            "No counterexample found. Assertion may be invalid.", MessageDialog.WARNING,
-            new String[] {"OK"}, 0);
+            message, MessageDialog.WARNING, new String[] {"OK"}, 0);
         warningDialog.open();
       }
     } catch (Err e1) {
@@ -116,12 +100,11 @@ public class RunAsAlloyContributionItem extends ContributionItem implements Sele
     AlloySolutionSelectionPage page = new AlloySolutionSelectionPage();
     wizard.addPage(page);
     page.setTitle("Alloy Solutions");
-    // Display display = Display.getDefault();
-    // Shell shell = new Shell(display);
-    // shell.setMaximized(true);
-    // shell.setVisible(false);
-    // shell.pack();
     ConverterWizardDialog dialog = new ConverterWizardDialog(null, wizard);
+    // dialog maximized
+    dialog.setPageSize(Display.getDefault().getBounds().width,
+        Display.getDefault().getPrimaryMonitor().getBounds().height);
+    // set the solution to show
     page.setFirstSolution(solution);
     dialog.open();
   }
