54706bc15acd660f8039fc08924612e46927e2ff
emre.kirmizi <emre.kirmizi@unitbilisim.com>
2016-10-11 15:41:40 +0300

    jgraphx traceabilityview

--- Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/views/visualizationview/TraceabilityViewJGraphx.java
+++ Source/eu.modelwriter.marker.ui/src/eu/modelwriter/marker/ui/internal/views/visualizationview/TraceabilityViewJGraphx.java
@@ -2,16 +2,19 @@ package eu.modelwriter.marker.ui.internal.views.visualizationview;
 
 import java.awt.Frame;
 
+import javax.swing.JPanel;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.awt.SWT_AWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
-import eu.modelwriter.model.ModelManager;
-import eu.modelwriter.model.example.ExampleGenerator;
+import eu.modelwriter.configuration.internal.AlloyUtilities;
+import eu.modelwriter.visualization.IVisualization;
+import eu.modelwriter.visualization.VisualizationFactory;
+import eu.modelwriter.visualization.input.GraphInputFactory;
 
 public class TraceabilityViewJGraphx extends ViewPart {
-  private static ModelManager modelManager = null;
   private static Composite parent;
 
   public TraceabilityViewJGraphx() {
@@ -20,9 +23,6 @@ public class TraceabilityViewJGraphx extends ViewPart {
 
   @Override
   public void createPartControl(final Composite parent) {
-    if (TraceabilityViewJGraphx.getModelManager() == null) {
-      return;
-    }
     TraceabilityViewJGraphx.parent = parent;
     TraceabilityViewJGraphx.show();
   }
@@ -30,23 +30,15 @@ public class TraceabilityViewJGraphx extends ViewPart {
   @Override
   public void setFocus() {}
 
-  public static ModelManager getModelManager() {
-    // TraceabilityViewJGraphx.modelManager.addObserver(new VisualizationObserver());
-    return TraceabilityViewJGraphx.modelManager;
-  }
-
-  static void setModelManager(final ModelManager modelManager) {
-    TraceabilityViewJGraphx.modelManager = modelManager;
-    // TraceabilityViewJGraphx.modelManager.addObserver(new VisualizationObserver());
-  }
-
   public static void show() {
+    final IVisualization visualization = VisualizationFactory.createVisualizationJGraphx(
+        GraphInputFactory.createGraphInputJGraphx(AlloyUtilities.getDocumentRoot()));
+    final JPanel graph = visualization.getGraph();
+
     final Composite cmp =
         new Composite(TraceabilityViewJGraphx.parent, SWT.EMBEDDED | SWT.NO_BACKGROUND);
     final Frame frame = SWT_AWT.new_Frame(cmp);
-    final Frame frm =
-        new eu.modelwriter.visualization.editor.Frame(ExampleGenerator.createFileSystemExample());
-    frame.add(frm.getComponent(0));
+    frame.add(graph);
     frame.setVisible(true);
     frame.setAlwaysOnTop(true);
     frame.pack();
