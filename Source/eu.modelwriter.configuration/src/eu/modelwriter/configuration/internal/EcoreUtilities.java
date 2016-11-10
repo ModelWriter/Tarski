8788011181d373479e57f790db7fa997cc23d3d2
Yunus Emre Şeker <y3seker@gmail.com>
2016-11-10 15:15:16 +0200

    Minor changes

--- Source/eu.modelwriter.configuration/src/eu/modelwriter/configuration/internal/EcoreUtilities.java
+++ Source/eu.modelwriter.configuration/src/eu/modelwriter/configuration/internal/EcoreUtilities.java
@@ -215,11 +215,11 @@ public class EcoreUtilities {
   @SuppressWarnings({"rawtypes", "unchecked"})
   public static void saveResource(EObject root) {
     Map options = new HashMap();
-    // options.put(XMLResource.OPTION_SCHEMA_LOCATION, Boolean.TRUE);
-    // options.put(XMLResource.OPTION_SAVE_TYPE_INFORMATION, noTypeInfo);
+    options.put(XMLResource.OPTION_SCHEMA_LOCATION, Boolean.TRUE);
+    options.put(XMLResource.OPTION_SAVE_TYPE_INFORMATION, noTypeInfo);
 
     try {
-      root.eResource().save(options);
+      root.eResource().save(null);
     } catch (IOException e) {
       e.printStackTrace();
     }
