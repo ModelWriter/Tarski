cbf22793bfe8f3beace8d1d08fe5871fd87e9fe9
Ferhat Erata <ferhat@computer.org>
2016-12-20 17:13:03 +0300

    Grammar updated.

--- Source/eu.modelwriter.core.alloyinecore/src/eu/modelwriter/core/alloyinecore/recognizer/AlloyInEcoreParser.java
+++ Source/eu.modelwriter.core.alloyinecore/src/eu/modelwriter/core/alloyinecore/recognizer/AlloyInEcoreParser.java
@@ -1,27 +1,3 @@
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2016, Ferhat Erata <ferhat@computer.org>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 // Generated from /home/ferhat/IdeaProjects/eu.modelwriter.core.alloyinecore/src/eu/modelwriter/core/alloyinecore/recognizer/AlloyInEcore.g4 by ANTLR 4.5.3
 package eu.modelwriter.core.alloyinecore.recognizer;
  
@@ -9008,8 +8984,8 @@ public class AlloyInEcoreParser extends Parser {
 	}
 
 	public static class QualifiedNameContext extends ParserRuleContext {
+		public IdentifierContext firstPart;
 		public IdentifierContext identifier;
-		public List<IdentifierContext> firstPart = new ArrayList<IdentifierContext>();
 		public List<IdentifierContext> midParts = new ArrayList<IdentifierContext>();
 		public IdentifierContext lastPart;
 		public List<IdentifierContext> identifier() {
@@ -9046,8 +9022,7 @@ public class AlloyInEcoreParser extends Parser {
 			enterOuterAlt(_localctx, 1);
 			{
 			setState(1460);
-			((QualifiedNameContext)_localctx).identifier = identifier();
-			((QualifiedNameContext)_localctx).firstPart.add(((QualifiedNameContext)_localctx).identifier);
+			((QualifiedNameContext)_localctx).firstPart = identifier();
 			setState(1470);
 			_la = _input.LA(1);
 			if (_la==T__131) {
