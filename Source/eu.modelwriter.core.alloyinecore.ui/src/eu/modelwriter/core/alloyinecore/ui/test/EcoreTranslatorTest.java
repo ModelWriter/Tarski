0b1fa39469b023d85557feff6e49f5f7c87c260e
Yunus Emre Şeker <y3seker@gmail.com>
2016-12-31 15:22:46 +0200

    EcoreTranslator: Package test added

--- Source/eu.modelwriter.core.alloyinecore.ui/src/eu/modelwriter/core/alloyinecore/ui/test/EcoreTranslatorTest.java
+++ Source/eu.modelwriter.core.alloyinecore.ui/src/eu/modelwriter/core/alloyinecore/ui/test/EcoreTranslatorTest.java
@@ -1,6 +1,11 @@
 package eu.modelwriter.core.alloyinecore.ui.test;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -19,16 +24,22 @@ public class EcoreTranslatorTest {
   @Test
   public void translatorTest() throws NullPointerException, IOException {
     long millis = System.currentTimeMillis();
-    String string = translator.translate("src/eu/modelwriter/core/alloyinecore/ui/test/Test.ecore");
+    String string =
+        translator.translate("src/eu/modelwriter/core/alloyinecore/ui/test/ecores/Test.ecore");
     System.out.println("Time ellapsed: " + (System.currentTimeMillis() - millis));
     System.out.println(string);
   }
 
-  public static void main(String[] args) {
-    String asd = "//falan/filan/whoo";
-    System.out.println(asd);
-    asd = asd.replaceAll("/([^/]*)$", "---" + "$1");
-    System.out.println(asd);
+  @Test
+  public void packageTranslate() throws IOException {
+    String actual = translator
+        .translate("src/eu/modelwriter/core/alloyinecore/ui/test/ecores/PackageTest.ecore");
+    String expected =
+        getString("src/eu/modelwriter/core/alloyinecore/ui/test/ecores/PackageTest.txt");
+    assertEquals(expected, actual);
   }
 
+  private String getString(String fileName) throws IOException {
+    return new String(Files.readAllBytes(Paths.get(fileName)), StandardCharsets.UTF_8);
+  }
 }
