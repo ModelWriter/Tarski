e7a366d420ac40b498db73e42ba239a5367a531e
emre.kirmizi <emre.kirmizi@unitbilisim.com>
2017-01-13 09:54:51 +0200

    test data is fixed. Ecore.ecore is added as test data.
    removeAllWhiteSpaces method is added to Utilities class.

--- Source/eu.modelwriter.configuration/src/eu/modelwriter/configuration/internal/Utilities.java
+++ Source/eu.modelwriter.configuration/src/eu/modelwriter/configuration/internal/Utilities.java
@@ -20,39 +20,39 @@ public class Utilities {
 
   public static final String FILE_SEPERATOR = System.getProperty("file.separator");
 
-  public static boolean appendToFile(String filePath, String textToAppend) {
+  public static boolean appendToFile(final String filePath, final String textToAppend) {
     try (FileWriter fw = new FileWriter(filePath, true)) {
       fw.write(textToAppend);
       fw.close();
       return true;
-    } catch (Exception e) {
+    } catch (final Exception e) {
       return false;
     }
   }
 
-  public static boolean copyFileContent(String sourcePath, String targetPath) {
+  public static boolean copyFileContent(final String sourcePath, final String targetPath) {
     FileInputStream fr = null;
     FileOutputStream fw = null;
     try {
       fr = new FileInputStream(sourcePath);
       fw = new FileOutputStream(targetPath);
-      FileChannel src = fr.getChannel();
-      FileChannel dest = fw.getChannel();
+      final FileChannel src = fr.getChannel();
+      final FileChannel dest = fw.getChannel();
       dest.transferFrom(src, 0, src.size());
       return true;
-    } catch (Exception e) {
+    } catch (final Exception e) {
       return false;
     } finally {
       try {
         fr.close();
         fw.close();
-      } catch (Exception e) {
+      } catch (final Exception e) {
         return false;
       }
     }
   }
 
-  public static void writeToFile(String fileName, StringBuilder builder) {
+  public static void writeToFile(final String fileName, final StringBuilder builder) {
     try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))) {
       final int aLength = builder.length();
       final int aChunk = 1024;// 1 kb buffer to read data from
@@ -62,21 +62,22 @@ public class Utilities {
         final int aPosEnd = Math.min(aPosStart + aChunk, aLength);
         builder.getChars(aPosStart, aPosEnd, aChars, 0); // Create no new buffer
         bw.write(aChars, 0, aPosEnd - aPosStart);// This is faster than just copying one byte at the
-                                                 // time
+        // time
       }
       bw.flush();
-    } catch (IOException e) {
+    } catch (final IOException e) {
       e.printStackTrace();
     }
   }
 
-  public static IFile getIFileFromPath(String path) {
+  public static IFile getIFileFromPath(final String path) {
     final Path filePath = new Path(path);
     // for absolute paths
     IFile iFile = ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(filePath);
     // might be relative path?
-    if (iFile == null)
+    if (iFile == null) {
       iFile = ResourcesPlugin.getWorkspace().getRoot().getFile(filePath);
+    }
     return iFile;
   }
 
@@ -86,11 +87,12 @@ public class Utilities {
    * @param scanner
    * @return comment free line
    */
-  public static String getNextLine(Scanner scanner) {
+  public static String getNextLine(final Scanner scanner) {
     while (scanner.hasNextLine()) {
       String line = scanner.nextLine().trim();
-      if (line.startsWith("//"))
+      if (line.startsWith("//")) {
         continue; // Its single comment line, skip
+      }
 
       if (line.startsWith("/*")) { // multi line comment
         String skipLine = line;
@@ -99,8 +101,9 @@ public class Utilities {
         }
         // in case there is data after comment close tag
         line = skipLine.substring(skipLine.indexOf("*/") + 2);
-        if (line.isEmpty())
+        if (line.isEmpty()) {
           continue;
+        }
       }
       return line;
     }
@@ -114,7 +117,7 @@ public class Utilities {
    * @param message dialog's message
    * @param type dialog's type image constant, see @MessageDialog
    */
-  public static void showOKDialog(int type, String title, String message) {
+  public static void showOKDialog(final int type, final String title, final String message) {
     Display.getDefault().asyncExec(new Runnable() {
 
       @Override
@@ -124,5 +127,40 @@ public class Utilities {
         warningdialog.open();
       }
     });
+  }
+
+  private final static String whitespace_chars = "" /* dummy empty string for homogeneity */
+      + "\\u0009" // CHARACTER TABULATION
+      + "\\u000A" // LINE FEED (LF)
+      + "\\u000B" // LINE TABULATION
+      + "\\u000C" // FORM FEED (FF)
+      + "\\u000D" // CARRIAGE RETURN (CR)
+      + "\\u0020" // SPACE
+      + "\\u0085" // NEXT LINE (NEL)
+      + "\\u00A0" // NO-BREAK SPACE
+      + "\\u1680" // OGHAM SPACE MARK
+      + "\\u180E" // MONGOLIAN VOWEL SEPARATOR
+      + "\\u2000" // EN QUAD
+      + "\\u2001" // EM QUAD
+      + "\\u2002" // EN SPACE
+      + "\\u2003" // EM SPACE
+      + "\\u2004" // THREE-PER-EM SPACE
+      + "\\u2005" // FOUR-PER-EM SPACE
+      + "\\u2006" // SIX-PER-EM SPACE
+      + "\\u2007" // FIGURE SPACE
+      + "\\u2008" // PUNCTUATION SPACE
+      + "\\u2009" // THIN SPACE
+      + "\\u200A" // HAIR SPACE
+      + "\\u2028" // LINE SEPARATOR
+      + "\\u2029" // PARAGRAPH SEPARATOR
+      + "\\u202F" // NARROW NO-BREAK SPACE
+      + "\\u205F" // MEDIUM MATHEMATICAL SPACE
+      + "\\u3000" // IDEOGRAPHIC SPACE
+      ;
+  /* A \s that actually works for Java’s native character set: Unicode */
+  private final static String whitespace_charclass = "[" + Utilities.whitespace_chars + "]";
+
+  public static String removeAllWhiteSpaces(final String str) {
+    return str.replaceAll(Utilities.whitespace_charclass, "");
   }
 }
