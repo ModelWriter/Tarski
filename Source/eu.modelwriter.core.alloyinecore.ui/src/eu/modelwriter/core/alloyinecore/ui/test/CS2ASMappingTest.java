5e067f90a25de64b148e752c9a1f8f4f6f5ff352
emre.kirmizi <emre.kirmizi@unitbilisim.com>
2017-01-13 09:54:51 +0200

    test data is fixed. Ecore.ecore is added as test data.
    removeAllWhiteSpaces method is added to Utilities class.

--- Source/eu.modelwriter.core.alloyinecore.ui/src/eu/modelwriter/core/alloyinecore/ui/test/CS2ASMappingTest.java
+++ Source/eu.modelwriter.core.alloyinecore.ui/src/eu/modelwriter/core/alloyinecore/ui/test/CS2ASMappingTest.java
@@ -29,8 +29,8 @@ public class CS2ASMappingTest {
     final long millis = System.currentTimeMillis();
 
     mapper.parseAndSave(getString("test/AnnotationTest.recore"),
-        URI.createFileURI("test/AnnotationTestTemp.ecore"));
-    final EObject generated = loadResource("test/AnnotationTestTemp.ecore");
+        URI.createFileURI("testTemp/AnnotationTestTemp.ecore"));
+    final EObject generated = loadResource("testTemp/AnnotationTestTemp.ecore");
     final EObject expected = loadResource("test/AnnotationTest.ecore");
 
     Assert.assertTrue(
@@ -43,8 +43,8 @@ public class CS2ASMappingTest {
     final long millis = System.currentTimeMillis();
 
     mapper.parseAndSave(getString("test/AttrTest.recore"),
-        URI.createFileURI("test/AttrTestTemp.ecore"));
-    final EObject generated = loadResource("test/AttrTestTemp.ecore");
+        URI.createFileURI("testTemp/AttrTestTemp.ecore"));
+    final EObject generated = loadResource("testTemp/AttrTestTemp.ecore");
     final EObject expected = loadResource("test/AttrTest.ecore");
 
     Assert.assertTrue("AttrTest.ecore is not OK: " + (System.currentTimeMillis() - millis) + "ms.",
@@ -56,8 +56,8 @@ public class CS2ASMappingTest {
     final long millis = System.currentTimeMillis();
 
     mapper.parseAndSave(getString("test/ClassifierTest.recore"),
-        URI.createFileURI("test/ClassifierTestTemp.ecore"));
-    final EObject generated = loadResource("test/ClassifierTestTemp.ecore");
+        URI.createFileURI("testTemp/ClassifierTestTemp.ecore"));
+    final EObject generated = loadResource("testTemp/ClassifierTestTemp.ecore");
     final EObject expected = loadResource("test/ClassifierTest.ecore");
 
     Assert.assertTrue(
@@ -70,8 +70,8 @@ public class CS2ASMappingTest {
     final long millis = System.currentTimeMillis();
 
     mapper.parseAndSave(getString("test/OperationTest.recore"),
-        URI.createFileURI("test/OperationTestTemp.ecore"));
-    final EObject generated = loadResource("test/OperationTestTemp.ecore");
+        URI.createFileURI("testTemp/OperationTestTemp.ecore"));
+    final EObject generated = loadResource("testTemp/OperationTestTemp.ecore");
     final EObject expected = loadResource("test/OperationTest.ecore");
 
     Assert.assertTrue(
@@ -84,8 +84,8 @@ public class CS2ASMappingTest {
     final long millis = System.currentTimeMillis();
 
     mapper.parseAndSave(getString("test/PackageTest.recore"),
-        URI.createFileURI("test/PackageTestTemp.ecore"));
-    final EObject generated = loadResource("test/PackageTestTemp.ecore");
+        URI.createFileURI("testTemp/PackageTestTemp.ecore"));
+    final EObject generated = loadResource("testTemp/PackageTestTemp.ecore");
     final EObject expected = loadResource("test/PackageTest.ecore");
 
     Assert.assertTrue(
@@ -98,8 +98,8 @@ public class CS2ASMappingTest {
     final long millis = System.currentTimeMillis();
 
     mapper.parseAndSave(getString("test/ReferenceTest.recore"),
-        URI.createFileURI("test/ReferenceTestTemp.ecore"));
-    final EObject generated = loadResource("test/ReferenceTestTemp.ecore");
+        URI.createFileURI("testTemp/ReferenceTestTemp.ecore"));
+    final EObject generated = loadResource("testTemp/ReferenceTestTemp.ecore");
     final EObject expected = loadResource("test/ReferenceTest.ecore");
 
     Assert.assertTrue(
@@ -111,8 +111,9 @@ public class CS2ASMappingTest {
   public void testTest() throws NullPointerException, IOException {
     final long millis = System.currentTimeMillis();
 
-    mapper.parseAndSave(getString("test/Test.recore"), URI.createFileURI("test/TestTemp.ecore"));
-    final EObject generated = loadResource("test/TestTemp.ecore");
+    mapper.parseAndSave(getString("test/Test.recore"),
+        URI.createFileURI("testTemp/TestTemp.ecore"));
+    final EObject generated = loadResource("testTemp/TestTemp.ecore");
     final EObject expected = loadResource("test/Test.ecore");
 
     Assert.assertTrue("Test.ecore is not OK: " + (System.currentTimeMillis() - millis) + "ms.",
@@ -124,14 +125,26 @@ public class CS2ASMappingTest {
     final long millis = System.currentTimeMillis();
 
     mapper.parseAndSave(getString("test/GenericsTest.recore"),
-        URI.createFileURI("test/GenericsTestTemp.ecore"));
-    final EObject generated = loadResource("test/GenericsTestTemp.ecore");
+        URI.createFileURI("testTemp/GenericsTestTemp.ecore"));
+    final EObject generated = loadResource("testTemp/GenericsTestTemp.ecore");
     final EObject expected = loadResource("test/GenericsTest.ecore");
 
     Assert.assertTrue("Test.ecore is not OK: " + (System.currentTimeMillis() - millis) + "ms.",
         EcoreUtil.equals(generated, expected));
   }
 
+  @Test
+  public void ecoreTest() throws NullPointerException, IOException {
+    final long millis = System.currentTimeMillis();
+
+    mapper.parseAndSave(getString("test/Ecore.recore"),
+        URI.createFileURI("testTemp/EcoreTemp.ecore"));
+    final EObject generated = loadResource("testTemp/EcoreTemp.ecore");
+    final EObject expected = loadResource("test/Ecore.ecore");
+
+    Assert.assertTrue("Test.ecore is not OK: " + (System.currentTimeMillis() - millis) + "ms.",
+        EcoreUtil.equals(generated, expected));
+  }
 
   private String getString(final String fileName) throws IOException {
     return new String(Files.readAllBytes(Paths.get(fileName)), StandardCharsets.UTF_8);
